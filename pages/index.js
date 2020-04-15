724ffbd84d307053c0ffb47bfef29cb188ff3036
Wen Ting Wang <chubberlisk@outlook.com>
2020-04-15 22:03:00 +0100

    Update fornmatting of index using Prettier

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
-import React, { useCallback, useState } from 'react';
-import Button from '../src/components/Button';
-import FormGroup from '../src/components/FormGroup';
-import { GridRow, GridColumn } from '../src/components/Grid';
-import Heading from '../src/components/Heading';
-import Hint from '../src/components/Hint';
-import Input from '../src/components/Input';
-import Label from '../src/components/Label';
-import Layout from '../src/components/Layout';
-import fetch from 'isomorphic-unfetch';
+import React, { useCallback, useState } from "react";
+import Button from "../src/components/Button";
+import FormGroup from "../src/components/FormGroup";
+import { GridRow, GridColumn } from "../src/components/Grid";
+import Heading from "../src/components/Heading";
+import Hint from "../src/components/Hint";
+import Input from "../src/components/Input";
+import Label from "../src/components/Label";
+import Layout from "../src/components/Layout";
+import fetch from "isomorphic-unfetch";
 
 const Home = () => {
-  const [contactNumber, setContactNumber] = useState('');
+  const [contactNumber, setContactNumber] = useState("");
 
   const onSubmit = useCallback(async (event) => {
     event.preventDefault();
 
-    const response = await fetch('/api/calls', {
-      method: 'POST',
+    const response = await fetch("/api/calls", {
+      method: "POST",
       headers: {
-        'content-type': 'application/json',
+        "content-type": "application/json",
       },
       body: JSON.stringify({
         contactNumber,
@@ -48,20 +48,18 @@ const Home = () => {
                 type="number"
                 maxLength={11}
                 className="nhsuk-u-font-size-32"
-                style={{ padding: '32px 16px!important' }}
+                style={{ padding: "32px 16px!important" }}
                 onChange={(event) => setContactNumber(event.target.value)}
                 name="contact"
               />
-              <Button className="nhsuk-u-margin-top-5">
-                Send invite
-              </Button>
+              <Button className="nhsuk-u-margin-top-5">Send invite</Button>
             </FormGroup>
           </form>
         </GridColumn>
-        <span style={{ clear: 'both', display: 'block' }}></span>
+        <span style={{ clear: "both", display: "block" }}></span>
       </GridRow>
     </Layout>
-  )
-}
+  );
+};
 
 export default Home;
