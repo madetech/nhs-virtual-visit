4db12d9a62631bf4916286d77ac227e6937ab88e
Antony O'Neill <antony@ant-web.co.uk>
2020-04-15 17:59:21 +0100

    Use call id for meeting room name
    Also check script has loaded prior to initialising room

--- pages/call/[id].js
+++ pages/call/[id].js
@@ -1,14 +1,24 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-import Layout from '../../src/components/Layout';
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Layout from "../../src/components/Layout";
+import "./[id].scss";
+import useScript from '../../src/hooks/useScript'
 
 const Call = () => {
-  const { query: { id } } = useRouter();
+  const {
+    query: { id },
+  } = useRouter();
+  
+  const [libraryLoaded] = useScript('https://meet.jit.si/external_api.js')
 
   useEffect(() => {
+    if (!libraryLoaded) {
+      return;
+    }
+
     const domain = "meet.jit.si";
     const options = {
-      roomName: "MadeTechJitsiPOC",
+      roomName: id,
       width: "100%",
       height: "100%",
       parentNode: document.querySelector("#meet"),
@@ -16,23 +26,25 @@ const Call = () => {
         TOOLBAR_BUTTONS: ["microphone", "camera", "hangup"],
       },
     };
-    
+
     const api = new window.JitsiMeetExternalAPI(domain, options);
-  }, []);
-     
+  }, [libraryLoaded]);
+
+
+  if (!id) {
+    return (
+      <Layout>
+        <h1>No calling code provided</h1>
+        <p>Please use the link provided in the message you received.</p>
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
-      Call Id: {id}
-
       <main>
         <div id="meet"></div>
       </main>
-
-      <style jsx global>{`
-      #meet, #meet > iframe {
-          min-height: 100vh;
-        }
-      `}</style>
     </Layout>
   );
 };
