06f9ac1dec4c5df8c3919088b546b9b2fec5f45c
Tom Davies <tom.davies@madetech.com>
2020-04-15 15:14:28 +0100

    Wires up page to call the API and redirect to call

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,51 @@
+import React, { useCallback, useState } from 'react';
 import Layout from '../src/components/Layout';
+import fetch from 'isomorphic-unfetch';
 
 const Home = () => {
+  const [contactNumber, setContactNumber] = useState('');
+  
+  const onSubmit = useCallback(async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('/api/calls', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        contactNumber,
+      }),
+    });
+
+    const { callUrl } = await response.json();
+    window.location.href = callUrl;
+  });
+
   return (
     <Layout>
-      <form style={{textAlign: 'center', paddingBottom: '30%'}}>
+      <form onSubmit={onSubmit} style={{textAlign: 'center', paddingBottom: '30%'}}>
 
         <h1 style={{paddingTop: '5%'}}>Call a Key Contact</h1>
         <p>Enter the key contacts mobile number to begin a call</p>
 
-        <div class="nhsuk-form-group">
-          <label class="nhsuk-label" for="example">
-            What is your name?
+        <div className="nhsuk-form-group">
+          <label className="nhsuk-label" htmlFor="contactNumber">
+            Enter key contact's mobile number
           </label>
-          <input class="nhsuk-input" id="example" name="example" type="text" style={{width: '25%'}}></input>
+          <input 
+            onChange={(event) => setContactNumber(event.target.value)}
+            className="nhsuk-input"
+            id="contactNumber"
+            name="contactNumber"
+            type="text" 
+            style={{width: '25%'}}></input>
           <br></br>
           <br></br>
           
-          <button class="nhsuk-button" type="submit">
+          <button className="nhsuk-button" type="submit">
             Call Key Contact
           </button>
-
         </div>
       </form>
     </Layout>
