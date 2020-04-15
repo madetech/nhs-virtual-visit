0ff83afe638eaf09d953cfeff6e147d4a1ce658d
Wen Ting Wang <chubberlisk@outlook.com>
2020-04-15 22:06:53 +0100

    Add ErrorMessage component

---
+++ src/components/ErrorMessage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ErrorMessage = ({ children }) => (
+  <span id="call-key-contact-form-mobile-number-error" className="nhsuk-error-message">
+    <span className="nhsuk-u-visually-hidden">Error:</span> Enter a UK mobile number
+  </span>
+);
+
+export default ErrorMessage;
