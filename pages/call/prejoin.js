85d5cece77405723bab10d8f1abf3b5a9e6452ed
Wen Ting Wang <chubberlisk@outlook.com>
2020-04-16 11:48:55 +0100

    Add presence validation to prejoin form for name

--- pages/call/prejoin.js
+++ pages/call/prejoin.js
@@ -1,40 +1,62 @@
-import React, { useCallback, useState } from 'react';
-import { useRouter } from 'next/router';
-import Button from '../../src/components/Button';
-import FormGroup from '../../src/components/FormGroup';
-import { GridRow, GridColumn } from '../../src/components/Grid';
-import Heading from '../../src/components/Heading';
-import Hint from '../../src/components/Hint';
-import Input from '../../src/components/Input';
-import Label from '../../src/components/Label';
-import Layout from '../../src/components/Layout';
-import Lead from '../../src/components/Lead';
+import React, { useCallback, useState } from "react";
+import { useRouter } from "next/router";
+import Button from "../../src/components/Button";
+import FormGroup from "../../src/components/FormGroup";
+import { GridRow, GridColumn } from "../../src/components/Grid";
+import Heading from "../../src/components/Heading";
+import Hint from "../../src/components/Hint";
+import Input from "../../src/components/Input";
+import Label from "../../src/components/Label";
+import Layout from "../../src/components/Layout";
+import Lead from "../../src/components/Lead";
+import ErrorSummary from "../../src/components/ErrorSummary";
 
 const PreJoin = () => {
   const router = useRouter();
-  const [name, setName] = useState('');
-  
+  const nameError = "Enter your name";
+  const [name, setName] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const hasError = (field) =>
+    errors.find((error) => error.id === `${field}-error`);
+
   const onSubmit = useCallback(async (event) => {
     event.preventDefault();
-    router.push(`/call/${router.query.callId}?name=${name}`);
+    const errors = [];
+
+    if (!name) {
+      errors.push({
+        id: "name-error",
+        message: nameError,
+      });
+    }
+
+    setErrors(errors);
+
+    if (errors.length === 0) {
+      router.push(`/call/${router.query.callId}?name=${name}`);
+    }
   });
 
   return (
     <Layout>
       <GridRow>
         <GridColumn width="full">
+          <ErrorSummary errors={errors} />
           <Heading>Joining video call</Heading>
           <Lead>You are about to be connected to a video call.</Lead>
           <GridRow>
-              <GridColumn width="one-half" style={{ padding: '0' }}>
+            <GridColumn width="one-half" style={{ padding: "0" }}>
               <form onSubmit={onSubmit}>
                 <FormGroup>
                   <Label htmlFor="name">Your name</Label>
                   <Hint>We'll show this to everyone in the call.</Hint>
-
                   <Input
+                    id="name"
                     type="text"
                     onChange={(event) => setName(event.target.value)}
+                    hasError={hasError("name")}
+                    errorMessage={nameError}
                     name="name"
                   />
                   <Button className="nhsuk-u-margin-top-5">
@@ -45,10 +67,10 @@ const PreJoin = () => {
             </GridColumn>
           </GridRow>
         </GridColumn>
-        <span style={{ clear: 'both', display: 'block' }}></span>
+        <span style={{ clear: "both", display: "block" }}></span>
       </GridRow>
     </Layout>
-  )
-}
+  );
+};
 
 export default PreJoin;
