ecfcaba3d9a4e4880519e232f1cb0db90fa52474
Wen Ting Wang <chubberlisk@outlook.com>
2020-04-16 11:13:58 +0100

    Update ErrorSummary to dynamically render errors
    Co-authored-by: Tom Davies <tom@t-davies.com>

--- src/components/ErrorSummary/index.js
+++ src/components/ErrorSummary/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 
-const ErrorSummary = ({ children, errors }) => (
-  <div className="nhsuk-error-summary" aria-labelledby="error-summary-title" role="alert">
-    <h2 className="nhsuk-error-summary__title" id="error-summary-title">
-      There is a problem
-    </h2>
-    <div className="nhsuk-error-summary__body">
-      <ul className="nhsuk-list nhsuk-error-summary__list">
-        <li>
-          <a href="#call-key-contact-form-mobile-number">Enter a UK mobile number</a>
-        </li>
-      </ul>
+const ErrorSummary = ({ errors }) => {
+  if (errors.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="nhsuk-error-summary" aria-labelledby="error-summary-title" role="alert">
+      <h2 className="nhsuk-error-summary__title" id="error-summary-title">
+        There is a problem
+      </h2>
+      <div className="nhsuk-error-summary__body">
+        <ul className="nhsuk-list nhsuk-error-summary__list">
+          {
+            errors.map(error => (
+              <li key={error.id}>
+                <a href={`#${error.id}`}>{error.message}</a>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ErrorSummary;
