d887ca5443a7facc67be6f9bc21fda3de3f41a5c
Tom Davies <tom.davies@madetech.com>
2020-04-16 15:46:31 +0100

    Protects the API endpoint too
    We will likely want to refactor this in the future, but it will work
    whilst we only have a single endpoint.

--- src/usecases/verifyToken.js
+++ src/usecases/verifyToken.js
@@ -1,22 +1,29 @@
 import cookie from "cookie";
 
-export default function (callback, { tokens }) {
+export default function (callback, container) {
   return function (context) {
     const { req, res } = context;
-    const redirectToLogin = () => {
-      res.writeHead(302, { Location: "/wards/login" }).end();
-    };
 
-    try {
-      const { token } = cookie.parse(req.headers.cookie);
-
-      if (!token || !tokens.validate(token)) {
-        return redirectToLogin();
-      }
-
+    if (verifyTokenOrRedirect(req, res, container) === true) {
       return callback(context) ?? { props: {} };
-    } catch (error) {
-      return redirectToLogin();
     }
   };
+}
+
+export function verifyTokenOrRedirect(req, res, { tokens }) {
+  const redirectToLogin = () => {
+    res.writeHead(302, { Location: "/wards/login" }).end();
+  };
+
+  try {
+    const { token } = cookie.parse(req.headers.cookie);
+
+    if (!token || !tokens.validate(token)) {
+      return redirectToLogin();
+    }
+
+    return true;
+  } catch (error) {
+    return redirectToLogin();
+  }
 }
