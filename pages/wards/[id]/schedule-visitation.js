c8c68125590f11ac8583ead7c3e36abbd9af6796
George Schena <george@madetech.com>
2020-04-16 18:10:44 +0100

    Add patient name and date time fields

--- pages/wards/[id]/schedule-visitation.js
+++ pages/wards/[id]/schedule-visitation.js
@@ -5,11 +5,13 @@ import { GridRow, GridColumn } from "../../../src/components/Grid";
 import Heading from "../../../src/components/Heading";
 import Hint from "../../../src/components/Hint";
 import Input from "../../../src/components/Input";
+import DateSelect from "../../../src/components/DateSelect";
 import Label from "../../../src/components/Label";
 import Layout from "../../../src/components/Layout";
 import ErrorSummary from "../../../src/components/ErrorSummary";
 import { PhoneNumberUtil, PhoneNumberType } from "google-libphonenumber";
 import fetch from "isomorphic-unfetch";
+import moment from "moment";
 
 const isValidPhoneNumber = (input) => {
   const validator = PhoneNumberUtil.getInstance();
@@ -20,8 +22,22 @@ const isValidPhoneNumber = (input) => {
   );
 };
 
+const isValidName = (input) => {
+  if (input.length !== 0) {
+    return input;
+  }
+};
+
+const isValidDate = ({ year, month, day, hour, min }) => {
+  const parsed = moment([year, month, day, hour, min]);
+  return parsed.isValid() && parsed.isAfter(moment());
+};
+
 const Home = () => {
   const [contactNumber, setContactNumber] = useState("");
+  const [patientName, setPatientName] = useState("");
+  const [callTime, setCallTime] = useState("");
+
   const [errors, setErrors] = useState([]);
 
   const hasError = (field) =>
@@ -38,6 +54,13 @@ const Home = () => {
       });
     };
 
+    const setPatientNameError = (errors) => {
+      errors.push({
+        id: "patient-name-error",
+        message: "Enter a patients name",
+      });
+    };
+
     try {
       if (!isValidPhoneNumber(contactNumber)) {
         setContactNumberError(errors);
@@ -47,16 +70,29 @@ const Home = () => {
       console.log(error.message);
     }
 
+    if (!isValidName(patientName)) {
+      setPatientNameError(errors);
+    }
+
+    if (!isValidDate(callTime)) {
+      errors.push({
+        id: "call-time-error",
+        message: "Enter a valid date",
+      });
+    }
+
     setErrors(errors);
 
     if (errors.length === 0) {
-      const response = await fetch("/api/send-visitation-ready-notification", {
+      const response = await fetch("/api/schedule-visitation", {
         method: "POST",
         headers: {
           "content-type": "application/json",
         },
         body: JSON.stringify({
           contactNumber,
+          patientName,
+          callTime,
         }),
       });
 
@@ -92,12 +128,33 @@ const Home = () => {
                 maxLength={11}
                 hasError={hasError("contact-number")}
                 errorMessage="Enter a UK mobile number"
-                className="nhsuk-u-font-size-32 nhsuk-input--width-10"
+                className="nhsuk-u-font-size-32 nhsuk-input--width-10 nhsuk-u-margin-bottom-5"
                 style={{ padding: "32px 16px!important" }}
                 onChange={(event) => setContactNumber(event.target.value)}
                 name="contact"
               />
-              <br />
+              <Label htmlFor="patient-name">Patient name</Label>
+              <Hint className="nhsuk-u-margin-bottom-2">
+                Enter the name of the patient
+              </Hint>
+              <Hint>This will help us identify who you are calling</Hint>
+              <Input
+                id="patient-name"
+                type="text"
+                hasError={hasError("patient-name")}
+                errorMessage="Enter the patients name"
+                className="nhsuk-u-font-size-32 nhsuk-input--width-10 nhsuk-u-margin-bottom-5"
+                style={{ padding: "32px 16px!important" }}
+                onChange={(event) => setPatientName(event.target.value)}
+                name="patient-name"
+              />
+              <DateSelect
+                onChange={(date) => setCallTime(date)}
+                name="call-time"
+                hasError={hasError("call-time")}
+                errorMessage="Enter a valid date"
+              ></DateSelect>
+              <br></br>
               <Button className="nhsuk-u-margin-top-5">Send invite</Button>
             </FormGroup>
           </form>
