2aa192d290c3de05ca4f74d4195ce61874f225dc
Daniel Burnley <dan@danops.uk>
2020-04-20 15:54:35 +0100

    Whereby Spike

--- pages/api/send-visit-ready-notification.js
+++ pages/api/send-visit-ready-notification.js
@@ -3,6 +3,7 @@ import RandomIdProvider from "../../src/providers/RandomIdProvider";
 import TokenProvider from "../../src/providers/TokenProvider";
 import { verifyTokenOrRedirect } from "../../src/usecases/verifyToken";
 import { NotifyClient } from "notifications-node-client";
+import fetch from "node-fetch";
 
 const ids = new RandomIdProvider();
 const notifier = new ConsoleNotifyProvider();
@@ -10,6 +11,25 @@ const notifier = new ConsoleNotifyProvider();
 const origin = process.env.ORIGIN;
 const apiKey = process.env.API_KEY;
 const templateId = process.env.TEMPLATE_ID;
+
+const whereby = async () => {
+  const response = await fetch("https://api.whereby.dev/v1/meetings", {
+    method: "POST",
+    headers: {
+      authorization: `Bearer ${process.env.WHEREBY_API_KEY}`,
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      startDate: "2020-04-20T10:00:00Z",
+      endDate: "2020-04-21T17:14:20Z",
+    }),
+  });
+
+  let jsonResponse = await response.json();
+  console.log(jsonResponse);
+  let roomUrl = new URL(jsonResponse.roomUrl);
+  return roomUrl.pathname.slice(1);
+};
 
 export default async (req, res) => {
   const { body, method } = req;
@@ -25,8 +45,14 @@ export default async (req, res) => {
     return;
   }
 
-  const { callId, contactNumber } = body;
+  let { callId, contactNumber } = body;
   console.log(callId);
+
+  if (process.env.WHEREBY_SPIKE) {
+    callId = await whereby();
+    console.log(callId);
+  }
+
   const waitingRoomUrl = `${origin}/visitors/waiting-room/${callId}`;
   const visitsUrl = `${origin}/visits/${callId}?name=Ward`;
 
