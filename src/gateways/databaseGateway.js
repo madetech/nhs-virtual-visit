d80fe1a9216a2d8210739fe256f5949eaf03b608
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-04-21 22:35:08 +0100

    Rename config.json to database.json
    config.json was too generic

--- src/gateways/databaseGateway.js
+++ src/gateways/databaseGateway.js
@@ -6,7 +6,7 @@ export default function getDatabase() {
   const Sequelize = require("sequelize");
   const basename = path.basename(__filename);
   const env = process.env.NODE_ENV || "development";
-  const config = require(__dirname + "/../../config/config.json")[env];
+  const config = require(__dirname + "/../../config/database.json")[env];
   const modelsPath = __dirname + "/../../models";
   const db = {};
 
