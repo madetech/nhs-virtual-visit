8d656e9d78cbe6f085529596f6c752c5de1bcce6
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-04-21 19:18:44 +0100

    Update createVisit to use ScheduledCall sequelize model to create

--- src/usecases/createVisit.js
+++ src/usecases/createVisit.js
@@ -2,14 +2,12 @@ const createVisit = ({ getDb }) => async (visit) => {
   const db = getDb();
 
   console.log("Creating visit for ", visit);
-  return await db.one(
-    `INSERT INTO scheduled_calls_table
-      (id, patient_name, recipient_number, call_time, call_id)
-      VALUES (default, $1, $2, $3, $4)
-      RETURNING id
-    `,
-    [visit.patientName, visit.contactNumber, visit.callTime, visit.callId]
-  );
+  return await db.ScheduledCall.create({
+    patientName: visit.patientName,
+    contactNumber: visit.contactNumber,
+    callTime: visit.callTime,
+    callId: visit.callId,
+  });
 };
 
 export default createVisit;
--- src/usecases/createVisit.test.js
+++ src/usecases/createVisit.test.js
@@ -2,11 +2,22 @@ import createVisit from "./createVisit";
 
 describe("createVisit", () => {
   it("creates a visit in the db when valid", async () => {
-    const oneSpy = jest.fn().mockReturnValue(10);
+    const callTime = new Date();
+
     const container = {
       getDb() {
         return {
-          one: oneSpy,
+          ScheduledCall: {
+            create: jest.fn().mockReturnValue([
+              {
+                id: 1,
+                patientName: "Bob Smith",
+                callTime: callTime,
+                contactNumber: "07123456789",
+                callId: "12345",
+              },
+            ]),
+          },
         };
       },
     };
@@ -14,19 +25,21 @@ describe("createVisit", () => {
     const request = {
       patientName: "Bob Smith",
       contactNumber: "07123456789",
-      callTime: new Date(),
-      callId: 12345,
+      callTime: callTime,
+      callId: "12345",
     };
 
-    const resultingId = await createVisit(container)(request);
+    const scheduledCalls = await createVisit(container)(request);
+    expect(scheduledCalls).toHaveLength(1);
 
-    expect(resultingId).toEqual(10);
-
-    expect(oneSpy).toHaveBeenCalledWith(expect.anything(), [
-      request.patientName,
-      request.contactNumber,
-      request.callTime,
-      request.callId,
-    ]);
+    expect(scheduledCalls[0]).toEqual(
+      expect.objectContaining({
+        id: 1,
+        patientName: "Bob Smith",
+        contactNumber: "07123456789",
+        callTime: callTime,
+        callId: "12345",
+      })
+    );
   });
 });
