55be72c2becd686de6d84a620945c6f40f835818
Antony O'Neill <antony.oneill@madetech.com>
2020-04-23 10:11:47 +0100

    Revert "Refactor test to use container"
    This reverts commit 9e2734ef1ded9e70491147b9bc33dfea78af4b38.

--- pageTests/wards/login.test.js
+++ pageTests/wards/login.test.js
@@ -1,15 +1,9 @@
 import { getServerSideProps } from "../../pages/wards/login";
+import TokenProvider from "../../src/providers/TokenProvider";
 
 describe("login", () => {
-  describe("getServerSideProps", () => {
-    const req = {
-      headers: {
-        cookie: "",
-      },
-    };
-
+  describe("prop provider", () => {
     let mockResponse;
-
     beforeEach(() => {
       mockResponse = {
         writeHead: jest.fn().mockReturnValue({ end: () => {} }),
@@ -17,21 +11,28 @@ describe("login", () => {
     });
 
     it("does not redirects not logged in", () => {
-      const container = {
-        getUserIsAuthenticated: () => () => false,
+      const req = {
+        headers: {
+          cookie: "",
+        },
       };
 
-      getServerSideProps({ req, res: mockResponse, container });
+      getServerSideProps({ req, res: mockResponse });
 
       expect(mockResponse.writeHead).not.toHaveBeenCalled();
     });
 
     it("redirects to the ward list page if logged in", () => {
-      const container = {
-        getUserIsAuthenticated: () => () => ({ ward: "my-test-ward" }),
+      process.env.JWT_SIGNING_KEY = "test-key";
+      const tokenProvider = new TokenProvider(process.env.JWT_SIGNING_KEY);
+      const token = tokenProvider.generate("my-test-ward");
+      const req = {
+        headers: {
+          cookie: `token=${token}`,
+        },
       };
 
-      getServerSideProps({ req, res: mockResponse, container });
+      getServerSideProps({ req, res: mockResponse });
 
       expect(mockResponse.writeHead).toHaveBeenCalledWith(
         302,
