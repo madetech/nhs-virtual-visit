24ceb57b984d384aea4eee319a17a1a98b8a5bf8
Wen Ting Wang <chubberlisk@outlook.com>
2020-04-23 16:06:46 +0100

    Add visit date and time when sending initial text message
    We want to display the scheduled date and time separately.

--- pageTests/api/schedule-visit.test.js
+++ pageTests/api/schedule-visit.test.js
@@ -82,6 +82,8 @@ describe("schedule-visit", () => {
         call_time: "5 April 2020, 10.10am",
         ward_name: "Defoe Ward",
         hospital_name: "Northwick Park Hospital",
+        visit_date: "5 April 2020",
+        visit_time: "10.10am",
       },
       null
     );
