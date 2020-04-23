1e31b02cd514792bc4f81cae571ceef063f9ae17
Daniel Burnley <dan@danops.uk>
2020-04-23 12:28:10 +0100

    Separate out confirmation page

--- pages/wards/[id]/schedule-visit.js
+++ pages/wards/[id]/schedule-visit.js
@@ -47,98 +47,19 @@ const isValidTime = ({ hour, minute }) => {
   return { timeIsValid, timeIsInThePast };
 };
 
-const CheckAnswers = ({ onSubmit, patientName, contactNumber, callTime }) => {
-  return (
-    <Layout title="Check your answers before scheduling a visit">
-      <GridRow>
-        <GridColumn width="two-thirds">
-          <form onSubmit={onSubmit}>
-            <Heading>Check your answers before scheduling a visit</Heading>
-            <dl className="nhsuk-summary-list">
-              <div className="nhsuk-summary-list__row">
-                <dt className="nhsuk-summary-list__key">Patient's name</dt>
-                <dd className="nhsuk-summary-list__value">{patientName}</dd>
-                <dd className="nhsuk-summary-list__actions">
-                  <a href="#" onClick={() => setConfirmation(false)}>
-                    Change
-                    <span className="nhsuk-u-visually-hidden">
-                      {" "}
-                      patient's name
-                    </span>
-                  </a>
-                </dd>
-              </div>
-
-              <div className="nhsuk-summary-list__row">
-                <dt className="nhsuk-summary-list__key">
-                  Key contact mobile number
-                </dt>
-                <dd className="nhsuk-summary-list__value">{contactNumber}</dd>
-                <dd className="nhsuk-summary-list__actions">
-                  <a href="#" onClick={() => setConfirmation(false)}>
-                    Change
-                    <span className="nhsuk-u-visually-hidden">
-                      {" "}
-                      key contact number
-                    </span>
-                  </a>
-                </dd>
-              </div>
-
-              <div className="nhsuk-summary-list__row">
-                <dt className="nhsuk-summary-list__key">Date of call</dt>
-                <dd className="nhsuk-summary-list__value">
-                  {moment(callTime).format("D MMMM YYYY")}
-                </dd>
-                <dd className="nhsuk-summary-list__actions">
-                  <a href="#" onClick={() => setConfirmation(false)}>
-                    Change
-                    <span className="nhsuk-u-visually-hidden">
-                      {" "}
-                      date of call
-                    </span>
-                  </a>
-                </dd>
-              </div>
-
-              <div className="nhsuk-summary-list__row">
-                <dt className="nhsuk-summary-list__key">Time of call</dt>
-                <dd className="nhsuk-summary-list__value">
-                  {moment(callTime).format("hh:mma")}
-                </dd>
-                <dd className="nhsuk-summary-list__actions">
-                  <a href="#" onClick={() => setConfirmation(false)}>
-                    Change
-                    <span className="nhsuk-u-visually-hidden">
-                      {" "}
-                      date of call
-                    </span>
-                  </a>
-                </dd>
-              </div>
-            </dl>
-            <h2 className="nhsuk-heading-l">Key contact's mobile number</h2>
-            <Text>
-              We recommend you double check the mobile number of the key contact
-              to ensure we setup the virtual visit with the correct person.
-              We'll send them a text message once you've scheduled the visit.
-            </Text>
-            <Button className="nhsuk-u-margin-top-5">Schedule visit</Button>
-          </form>
-        </GridColumn>
-      </GridRow>
-    </Layout>
+const Home = ({
+  id,
+  initialPatientName,
+  initialContactNumber,
+  initialCallDateTime,
+}) => {
+  const [contactNumber, setContactNumber] = useState(
+    initialContactNumber || ""
   );
-};
-
-const Home = ({ id }) => {
-  const [contactNumber, setContactNumber] = useState("");
-  const [patientName, setPatientName] = useState("");
-  const [callDateTime, setCallDateTime] = useState("");
+  const [patientName, setPatientName] = useState(initialPatientName || "");
+  const [callDateTime, setCallDateTime] = useState(initialCallDateTime || "");
 
   const [errors, setErrors] = useState([]);
-  const [confirmation, setConfirmation] = useState(false);
-  const [success, setSuccess] = useState(false);
 
   const hasError = (field) =>
     errors.find((error) => error.id === `${field}-error`);
@@ -219,49 +140,13 @@ const Home = ({ id }) => {
     }
     setErrors(errors);
 
-    const submitAnswers = async ({ contactNumber, patientName, callTime }) => {
-      const response = await fetch("/api/schedule-visit", {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({
-          contactNumber,
-          patientName,
-          callTime: moment(callTime).toISOString(),
-          callTimeLocal: callTime,
-        }),
-      });
-
-      const { success, err } = await response.json();
-
-      if (success) {
-        Router.push(`/wards/${id}/schedule-success`);
-      } else {
-        console.error(err);
-      }
-    };
-
     if (errors.length === 0) {
-      if (confirmation) {
-        submitAnswers({ contactNumber, patientName, callTime: callDateTime });
-      } else {
-        setConfirmation(true);
-      }
+      Router.push({
+        pathname: `/wards/${id}/schedule-confirmation`,
+        query: { patientName, contactNumber, ...callDateTime },
+      });
     }
   });
-
-  if (confirmation) {
-    return (
-      <CheckAnswers
-        onSubmit={onSubmit}
-        contactNumber={contactNumber}
-        patientName={patientName}
-        callTime={callDateTime}
-      />
-    );
-  }
-
   return (
     <Layout title="Schedule a virtual visit" hasErrors={errors.length != 0}>
       <GridRow>
@@ -333,10 +218,45 @@ const Home = ({ id }) => {
   );
 };
 
+const queryContainsInitialData = (query) => {
+  const initialDataKeys = [
+    "patientName",
+    "contactNumber",
+    "day",
+    "month",
+    "year",
+    "hour",
+    "minute",
+  ];
+
+  const queryKeys = Object.keys(query);
+  return initialDataKeys.every((key) => queryKeys.includes(key));
+};
+
 export const getServerSideProps = verifyToken(
   ({ query }) => {
     const { id } = query;
-    return { props: { id } };
+    let props = { id };
+    if (queryContainsInitialData(query)) {
+      const {
+        patientName,
+        contactNumber,
+        day,
+        month,
+        year,
+        hour,
+        minute,
+      } = query;
+      const callDateTime = { day, month, year, hour, minute };
+
+      props = {
+        ...props,
+        initialPatientName: patientName,
+        initialContactNumber: contactNumber,
+        initialCallDateTime: callDateTime,
+      };
+    }
+    return { props };
   },
   {
     tokens: new TokenProvider(process.env.JWT_SIGNING_KEY),
