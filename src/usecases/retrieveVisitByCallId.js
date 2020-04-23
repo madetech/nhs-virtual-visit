b264ea6021a18ca6f9c964fca30f4d0e9f414079
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-04-23 15:50:36 +0100

    Add a retrieveVisitByCallId usecase
    Retrieves a single scheduled call record by the callId
    
    Co-authored by Joshua-Luke Bevan <joshua.bevan@madetech.com>

---
+++ src/usecases/retrieveVisitByCallId.js
@@ -0,0 +1,34 @@
+const retrieveVisitByCallId = ({ getDb }) => async (callId) => {
+  const db = await getDb();
+  console.log("Retrieving visit for  ", callId);
+  try {
+    const scheduledCalls = await db.any(
+      `SELECT * FROM scheduled_calls_table WHERE call_id = '${callId}' LIMIT 1`
+    );
+
+    const scheduledCall = scheduledCalls[0];
+    console.log("Scheduled call", scheduledCall);
+
+    return {
+      scheduledCall: {
+        id: scheduledCall.id,
+        patientName: scheduledCall.patient_name,
+        recipientNumber: scheduledCall.recipient_number,
+        callTime: scheduledCall.call_time
+          ? scheduledCall.call_time.toISOString()
+          : null,
+        callId: scheduledCall.call_id,
+        provider: scheduledCall.provider,
+      },
+      error: null,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      scheduledCalls: null,
+      error: error.toString(),
+    };
+  }
+};
+
+export default retrieveVisitByCallId;
