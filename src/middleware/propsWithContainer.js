cb67fd08ce5a03c5b9a0781726f15b38f398b5dd
Antony O'Neill <antony.oneill@madetech.com>
2020-04-27 14:33:45 +0100

    Make AppContainer singleton
    Use AppContainer.instance

--- src/middleware/propsWithContainer.js
+++ src/middleware/propsWithContainer.js
@@ -1,6 +1,6 @@
 import AppContainer from "../containers/AppContainer";
 
 export default (callback) => (context) => {
-  context.container = context.container || new AppContainer();
+  context.container = context.container || AppContainer.getInstance();
   return callback(context);
 };
