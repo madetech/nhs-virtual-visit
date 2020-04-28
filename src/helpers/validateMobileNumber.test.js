1c12858c98a87f3b50e9c42fd954708b0b2046b0
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-04-28 13:34:00 +0100

    added tests for validating mobile numbers

---
+++ src/helpers/validateMobileNumber.test.js
@@ -0,0 +1,20 @@
+import validateMobileNumber from "./validateMobileNumber";
+
+describe("validateMobileNumber", () => {
+  it("accepts a UK mobile number", () => {
+    const actual = validateMobileNumber("07123456789");
+    expect(actual).toEqual(true);
+  });
+  it("accepts an Italian mobile number (+)", () => {
+    const actual = validateMobileNumber("+393123456789");
+    expect(actual).toEqual(true);
+  });
+  it("accepts an Italian mobile number (00)", () => {
+    const actual = validateMobileNumber("00393123456789");
+    expect(actual).toEqual(true);
+  });
+  it("rejects a UK landline number", () => {
+    const actual = validateMobileNumber("02045678901");
+    expect(actual).toEqual(false);
+  });
+});
