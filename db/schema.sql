59245d59746e0aa363a4cdc44c03005889bcae0e
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-04-29 11:06:26 +0100

    Set a NOT NULL constraint on scheduled_calls_table.ward_id

--- db/schema.sql
+++ db/schema.sql
@@ -28,7 +28,7 @@ CREATE TABLE public.scheduled_calls_table (
     call_id text,
     recipient_name character varying(255),
     provider character varying(255) DEFAULT 'jitsi'::character varying NOT NULL,
-    ward_id integer
+    ward_id integer NOT NULL
 );
 
 
