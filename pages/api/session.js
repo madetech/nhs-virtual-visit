a8394ad3b7930d795bd01c10b1c867a3968f7f25
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-04-29 19:00:26 +0100

    Add DELETE action to /api/session for logging out
    Sets an expired empty token which will be cleared immediately

--- pages/api/session.js
+++ pages/api/session.js
@@ -2,30 +2,39 @@ import withContainer from "../../src/middleware/withContainer";
 
 export default withContainer(
   async ({ body: { code }, method }, res, { container }) => {
-    if (method !== "POST") {
+    if (method === "POST") {
+      const verifyWardCode = container.getVerifyWardCode();
+      const verifyWardCodeResponse = await verifyWardCode(code);
+
+      if (!verifyWardCodeResponse.validWardCode) {
+        res.statusCode = 401;
+        res.end();
+        return;
+      }
+
+      const tokens = container.getTokenProvider();
+      const token = tokens.generate(code);
+      const expiryHours = 2;
+      let expiry = new Date();
+      expiry.setTime(expiry.getTime() + expiryHours * 60 * 60 * 1000);
+
+      res.writeHead(201, {
+        "Set-Cookie": `token=${token}; httpOnly; path=/; expires=${expiry}`,
+      });
+      res.end(JSON.stringify({ wardId: code }));
+      return;
+    } else if (method === "DELETE") {
+      const expired = new Date(0);
+
+      res.writeHead(201, {
+        "Set-Cookie": `token=''; httpOnly; path=/; expires=${expired}`,
+      });
+      res.end(JSON.stringify({ wardId: code }));
+      return;
+    } else {
       res.statusCode = 406;
       res.end();
       return;
     }
-
-    const verifyWardCode = container.getVerifyWardCode();
-    const verifyWardCodeResponse = await verifyWardCode(code);
-
-    if (!verifyWardCodeResponse.validWardCode) {
-      res.statusCode = 401;
-      res.end();
-      return;
-    }
-
-    const tokens = container.getTokenProvider();
-    const token = tokens.generate(code);
-    const expiryHours = 2;
-    let expiry = new Date();
-    expiry.setTime(expiry.getTime() + expiryHours * 60 * 60 * 1000);
-
-    res.writeHead(201, {
-      "Set-Cookie": `token=${token}; httpOnly; path=/; expires=${expiry}`,
-    });
-    res.end(JSON.stringify({ wardId: code }));
   }
 );
