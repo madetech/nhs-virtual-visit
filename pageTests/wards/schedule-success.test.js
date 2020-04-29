317e2723fac4f9545f5cbdf378b7e2c81beff6cb
Antony O'Neill <antony.oneill@madetech.com>
2020-04-29 14:09:26 +0100

    Cover protecting schedule-success

---
+++ pageTests/wards/schedule-success.test.js
@@ -0,0 +1,27 @@
+import { getServerSideProps } from "../../pages/wards/[id]/schedule-success";
+
+describe("wards/schedule-success", () => {
+  const anonymousReq = {
+    headers: {
+      cookie: "",
+    },
+  };
+
+  let res;
+
+  beforeEach(() => {
+    res = {
+      writeHead: jest.fn().mockReturnValue({ end: () => {} }),
+    };
+  });
+
+  describe("getServerSideProps", () => {
+    it("redirects to login page if not authenticated", async () => {
+      await getServerSideProps({ req: anonymousReq, res });
+
+      expect(res.writeHead).toHaveBeenCalledWith(302, {
+        Location: "/wards/login",
+      });
+    });
+  });
+});
