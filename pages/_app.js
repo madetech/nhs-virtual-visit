5c9bb1d259cc71bea6968efbfc491c5df70da410
Antony O'Neill <antony.oneill@madetech.com>
2020-04-30 10:46:53 +0100

    Prevent robots indexing and following

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,11 @@
 import "../src/global.scss";
+import Head from "next/head";
 
-export default ({ Component, pageProps }) => <Component {...pageProps} />;
+export default ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <meta name="robots" content="noindex, nofollow" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
