05159252dfb30f9abbcde8c80ecf50402a486259
Wen Ting Wang <chubberlisk@outlook.com>
2020-04-30 13:08:51 +0100

    Update formatTime to take in a time format

--- src/helpers/formatTime.test.js
+++ src/helpers/formatTime.test.js
@@ -4,4 +4,8 @@ describe("formatTime", () => {
   it("returns the formatted time", () => {
     expect(formatTime("2020-04-05T10:10:10")).toEqual("10:10am");
   });
+
+  it("returns the formatted time with time format provided", () => {
+    expect(formatTime("2020-04-05T17:10:10", "HH:mm")).toEqual("17:10");
+  });
 });
