5c5e8cfb6eda21c6c1c25d1fc4a7fc1d31e51289
Steve Knight <steve.knight@madetech.com>
2020-04-30 17:46:34 +0100

    Protect visits/[id] with callPassword

--- pageTests/visits/[id].test.js
+++ pageTests/visits/[id].test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Call from "../../pages/visits/[id]";
+import Call, { getServerSideProps } from "../../pages/visits/[id]";
 
 jest.mock("../../src/hooks/useScript", () => ({
   __esModule: true,
@@ -46,6 +46,80 @@ describe("call", () => {
       render(<Call />);
 
       expect(spy).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("getServerSideProps", () => {
+    let getVerifyCallPasswordSpy;
+    let getUserIsAuthenticatedSpy;
+    let container;
+    let res;
+    const req = {
+      headers: {
+        cookie: "",
+      },
+    };
+
+    beforeEach(() => {
+      getVerifyCallPasswordSpy = jest.fn((callId, password) => ({
+        validCallPassword: password === "securePassword",
+        error: null,
+      }));
+      getUserIsAuthenticatedSpy = jest.fn().mockReturnValue(false);
+      container = {
+        getVerifyCallPassword: () => getVerifyCallPasswordSpy,
+        getUserIsAuthenticated: () => getUserIsAuthenticatedSpy,
+        getRetrieveVisitByCallId: () => () => ({
+          scheduledCall: { id: 1, provider: "whereby" },
+        }),
+      };
+      res = {
+        writeHead: jest.fn().mockReturnValue({ end: () => {} }),
+        end: jest.fn(),
+      };
+    });
+    it("returns callId when the password is valid", async () => {
+      const { props } = await getServerSideProps({
+        query: {
+          callPassword: "securePassword",
+          id: 1,
+        },
+        container,
+        res,
+        req,
+      });
+      expect(getVerifyCallPasswordSpy).toHaveBeenCalledWith(
+        1,
+        "securePassword"
+      );
+      expect(props.callId).toEqual(1);
+    });
+    it("returns callId when the password is invalid, but the user is authenticated", async () => {
+      getUserIsAuthenticatedSpy.mockReturnValueOnce(true);
+      const { props } = await getServerSideProps({
+        query: {
+          callPassword: undefined,
+          id: 1,
+        },
+        container,
+        res,
+        req,
+      });
+      expect(getVerifyCallPasswordSpy).toHaveBeenCalledWith(1, undefined);
+      expect(props.callId).toEqual(1);
+    });
+    it("redirects when password is invalid, and the user is unauthenticated", async () => {
+      await getServerSideProps({
+        query: {
+          callPassword: "fakeCode",
+          id: 1,
+        },
+        container,
+        res,
+        req,
+      });
+      expect(getVerifyCallPasswordSpy).toHaveBeenCalledWith(1, "fakeCode");
+      expect(res.writeHead).toHaveBeenCalledWith(307, { Location: "/error" });
     });
   });
 });
