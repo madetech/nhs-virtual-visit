79ff0a3ae371647402cbe76003b0621aa52a5215
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-04-30 09:55:02 +0100

    Move POST session tests to their own describe block
    Will help distinguish them from the DELETE tests

--- pageTests/api/session.test.js
+++ pageTests/api/session.test.js
@@ -13,60 +13,62 @@ describe("api/session", () => {
     });
   });
 
-  describe("Given an invalid code", () => {
-    it("Returns a 401", async () => {
-      const invalidRequest = {
-        method: "POST",
-        body: {
-          code: "WOOF",
-        },
-      };
+  describe("POST", () => {
+    describe("Given an invalid code", () => {
+      it("Returns a 401", async () => {
+        const invalidRequest = {
+          method: "POST",
+          body: {
+            code: "WOOF",
+          },
+        };
 
-      const response = { statusCode: 0, end: jest.fn() };
-      const verifyWardCodeSpy = jest.fn(async () => ({ validWardCode: false }));
-      const container = {
-        getVerifyWardCode: () => verifyWardCodeSpy,
-      };
+        const response = { statusCode: 0, end: jest.fn() };
+        const verifyWardCodeSpy = jest.fn(async () => ({ validWardCode: false }));
+        const container = {
+          getVerifyWardCode: () => verifyWardCodeSpy,
+        };
 
-      await session(invalidRequest, response, { container });
+        await session(invalidRequest, response, { container });
 
-      expect(verifyWardCodeSpy).toHaveBeenCalledWith("WOOF");
-      expect(response.statusCode).toEqual(401);
-      expect(response.end).toHaveBeenCalled();
+        expect(verifyWardCodeSpy).toHaveBeenCalledWith("WOOF");
+        expect(response.statusCode).toEqual(401);
+        expect(response.end).toHaveBeenCalled();
+      });
     });
-  });
 
-  describe("Given a valid code", () => {
-    it("Returns the generated token in the response", async () => {
-      const validRequest = {
-        method: "POST",
-        body: {
-          code: "MEOW",
-        },
-      };
+    describe("Given a valid code", () => {
+      it("Returns the generated token in the response", async () => {
+        const validRequest = {
+          method: "POST",
+          body: {
+            code: "MEOW",
+          },
+        };
 
-      const response = {
-        writeHead: jest.fn(),
-        end: jest.fn(),
-      };
+        const response = {
+          writeHead: jest.fn(),
+          end: jest.fn(),
+        };
 
-      const verifyWardCodeSpy = jest.fn(async () => ({ validWardCode: true }));
-      const container = {
-        getTokenProvider: jest.fn(() => ({
-          generate: jest.fn(() => "generatedToken"),
-        })),
-        getVerifyWardCode: () => verifyWardCodeSpy,
-      };
+        const verifyWardCodeSpy = jest.fn(async () => ({ validWardCode: true }));
+        const container = {
+          getTokenProvider: jest.fn(() => ({
+            generate: jest.fn(() => "generatedToken"),
+          })),
+          getVerifyWardCode: () => verifyWardCodeSpy,
+        };
 
-      await session(validRequest, response, { container });
+        await session(validRequest, response, { container });
 
-      expect(verifyWardCodeSpy).toHaveBeenCalledWith("MEOW");
-      expect(response.writeHead).toHaveBeenCalledWith(
-        201,
-        expect.objectContaining({
-          "Set-Cookie": expect.stringContaining("generatedToken"),
-        })
-      );
+        expect(verifyWardCodeSpy).toHaveBeenCalledWith("MEOW");
+        expect(response.writeHead).toHaveBeenCalledWith(
+          201,
+          expect.objectContaining({
+            "Set-Cookie": expect.stringContaining("generatedToken"),
+          })
+        );
+      });
     });
   });
 });
