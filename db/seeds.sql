51b112cfdec067e611f72f5290abe58f0d5b3679
Daniel Burnley <dan@danops.uk>
2020-05-01 10:05:52 +0100

    Add database seeds for test wards

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO wards (name, hospital_name, code) VALUES ('Test Ward One', 'Test Hospital', 'TEST1');
+INSERT INTO wards (name, hospital_name, code) VALUES ('Test Ward Two', 'Test Hospital', 'TEST2');
