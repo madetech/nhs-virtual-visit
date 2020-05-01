aa27f7cbaf7e5931f167b394fbc9f554b98d2425
Wen Ting Wang <chubberlisk@outlook.com>
2020-05-01 08:40:09 +0100

    Remove page redirections and their tests

--- pages/wards/[id]/cancel-visit-confirmation.js
+++
@@ -1,7 +0,0 @@
-import redirectWithQuery from "../../../src/helpers/redirectWithQuery";
-
-export default () => null;
-
-export const getServerSideProps = redirectWithQuery(
-  "/wards/cancel-visit-confirmation"
-);
--- pages/wards/[id]/schedule-visit.js
+++
@@ -1,5 +0,0 @@
-import redirectWithQuery from "../../../src/helpers/redirectWithQuery";
-
-export default () => null;
-
-export const getServerSideProps = redirectWithQuery("/wards/book-a-visit");
