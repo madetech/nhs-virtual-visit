3be5463411b15408336f224f2f9db5dd7e41932a
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-05-04 14:35:44 +0100

    Added an intial set of tests and functionality for dates

---
+++ src/helpers/validateDateAndTime.test.js
@@ -0,0 +1,23 @@
+import moment from "moment";
+import validateDateAndTime from "./validateDateAndTime";
+
+describe("validateDateAndTime", () => {
+  it("accepts a valid date and time", () => {
+    const { isValid, _ } = validateDateAndTime(new moment());
+    expect(isValid).toBe(true);
+  });
+  it("rejects an invalid date and time", () => {
+    const { isValid, errorMessage } = validateDateAndTime("");
+    expect(isValid).toEqual(false);
+    expect(errorMessage).toEqual("Please enter a valid date and time");
+  });
+  it("rejects an invalid date for being too far in the future", () => {
+    const currentMoment = new moment();
+    const momentInTheFuture = currentMoment.add(3, "years");
+    const { isValid, errorMessage } = validateDateAndTime(momentInTheFuture);
+    expect(isValid).toEqual(false);
+    expect(errorMessage).toEqual(
+      "Please enter a time within the next three years"
+    );
+  });
+});
