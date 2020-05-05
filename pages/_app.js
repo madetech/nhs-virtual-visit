121db53a70b1ee2c2b795b99ca2b78e3060c561b
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-05-05 13:53:43 +0100

    Add sentry application logging
    Following https://github.com/zeit/next.js/tree/canary/examples/with-sentry-simple

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,31 @@
-import "../src/global.scss";
+import React from "react";
+import App from "next/app";
+import * as Sentry from "@sentry/node";
 import Head from "next/head";
 
-export default ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <meta name="robots" content="noindex, nofollow" />
-    </Head>
-    <Component {...pageProps} />
-  </>
-);
+Sentry.init({
+  enabled: process.env.ENABLE_SENTRY === "yes",
+  environment: process.env.NODE_ENV,
+  dsn: process.env.SENTRY_DSN,
+});
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    // Workaround for https://github.com/zeit/next.js/issues/8592
+    const { err } = this.props;
+    const modifiedPageProps = { ...pageProps, err };
+
+    return (
+      <>
+        <Head>
+          <meta name="robots" content="noindex, nofollow" />
+        </Head>
+        <Component {...modifiedPageProps} />
+      </>
+    );
+  }
+}
+
+export default MyApp;
