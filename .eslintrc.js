e682bdbe4ea38f0734f4668abf2264b8d93b3bf7
Antony O'Neill <antony.oneill@madetech.com>
2020-05-05 18:23:36 +0100

    Configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": 0,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
