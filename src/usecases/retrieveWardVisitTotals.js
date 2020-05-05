05a0d775850cdc2363b4bfbc80037a2fd5980ee3
Daniel Burnley <dan@danops.uk>
2020-05-05 14:05:47 +0100

    Add usecase for retreiveing ward visit totals

---
+++ src/usecases/retrieveWardVisitTotals.js
@@ -0,0 +1,22 @@
+const calculateTotalNumberOfVisits = (visitsByWard) =>
+  visitsByWard.reduce((total, { visits }) => total + visits, 0);
+
+const getVisitsByWard = async (db) => {
+  const queryResult = await db.any(
+    "SELECT wards.hospital_name, wards.name, SUM(totals.total) AS total_visits FROM ward_visit_totals AS totals JOIN wards ON wards.id = totals.ward_id GROUP BY(wards.name)"
+  );
+
+  return queryResult.map(({ hospital_name, total_visits, name }) => ({
+    hospitalName: hospital_name,
+    name,
+    visits: parseInt(total_visits),
+  }));
+};
+
+export default ({ getDb }) => async () => {
+  const db = await getDb();
+  const visitTotals = await getVisitsByWard(db);
+  const overallTotal = calculateTotalNumberOfVisits(visitTotals);
+
+  return { total: overallTotal, byWard: visitTotals };
+};
