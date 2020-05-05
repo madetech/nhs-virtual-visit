538b9fd2b61df945057e218b49672e9bb1396957
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-05-05 10:46:31 +0100

    Add a custom 404 page
    Resolves the following error:
    https://github.com/zeit/next.js/blob/master/errors/custom-error-no-custom-404.md

---
+++ pages/404.js
@@ -0,0 +1,20 @@
+import Layout from "../src/components/Layout";
+import { GridRow, GridColumn } from "../src/components/Grid";
+import Heading from "../src/components/Heading";
+import Text from "../src/components/Text";
+
+export default function Custom404() {
+  return (
+    <Layout title="We can’t find the page you’re looking for">
+      <GridRow>
+        <GridColumn width="two-thirds">
+          <Heading>We can’t find the page you’re looking for</Heading>
+          <Text>If you typed the web address, check it is correct.</Text>
+          <Text>
+            If you pasted the web address, check you copied the entire address.
+          </Text>
+        </GridColumn>
+      </GridRow>
+    </Layout>
+  );
+}
