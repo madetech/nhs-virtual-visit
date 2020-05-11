3b43a8227041fc82008bae7a55f6612252b99379
Wen Ting Wang <chubberlisk@outlook.com>
2020-05-11 15:03:00 +0100

    Add edit a ward page
    Co-authored-by: Joshua-Luke Bevan <joshua.bevan@madetech.com>

---
+++ pages/admin/edit-a-ward.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import Error from "next/error";
+import { GridRow, GridColumn } from "../../src/components/Grid";
+import Layout from "../../src/components/Layout";
+import verifyAdminToken from "../../src/usecases/verifyAdminToken";
+import TokenProvider from "../../src/providers/TokenProvider";
+import propsWithContainer from "../../src/middleware/propsWithContainer";
+import EditWardForm from "../../src/components/EditWardForm";
+
+const EditAWard = ({ error, name, hospitalName }) => {
+  if (error) {
+    return <Error />;
+  }
+
+  const [errors, setErrors] = useState([]);
+
+  return (
+    <Layout
+      title="Edit a ward"
+      hasErrors={errors.length != 0}
+      renderLogout={true}
+    >
+      <GridRow>
+        <GridColumn width="two-thirds">
+          <EditWardForm
+            errors={errors}
+            setErrors={setErrors}
+            initialName={name}
+            initialHospitalName={hospitalName}
+          />
+        </GridColumn>
+      </GridRow>
+    </Layout>
+  );
+};
+
+export const getServerSideProps = propsWithContainer(
+  verifyAdminToken(
+    async ({ container, query }) => {
+      const getWardById = container.getWardById();
+      const { ward, error } = await getWardById(query.wardId);
+
+      return {
+        props: {
+          error: error,
+          name: ward.name,
+          hospitalName: ward.hospitalName,
+        },
+      };
+    },
+    {
+      tokens: new TokenProvider(process.env.JWT_SIGNING_KEY),
+    }
+  )
+);
+
+export default EditAWard;
