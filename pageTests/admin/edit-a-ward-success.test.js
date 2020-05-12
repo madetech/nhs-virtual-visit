dddb8909c236761f276537ab45fbce0367f40c3e
Wen Ting Wang <chubberlisk@outlook.com>
2020-05-12 10:10:24 +0100

    Add an edit a ward success page
    Co-authored-by: Joshua-Luke Bevan <joshua.bevan@madetech.com>

---
+++ pageTests/admin/edit-a-ward-success.test.js
@@ -0,0 +1,93 @@
+import { getServerSideProps } from "../../pages/admin/edit-a-ward-success";
+
+// TODO: This needs to be moved once the verifyToken logic is in the container..
+jest.mock("../../src/usecases/adminIsAuthenticated", () => () => (token) =>
+  token && { admin: true }
+);
+
+const authenticatedReq = {
+  headers: {
+    cookie: "token=123",
+  },
+};
+
+describe("/admin/edit-a-ward-success", () => {
+  const anonymousReq = {
+    headers: {
+      cookie: "",
+    },
+  };
+
+  let res;
+
+  beforeEach(() => {
+    res = {
+      writeHead: jest.fn().mockReturnValue({ end: () => {} }),
+    };
+  });
+
+  describe("getServerSideProps", () => {
+    it("redirects to login page if not authenticated", async () => {
+      await getServerSideProps({ req: anonymousReq, res });
+
+      expect(res.writeHead).toHaveBeenCalledWith(302, {
+        Location: "/wards/login",
+      });
+    });
+
+    describe("with wardId parameter", () => {
+      it("retrieves a ward by the wardId parameter", async () => {
+        const retrieveWardByIdSpy = jest.fn().mockReturnValue({
+          ward: {
+            id: 1,
+            name: "Defoe Ward",
+            hospitalName: "Northwick Park Hospital",
+          },
+          error: null,
+        });
+
+        const container = {
+          getWardById: () => retrieveWardByIdSpy,
+        };
+
+        await getServerSideProps({
+          req: authenticatedReq,
+          res,
+          query: {
+            wardId: "ward ID",
+          },
+          container,
+        });
+
+        expect(retrieveWardByIdSpy).toHaveBeenCalledWith("ward ID");
+      });
+
+      it("set a ward prop based on the retrieved ward", async () => {
+        const retrieveWardByIdSpy = jest.fn().mockReturnValue({
+          ward: {
+            id: 1,
+            name: "Defoe Ward",
+            hospitalName: "Northwick Park Hospital",
+          },
+          error: null,
+        });
+
+        const container = {
+          getWardById: () => retrieveWardByIdSpy,
+        };
+
+        const { props } = await getServerSideProps({
+          req: authenticatedReq,
+          res,
+          query: {
+            wardId: "1",
+          },
+          container,
+        });
+
+        expect(props.name).toEqual("Defoe Ward");
+        expect(props.hospitalName).toEqual("Northwick Park Hospital");
+      });
+    });
+  });
+});
