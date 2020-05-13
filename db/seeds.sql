32b335ad2fb8f94eced27f9509bae17da08e1b92
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-05-13 11:49:10 +0100

    Added hospitals table
    Co-authored-by: Jiv Dhaliwal <jiv.dhaliwal@madetech.com>

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
 INSERT INTO trusts (name, admin_code) VALUES ('Test Trust', 'admin');
 INSERT INTO wards (name, hospital_name, code, trust_id) VALUES ('Test Ward One', 'Test Hospital', 'TEST1', (SELECT id FROM trusts WHERE name='Test Trust'));
 INSERT INTO wards (name, hospital_name, code, trust_id) VALUES ('Test Ward Two', 'Test Hospital', 'TEST2', (SELECT id FROM trusts WHERE name='Test Trust'));
+INSERT INTO hospitals (name, trust_id) VALUES ('Test Hospital', (SELECT id FROM trusts WHERE name='Test Trust'));
