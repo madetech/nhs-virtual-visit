19474d390049056df172d6ff45375294f432a2bf
Stephen Thomson <stephen.thomson@madetech.com>
2020-05-13 10:35:31 +0100

    Updated seeds.sql to have expected code
    Co-authored-by: Wen Ting Wang <chubberlisk@outlook.com>

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO trusts (name, admin_code) VALUES ('Test Trust', 'TRUST1');
+INSERT INTO trusts (name, admin_code) VALUES ('Test Trust', 'admin');
 INSERT INTO wards (name, hospital_name, code, trust_id) VALUES ('Test Ward One', 'Test Hospital', 'TEST1', (SELECT id FROM trusts WHERE name='Test Trust'));
 INSERT INTO wards (name, hospital_name, code, trust_id) VALUES ('Test Ward Two', 'Test Hospital', 'TEST2', (SELECT id FROM trusts WHERE name='Test Trust'));
