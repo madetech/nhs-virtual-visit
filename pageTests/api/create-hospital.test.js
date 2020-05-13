742525db32613c35e1463bdb645350ec4d860a16
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-05-13 14:42:24 +0100

    Add a create-hospital api endpoint
    Creates hospital records given a name and trust ID
    
    Co-authored-by: Joshua-Luke Bevan <joshua.bevan@madetech.com>

---
+++ pageTests/api/create-hospital.test.js
@@ -0,0 +1,201 @@
+import createHospital from "../../pages/api/create-hospital";
+
+jest.mock("node-fetch");
+
+describe("create-hospital", () => {
+  let validRequest;
+  let response;
+  let container;
+
+  beforeEach(() => {
+    validRequest = {
+      method: "POST",
+      body: {
+        name: "Yugi Muto Hospital",
+        trustId: "1",
+      },
+      headers: {
+        cookie: "token=valid.token.value",
+      },
+    };
+    response = {
+      status: jest.fn(),
+      setHeader: jest.fn(),
+      send: jest.fn(),
+      end: jest.fn(),
+      body: jest.fn(),
+    };
+    container = {
+      getCreateHospital: jest.fn().mockReturnValue(() => {
+        return { hospitalId: 1, error: null };
+      }),
+      getAdminIsAuthenticated: jest
+        .fn()
+        .mockReturnValue((cookie) => cookie === "token=valid.token.value"),
+    };
+  });
+
+  it("returns 405 if not POST method", async () => {
+    validRequest.method = "GET";
+
+    await createHospital(validRequest, response, {
+      container: container,
+    });
+
+    expect(response.status).toHaveBeenCalledWith(405);
+  });
+
+  it("returns a 401 if no token provided", async () => {
+    const adminIsAuthenticatedSpy = jest.fn().mockReturnValue(false);
+
+    await createHospital(
+      {
+        method: "POST",
+        body: {},
+        headers: {},
+      },
+      response,
+      {
+        container: {
+          ...container,
+          getAdminIsAuthenticated: () => adminIsAuthenticatedSpy,
+        },
+      }
+    );
+
+    expect(response.status).toHaveBeenCalledWith(401);
+    expect(adminIsAuthenticatedSpy).toHaveBeenCalled();
+  });
+
+  it("creates a new ward if valid", async () => {
+    const createHospitalSpy = jest
+      .fn()
+      .mockReturnValue({ hospitalId: 123, error: null });
+
+    await createHospital(validRequest, response, {
+      container: {
+        ...container,
+        getCreateHospital: () => createHospitalSpy,
+      },
+    });
+
+    expect(response.status).toHaveBeenCalledWith(201);
+    expect(response.end).toHaveBeenCalledWith(
+      JSON.stringify({ hospitalId: 123 })
+    );
+    expect(createHospitalSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        name: "Yugi Muto Hospital",
+        trustId: "1",
+      })
+    );
+  });
+
+  it("returns a 400 status if errors", async () => {
+    const createHospitalStub = jest
+      .fn()
+      .mockReturnValue({ hospitalId: 123, error: "Error message" });
+
+    await createHospital(validRequest, response, {
+      container: {
+        ...container,
+        getCreateHospital: () => createHospitalStub,
+      },
+    });
+
+    expect(response.status).toHaveBeenCalledWith(400);
+  });
+
+  it("returns a 400 if the name is an empty string", async () => {
+    const invalidRequest = {
+      method: "POST",
+      body: {
+        name: "",
+        hospitalName: "Yugi Muto Hospital",
+      },
+      headers: {
+        cookie: "token=valid.token.value",
+      },
+    };
+
+    await createHospital(invalidRequest, response, {
+      container: {
+        ...container,
+      },
+    });
+
+    expect(response.status).toHaveBeenCalledWith(400);
+    expect(response.end).toHaveBeenCalledWith(
+      JSON.stringify({ err: "name must be present" })
+    );
+  });
+
+  it("returns a 400 if the name is not provided", async () => {
+    const invalidRequest = {
+      method: "POST",
+      body: {
+        hospitalName: "Yugi Muto Hospital",
+      },
+      headers: {
+        cookie: "token=valid.token.value",
+      },
+    };
+
+    await createHospital(invalidRequest, response, {
+      container: {
+        ...container,
+      },
+    });
+
+    expect(response.status).toHaveBeenCalledWith(400);
+    expect(response.end).toHaveBeenCalledWith(
+      JSON.stringify({ err: "name must be present" })
+    );
+  });
+
+  it("returns a 400 if the hospital name is an empty string", async () => {
+    const invalidRequest = {
+      method: "POST",
+      body: {
+        name: "",
+      },
+      headers: {
+        cookie: "token=valid.token.value",
+      },
+    };
+
+    await createHospital(invalidRequest, response, {
+      container: {
+        ...container,
+      },
+    });
+
+    expect(response.status).toHaveBeenCalledWith(400);
+    expect(response.end).toHaveBeenCalledWith(
+      JSON.stringify({ err: "name must be present" })
+    );
+  });
+
+  it("returns a 400 if the trust ID is not provided", async () => {
+    const invalidRequest = {
+      method: "POST",
+      body: {
+        name: "Seto Kaiba Ward",
+      },
+      headers: {
+        cookie: "token=valid.token.value",
+      },
+    };
+
+    await createHospital(invalidRequest, response, {
+      container: {
+        ...container,
+      },
+    });
+
+    expect(response.status).toHaveBeenCalledWith(400);
+    expect(response.end).toHaveBeenCalledWith(
+      JSON.stringify({ err: "trust ID must be present" })
+    );
+  });
+});
