11b8b3a786e5afa9270e6324c8cd4e3ba945d446
Stephen Thomson <stephen.thomson@madetech.com>
2020-05-15 10:24:35 +0100

    Add details html to visitpanel list and styling
    Co-authored-by: Wen Ting Wang <chubberlisk@outlook.com>

--- src/components/VisitsPanelList/index.js
+++ src/components/VisitsPanelList/index.js
@@ -1,71 +1,98 @@
 import React from "react";
 import Router from "next/router";
+import "./styles.scss";
 import formatDateAndTime from "../../helpers/formatDateAndTime";
 
-const Visits = ({ visits }) => (
-  <div className="nhsuk-table-responsive">
-    <table className="nhsuk-table">
-      <caption className="nhsuk-table__caption">List of ward visits</caption>
-      <thead className="nhsuk-table__head">
-        <tr className="nhsuk-table__row">
-          <th className="nhsuk-table__header" scope="col">
-            Patient name
-          </th>
-          <th className="nhsuk-table__header" scope="col">
-            Key contact name
-          </th>
-          <th className="nhsuk-table__header" scope="col">
-            Key contact mobile number
-          </th>
-          <th className="nhsuk-table__header" scope="col">
-            Call time
-          </th>
-          <th className="nhsuk-table__header" colSpan="2" scope="col"></th>
-        </tr>
-      </thead>
-      <tbody className="nhsuk-table__body">
-        {visits.map((visit) => (
-          <tr key={visit.callId} className="nhsuk-table__row">
-            <td className="nhsuk-table__cell">{visit.patientName}</td>
-            <td className="nhsuk-table__cell">{visit.recipientName}</td>
-            <td className="nhsuk-table__cell">{visit.recipientNumber}</td>
-            <td className="nhsuk-table__cell">
-              {formatDateAndTime(visit.callTime, "HH:mm")}
-            </td>
-            <td className="nhsuk-table__cell">
-              <button
-                className="nhsuk-button"
-                type="submit"
-                onClick={() => {
-                  const callId = visit.callId;
-                  Router.push({
-                    pathname: `/wards/visit-start`,
-                    query: { callId },
-                  });
-                }}
-              >
-                Start
-              </button>
-            </td>
-            <td className="nhsuk-table__cell">
-              <button
-                className="nhsuk-button nhsuk-button--secondary"
-                onClick={() => {
-                  const callId = visit.callId;
-                  Router.push({
-                    pathname: `/wards/cancel-visit-confirmation`,
-                    query: { callId },
-                  });
-                }}
-              >
-                Cancel
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+const Visits = ({visits}) => (
+  <div className="nhsuk-list-panel nhsuk-u-margin-0">
+    <h3 className="nhsuk-list-panel__label" style={{fontSize: "1.5rem"}} id="A">Next</h3>
+    <ul className="nhsuk-list-panel__list nhsuk-list-panel__list--with-label">
+    {visits.map((visit) => (
+      <li className="nhsuk-list-panel__item">
+        <div className="app-visit-card">
+          <div className="app-visit-card-body">
+            <details className="nhsuk-details nhsuk-u-margin-0" nhsuk-polyfilled="true" id="nhsuk-details0">
+              <summary className="nhsuk-details__summary" role="button" aria-controls="nhsuk-details__text0"
+                       tabIndex="0" aria-expanded="false">
+                        <span className="nhsuk-details__summary-text">
+                          11:45 - Steven Universe
+                        </span>
+              </summary>
+              <div className="nhsuk-details__text" id="nhsuk-details__text0" aria-hidden="true">
+                <h4>Visitor</h4>
+
+                <dl className="nhsuk-summary-list">
+
+                  <div className="nhsuk-summary-list__row">
+                    <dt className="nhsuk-summary-list__key">
+                      Name
+                    </dt>
+                    <dd className="nhsuk-summary-list__value">
+                      Pink Diamond
+                    </dd>
+
+                  </div>
+
+                  <div className="nhsuk-summary-list__row">
+                    <dt className="nhsuk-summary-list__key">
+                      Mobile number
+                    </dt>
+                    <dd className="nhsuk-summary-list__value">
+                      0712345679
+                    </dd>
+
+                  </div>
+
+                </dl>
+
+
+                <h4>Virtual visit</h4>
+
+                <dl className="nhsuk-summary-list">
+
+                  <div className="nhsuk-summary-list__row">
+                    <dt className="nhsuk-summary-list__key">
+                      Date
+                    </dt>
+                    <dd className="nhsuk-summary-list__value">
+                      25 April 2020
+                    </dd>
+
+                  </div>
+
+                  <div className="nhsuk-summary-list__row">
+                    <dt className="nhsuk-summary-list__key">
+                      Time
+                    </dt>
+                    <dd className="nhsuk-summary-list__value">
+                      11:45
+                    </dd>
+
+                  </div>
+
+                </dl>
+
+
+                <a href="/wards/visits/start"
+                   className="nhsuk-button nhsuk-u-margin-right-5 nhsuk-u-margin-bottom-4">
+                  Start visit
+                </a>
+
+                <a href="/wards/visits/cancel" className="nhsuk-button app-button--red">
+                  Cancel visit
+                </a>
+              </div>
+            </details>
+          </div>
+          <div className="app-visit-card-beside">
+            <p>
+              <b>in 22 minutes</b>
+            </p>
+          </div>
+        </div>
+      </li>
+    ))}
+  </ul>
   </div>
 );
-
 export default Visits;
