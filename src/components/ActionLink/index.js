3495441ffc5ff8751c526e7b9c27cc2e994f5bd4
Wen Ting Wang <chubberlisk@outlook.com>
2020-05-15 15:06:00 +0100

    Update ActionLink component to use client-side navigation

--- src/components/ActionLink/index.js
+++ src/components/ActionLink/index.js
@@ -1,19 +1,22 @@
 import React from "react";
+import Link from "next/link";
 
 const ActionLink = ({ href, children }) => (
   <div className="nhsuk-action-link">
-    <a className="nhsuk-action-link__link" href={href}>
-      <svg
-        className="nhsuk-icon nhsuk-icon__arrow-right-circle"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        aria-hidden="true"
-      >
-        <path d="M0 0h24v24H0z" fill="none"></path>
-        <path d="M12 2a10 10 0 0 0-9.95 9h11.64L9.74 7.05a1 1 0 0 1 1.41-1.41l5.66 5.65a1 1 0 0 1 0 1.42l-5.66 5.65a1 1 0 0 1-1.41 0 1 1 0 0 1 0-1.41L13.69 13H2.05A10 10 0 1 0 12 2z"></path>
-      </svg>
-      <span className="nhsuk-action-link__text">{children}</span>
-    </a>
+    <Link href={href}>
+      <a className="nhsuk-action-link__link">
+        <svg
+          className="nhsuk-icon nhsuk-icon__arrow-right-circle"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          aria-hidden="true"
+        >
+          <path d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M12 2a10 10 0 0 0-9.95 9h11.64L9.74 7.05a1 1 0 0 1 1.41-1.41l5.66 5.65a1 1 0 0 1 0 1.42l-5.66 5.65a1 1 0 0 1-1.41 0 1 1 0 0 1 0-1.41L13.69 13H2.05A10 10 0 1 0 12 2z"></path>
+        </svg>
+        <span className="nhsuk-action-link__text">{children}</span>
+      </a>
+    </Link>
   </div>
 );
 
