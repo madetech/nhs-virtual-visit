221aac996b4fda5d5b1b07e7b31679ca1dbf3945
Wen Ting Wang <chubberlisk@outlook.com>
2020-05-18 16:57:40 +0100

    Update visits filtering tests to use mockdate
    So our tests don't fail on another date!
    
    Co-authored-by: Jiv Dhaliwal <jiv.dhaliwal@madetech.com>

--- src/helpers/filterTodaysVisits.test.js
+++ src/helpers/filterTodaysVisits.test.js
@@ -1,29 +1,38 @@
+import MockDate from "mockdate";
 import filterTodaysVisits from "./filterTodaysVisits";
 
 describe("filterTodaysVisits", () => {
+  beforeEach(() => {
+    MockDate.set(new Date("2020-05-18"));
+  });
+
+  afterEach(() => {
+    MockDate.reset();
+  });
+
   it("returns visits that are scheduled for today", () => {
     const visits = [
-      { callTime: "2020-05-17 23:59:59+01" },
-      { callTime: "2020-05-18 13:00:00+01" },
-      { callTime: "2020-05-18 13:00:00+01" },
-      { callTime: "2020-05-19 01:00:00+01" },
+      { callTime: "2020-05-17 23:59:59" },
+      { callTime: "2020-05-18 13:00:00" },
+      { callTime: "2020-05-18 13:00:00" },
+      { callTime: "2020-05-19 01:00:00" },
     ];
 
     expect(filterTodaysVisits(visits)).toEqual([
-      { callTime: "2020-05-18 13:00:00+01" },
-      { callTime: "2020-05-18 13:00:00+01" },
+      { callTime: "2020-05-18 13:00:00" },
+      { callTime: "2020-05-18 13:00:00" },
     ]);
   });
 
   it("returns visits that are before midnight", () => {
     const visits = [
-      { callTime: "2020-05-18 23:59:59+01" },
-      { callTime: "2020-05-19 00:00:00+01" },
-      { callTime: "2020-05-19 00:00:01+01" },
+      { callTime: "2020-05-18 23:59:59" },
+      { callTime: "2020-05-19 00:00:00" },
+      { callTime: "2020-05-19 00:00:01" },
     ];
 
     expect(filterTodaysVisits(visits)).toEqual([
-      { callTime: "2020-05-18 23:59:59+01" },
+      { callTime: "2020-05-18 23:59:59" },
     ]);
   });
 });
