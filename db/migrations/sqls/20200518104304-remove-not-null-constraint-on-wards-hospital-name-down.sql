e7b5a331a11b4d7d9108e2cb67dfbe3798329cfc
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-05-20 17:20:52 +0100

    Fix down migration to ensure that hospital_names are set before setting constraint

--- db/migrations/sqls/20200518104304-remove-not-null-constraint-on-wards-hospital-name-down.sql
+++ db/migrations/sqls/20200518104304-remove-not-null-constraint-on-wards-hospital-name-down.sql
@@ -1,1 +1,14 @@
+UPDATE
+  wards
+SET
+  hospital_name = (
+    SELECT
+      name
+    FROM
+      hospitals
+    WHERE
+      hospitals.id = wards.hospital_id
+      AND hospitals.trust_id = wards.trust_id
+  );
+
 ALTER TABLE wards ALTER COLUMN hospital_name SET NOT NULL;
