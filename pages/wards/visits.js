e512d0267c1faeb007bd1a16c8eb8c4d5dd51391
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-05-20 14:16:08 +0100

    Extract accordionised visits to a component
    This will allow us to style the component
    
    Co-authored-by: Wen Ting Wang <chubberlisk@outlook.com>

--- pages/wards/visits.js
+++ pages/wards/visits.js
@@ -1,17 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import Layout from "../../src/components/Layout";
 import Heading from "../../src/components/Heading";
 import ActionLink from "../../src/components/ActionLink";
 import { GridRow, GridColumn } from "../../src/components/Grid";
-import VisitsPanelList from "../../src/components/VisitsPanelList";
 import VisitsTable from "../../src/components/VisitsTable";
 import Error from "next/error";
 import Text from "../../src/components/Text";
 import verifyToken from "../../src/usecases/verifyToken";
 import propsWithContainer from "../../src/middleware/propsWithContainer";
-import filterTodaysVisits from "../../src/helpers/filterTodaysVisits";
-import filterUpcomingVisits from "../../src/helpers/filterUpcomingVisits";
-import filterPastVisits from "../../src/helpers/filterPastVisits";
+import AccordionVisits from "../../src/components/AccordionVisits";
 
 export default function WardVisits({
   scheduledCalls,
@@ -23,62 +20,8 @@ export default function WardVisits({
     return <Error />;
   }
 
-  const [displayedVisits, setDisplayedVisits] = useState(
-    filterTodaysVisits(scheduledCalls)
-  );
-  const [visitsPanelListTitle, setVisitsPanelListTitle] = useState("Today");
-
   const tableContainer = showAccordion ? (
-    <div className="nhsuk-grid-row">
-      <div className="nhsuk-grid-column-one-quarter">
-        <ul className="nhsuk-list">
-          <li
-            className={
-              visitsPanelListTitle == "Today" ? "nhsuk-u-font-weight-bold" : ""
-            }
-          >
-            <a
-              href="#"
-              onClick={() => {
-                setDisplayedVisits(filterTodaysVisits(scheduledCalls));
-                setVisitsPanelListTitle("Today");
-              }}
-            >
-              Today
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              onClick={() => {
-                setDisplayedVisits(filterUpcomingVisits(scheduledCalls));
-                setVisitsPanelListTitle("Upcoming");
-              }}
-            >
-              Upcoming
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              onClick={() => {
-                setDisplayedVisits(filterPastVisits(scheduledCalls));
-                setVisitsPanelListTitle("Past");
-              }}
-            >
-              Past
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="nhsuk-grid-column-three-quarters">
-        <VisitsPanelList
-          visits={displayedVisits}
-          title={visitsPanelListTitle}
-        />
-      </div>
-    </div>
+    <AccordionVisits visits={scheduledCalls} />
   ) : (
     <VisitsTable visits={scheduledCalls} />
   );
