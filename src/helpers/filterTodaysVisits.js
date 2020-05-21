86d5e641f6ed68b455ff8155ac0b9c7205e2b08d
Wen Ting Wang <chubberlisk@outlook.com>
2020-05-21 13:41:15 +0100

    Update filterTodaysVisits to be from 1 hour ago
    Co-authored-by: Joshua-Luke Bevan <joshua.bevan@madetech.com>

--- src/helpers/filterTodaysVisits.js
+++ src/helpers/filterTodaysVisits.js
@@ -3,7 +3,7 @@ import moment from "moment";
 export default function filterTodaysVisits(visits) {
   return visits.filter((visit) =>
     moment(visit.callTime).isBetween(
-      moment().startOf("day"),
+      moment().subtract(1, "hours"),
       moment().endOf("day")
     )
   );
--- src/helpers/filterTodaysVisits.test.js
+++ src/helpers/filterTodaysVisits.test.js
@@ -3,24 +3,29 @@ import filterTodaysVisits from "./filterTodaysVisits";
 
 describe("filterTodaysVisits", () => {
   beforeEach(() => {
-    MockDate.set(new Date("2020-05-18"));
+    MockDate.set(new Date("2020-05-18 13:00"));
   });
 
   afterEach(() => {
     MockDate.reset();
   });
 
-  it("returns visits that are scheduled for today", () => {
+  it("returns visits that are scheduled between 1 hour ago until end of the day", () => {
     const visits = [
       { callTime: "2020-05-17 23:59:59" },
+      { callTime: "2020-05-18 09:00:00" },
+      { callTime: "2020-05-18 11:59:59" },
+      { callTime: "2020-05-18 12:00:00" },
+      { callTime: "2020-05-18 12:00:01" },
       { callTime: "2020-05-18 13:00:00" },
-      { callTime: "2020-05-18 13:00:00" },
+      { callTime: "2020-05-18 15:00:00" },
       { callTime: "2020-05-19 01:00:00" },
     ];
 
     expect(filterTodaysVisits(visits)).toEqual([
+      { callTime: "2020-05-18 12:00:01" },
       { callTime: "2020-05-18 13:00:00" },
-      { callTime: "2020-05-18 13:00:00" },
+      { callTime: "2020-05-18 15:00:00" },
     ]);
   });
 
