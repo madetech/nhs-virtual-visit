d2821063a3f96884afaa53460648b0cb3b3cc692
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-05-26 18:17:24 +0100

    Move all database cleaning to testTeardown
    Means that we don't have to worry about database cleaning when writing new tests

--- src/usecases/createTrust.contractTest.js
+++ src/usecases/createTrust.contractTest.js
@@ -1,18 +1,9 @@
 import createTrust from "./createTrust";
 import AppContainer from "../containers/AppContainer";
-import truncateAllTables from "../testUtils/truncateAllTables";
 
 describe("createTrust contract tests", () => {
   const container = AppContainer.getInstance();
   const retrieveTrustById = container.getRetrieveTrustById();
-
-  beforeEach(async () => {
-    await truncateAllTables(container);
-  });
-
-  afterEach(async () => {
-    await truncateAllTables(container);
-  });
 
   it("creates a trust in the db when valid", async () => {
     const request = {
