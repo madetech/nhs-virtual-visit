edd8c399c7f1c6d57df3051b98cf98702843bec9
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-05-27 12:20:02 +0100

    Recreate Admin token if it is about to expire
    Co-authored-by: Joshua-Luke Bevan <joshua.bevan@madetech.com>

--- src/usecases/verifyAdminToken.js
+++ src/usecases/verifyAdminToken.js
@@ -1,4 +1,5 @@
 import adminIsAuthenticated from "./adminIsAuthenticated";
+import moment from "moment";
 
 export default function (callback) {
   return function (context) {
@@ -9,7 +10,34 @@ export default function (callback) {
     );
 
     if (authenticationToken) {
-      return callback({ ...context, authenticationToken }) ?? { props: {} };
+      const expiresAt = new Date(authenticationToken.exp * 1000);
+      if (
+        moment(moment()).isBetween(
+          moment(expiresAt).subtract(3, "hours"),
+          expiresAt
+        )
+      ) {
+        const tokenProvider = container.getTokenProvider();
+        const refreshedEncToken = tokenProvider.generate({
+          wardId: authenticationToken.wardId,
+          wardCode: authenticationToken.wardCode,
+          trustId: authenticationToken.trustId,
+          type: authenticationToken.type,
+        });
+        res.setHeader("Set-Cookie", [
+          `token=${refreshedEncToken}; httpOnly; path=/;`,
+        ]);
+
+        const refreshedToken = tokenProvider.validate(refreshedEncToken);
+
+        return (
+          callback({ ...context, authenticationToken: refreshedToken }) ?? {
+            props: {},
+          }
+        );
+      } else {
+        return callback({ ...context, authenticationToken }) ?? { props: {} };
+      }
     } else {
       res.writeHead(302, { Location: "/wards/login" }).end();
     }
