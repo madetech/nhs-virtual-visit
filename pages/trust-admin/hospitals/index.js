504b4a005421451d66a72d64f2d82df3b4ad4a3d
Wen Ting Wang <chubberlisk@outlook.com>
2020-06-01 16:07:30 +0100

    Add hospitals page for a trust admin

---
+++ pages/trust-admin/hospitals/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Error from "next/error";
+import Layout from "../../../src/components/Layout";
+import propsWithContainer from "../../../src/middleware/propsWithContainer";
+import verifyTrustAdminToken from "../../../src/usecases/verifyTrustAdminToken";
+import HospitalsTable from "../../../src/components/HospitalsTable";
+import { GridRow, GridColumn } from "../../../src/components/Grid";
+import Heading from "../../../src/components/Heading";
+import ActionLink from "../../../src/components/ActionLink";
+import Text from "../../../src/components/Text";
+
+const TrustAdmin = ({ hospitals, hospitalError, trust, trustError }) => {
+  if (hospitalError || trustError) {
+    return <Error err={hospitalError || trustError} />;
+  }
+
+  return (
+    <Layout title={`Hospitals for ${trust.name}`} renderLogout={true}>
+      <GridRow>
+        <GridColumn width="full">
+          <Heading>
+            <span className="nhsuk-caption-l">
+              {trust.name}
+              <span className="nhsuk-u-visually-hidden">-</span>
+            </span>
+            Hospitals
+          </Heading>
+          <ActionLink href={`/trust-admin/add-a-hospital`}>
+            Add a hospital
+          </ActionLink>
+          {hospitals.length > 0 ? (
+            <HospitalsTable hospitals={hospitals} />
+          ) : (
+            <Text>There are no hospitals.</Text>
+          )}
+        </GridColumn>
+      </GridRow>
+    </Layout>
+  );
+};
+
+export const getServerSideProps = propsWithContainer(
+  verifyTrustAdminToken(async ({ container, authenticationToken }) => {
+    const hospitalsResponse = await container.getRetrieveHospitalsByTrustId()(
+      authenticationToken.trustId
+    );
+    const trustResponse = await container.getRetrieveTrustById()(
+      authenticationToken.trustId
+    );
+
+    return {
+      props: {
+        hospitals: hospitalsResponse.hospitals,
+        hospitalError: hospitalsResponse.error,
+        trust: { name: trustResponse.trust?.name },
+        trustError: trustResponse.error,
+      },
+    };
+  })
+);
+
+export default TrustAdmin;
