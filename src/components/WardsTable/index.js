e0cb1105735bdde9c4632407e9c80556eee5454a
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-06-03 17:32:12 +0100

    Hide the hospital name column if the ward visit totals are displayed
    The hospital name will eventually be removed. But while it is still appearing
    on the TrustAdmin dashboard we want to show the Hospital Name column when the
    table is rendered there. If we are showing ward visit totals then we're assuming
    we're on the view hospital page and can hide the hospital name column

--- src/components/WardsTable/index.js
+++ src/components/WardsTable/index.js
@@ -7,9 +7,11 @@ const WardsTable = ({ wards, wardVisitTotals }) => (
       <caption className="nhsuk-table__caption">List of wards</caption>
       <thead className="nhsuk-table__head">
         <tr className="nhsuk-table__row">
-          <th className="nhsuk-table__header" scope="col">
-            Hospital name
-          </th>
+          {!wardVisitTotals && (
+            <th className="nhsuk-table__header" scope="col">
+              Hospital name
+            </th>
+          )}
           <th className="nhsuk-table__header" scope="col">
             Ward name
           </th>
@@ -28,7 +30,9 @@ const WardsTable = ({ wards, wardVisitTotals }) => (
       <tbody className="nhsuk-table__body">
         {wards.map((ward) => (
           <tr key={ward.callId} className="nhsuk-table__row">
-            <td className="nhsuk-table__cell">{ward.hospitalName}</td>
+            {!wardVisitTotals && (
+              <td className="nhsuk-table__cell">{ward.hospitalName}</td>
+            )}
             <td className="nhsuk-table__cell">{ward.name}</td>
             <td className="nhsuk-table__cell">{ward.code}</td>
             {wardVisitTotals && (
