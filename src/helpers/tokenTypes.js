49986ea63073f0813b8b009eb75be17c40f5824f
Wen Ting Wang <chubberlisk@outlook.com>
2020-06-04 10:32:17 +0100

    Refactor tokenTypes to use new module syntax

--- src/helpers/tokenTypes.js
+++ src/helpers/tokenTypes.js
@@ -1,9 +1,3 @@
-const WARD_STAFF = "wardStaff";
-const TRUST_ADMIN = "trustAdmin";
-const ADMIN = "admin";
-
-module.exports = {
-  WARD_STAFF,
-  TRUST_ADMIN,
-  ADMIN,
-};
+export const WARD_STAFF = "wardStaff";
+export const TRUST_ADMIN = "trustAdmin";
+export const ADMIN = "admin";
