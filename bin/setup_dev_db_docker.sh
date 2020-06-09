aa71ab77a012bd5a01ad8521607c6dc2745504ba
Robert Marshall <robert.marshall@madetech.com>
2020-06-09 14:04:57 +0100

    Specify username and database instead of full connection string.

--- bin/setup_dev_db_docker.sh
+++ bin/setup_dev_db_docker.sh
@@ -3,4 +3,4 @@
 sudo docker-compose exec postgres dropdb nhs-virtual-visit-dev -U postgres
 sudo docker-compose exec postgres createdb nhs-virtual-visit-dev -U postgres
 npm run dbmigrate up
-sudo docker-compose exec postgres psql postgresql://postgres:postgres@localhost/nhs-virtual-visit-dev -f /db/seeds.sql
+sudo docker-compose exec postgres psql nhs-virtual-visit-dev -U postgres -f /db/seeds.sql
