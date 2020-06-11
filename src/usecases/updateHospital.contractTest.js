842e2bd35bae29dbca9e747a85ea429a83f823d8
Robert Marshall <robert.marshall@madetech.com>
2020-06-11 10:58:18 +0100

    Add contract test for updating hospital

---
+++ src/usecases/updateHospital.contractTest.js
@@ -0,0 +1,39 @@
+import updateHospital from "./updateHospital";
+import AppContainer from "../containers/AppContainer";
+
+describe("updateHospital contract tests", () => {
+  const container = AppContainer.getInstance();
+
+  it("updates a hospital in the db when valid", async () => {
+    const { trustId } = await container.getCreateTrust(container)({
+      name: "Test Trust",
+      adminCode: "TEST",
+    });
+
+    const { hospitalId } = await container.getCreateHospital()({
+      name: "Test Hospital",
+      trustId: trustId,
+    });
+
+    const request = {
+      name: "Test Hospital 2",
+      id: hospitalId,
+    };
+
+    const { id: updatedHospitalId, error } = await updateHospital(container)(
+      request
+    );
+
+    const { hospital } = await container.getRetrieveHospitalById()(
+      updatedHospitalId,
+      trustId
+    );
+
+    expect(hospital).toEqual({
+      id: hospitalId,
+      name: "Test Hospital 2",
+    });
+
+    expect(error).toBeNull();
+  });
+});
