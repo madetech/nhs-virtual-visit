992039851e1e1efd6f0365f0840c786185aa891c
Robert Marshall <robert.marshall@madetech.com>
2020-06-12 10:10:52 +0100

    Add edit journey (missing success page)

---
+++ src/components/EditHospitalForm/index.js
@@ -0,0 +1,101 @@
+import React, { useCallback, useState } from "react";
+import Button from "../Button";
+import FormGroup from "../FormGroup";
+import Heading from "../Heading";
+import Input from "../Input";
+import ErrorSummary from "../ErrorSummary";
+import Label from "../Label";
+import Router from "next/router";
+
+const isPresent = (input) => {
+  if (input.length !== 0) {
+    return input;
+  }
+};
+
+const EditHospitalForm = ({ errors, setErrors, hospital }) => {
+  const [hospitalName, setHospitalName] = useState(hospital.name);
+  let onSubmitErrors = [];
+
+  const hasError = (field) =>
+    errors.find((error) => error.id === `${field}-error`);
+
+  const errorMessage = (field) => {
+    const error = errors.filter((err) => err.id === `${field}-error`);
+    return error.length === 1 ? error[0].message : "";
+  };
+
+  const submitAnswers = async () =>
+    await fetch("/api/update-a-hospital", {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        id: hospital.id,
+        name: hospitalName,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) throw Error(response.status);
+        return response.json();
+      })
+      .then(() => Router.push({ pathname: `/trust-admin/hospitals` }))
+      .catch(() => {
+        onSubmitErrors.push({
+          id: "hospital-update-error",
+          message: "There was a problem saving your changes",
+        });
+        setErrors(onSubmitErrors);
+      });
+
+  const onSubmit = useCallback(async (event) => {
+    event.preventDefault();
+    onSubmitErrors = [];
+    const setHospitalNameError = (errors) => {
+      errors.push({
+        id: "hospital-name-error",
+        message: "Enter a hospital name",
+      });
+    };
+
+    if (!isPresent(hospitalName)) {
+      setHospitalNameError(onSubmitErrors);
+    }
+
+    if (onSubmitErrors.length === 0) {
+      await submitAnswers();
+    }
+    setErrors(onSubmitErrors);
+  });
+
+  return (
+    <>
+      <ErrorSummary errors={errors} />
+      <form onSubmit={onSubmit}>
+        <Heading>Edit a hospital</Heading>
+        <FormGroup>
+          <Label htmlFor="hospital-name" className="nhsuk-label--l">
+            What is the hospital name?
+          </Label>
+          <Input
+            id="hospital-name"
+            type="text"
+            hasError={hasError("hospital-name")}
+            errorMessage={errorMessage("hospital-name")}
+            className="nhsuk-u-font-size-32 nhsuk-input--width-10"
+            style={{ padding: "16px!important", height: "64px" }}
+            onChange={(event) => setHospitalName(event.target.value)}
+            name="hospital-name"
+            autoComplete="off"
+            value={hospitalName || ""}
+          />
+        </FormGroup>
+
+        <Button className="nhsuk-u-margin-top-5">Edit hospital</Button>
+      </form>
+    </>
+  );
+};
+
+export default EditHospitalForm;
