43f57a92435d1dcea3aaa12c8c8893f1ab772cd4
Robert Marshall <robert.marshall@madetech.com>
2020-06-16 11:22:00 +0100

    Don't remove the call password when cleaning up

--- db/scripts/cleanup_scheduled_calls.js
+++ db/scripts/cleanup_scheduled_calls.js
@@ -16,7 +16,7 @@ async function cleanupScheduledCalls() {
   const db = await getDb();
   const scheduledCalls = await db.result(
     `UPDATE scheduled_calls_table 
-     SET patient_name = null, recipient_number = null, recipient_name = null, call_password = null, recipient_email = null, status = $1
+     SET patient_name = null, recipient_number = null, recipient_name = null, recipient_email = null, status = $1
      WHERE call_time < (now() - INTERVAL '1 DAY')`,
     status.COMPLETE
   );
