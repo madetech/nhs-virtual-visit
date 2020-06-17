c03c7115e2f012ed708ac13fae4edaec27a38903
Wen Ting Wang <chubberlisk@outlook.com>
2020-06-17 11:16:25 +0100

    Update GovNotify gateway to return fake NotifyClient when test env
    This means that when Cypress tests are run we won't call actually call
    Notify.

--- src/gateways/GovNotify/index.test.js
+++ src/gateways/GovNotify/index.test.js
@@ -1,4 +1,9 @@
+import { NotifyClient } from "notifications-node-client";
 import GovNotify from "./";
+
+const {
+  NotifyClient: FakeNotifyClient,
+} = require("../../../__mocks__/notifications-node-client");
 
 describe("GovNotify", () => {
   it("Produces a singleton", () => {
@@ -9,5 +14,29 @@ describe("GovNotify", () => {
     const instanceTwo = GovNotify.getInstance();
 
     expect(instanceOne).toEqual(instanceTwo);
+  });
+
+  describe("when using production environment", () => {
+    it("returns the real Notify client", async () => {
+      const instance = await GovNotify.getInstance();
+
+      expect(instance).toBeInstanceOf(NotifyClient);
+    });
+  });
+
+  describe("when using test environment", () => {
+    beforeEach(() => {
+      process.env.APP_ENV = "test";
+    });
+
+    afterEach(() => {
+      process.env.APP_ENV = "";
+    });
+
+    it("returns the fake Notify client", async () => {
+      const instance = await GovNotify.getInstance();
+
+      expect(instance).toBeInstanceOf(FakeNotifyClient);
+    });
   });
 });
