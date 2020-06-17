951f153ad42182fb93473dbe9846113f692d8e94
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-06-17 10:00:39 +0100

    Update verifyAdminCode usecase to use a password
    Co-authored-by: Rob Marshall <robert.marshall@madetech.com>

--- src/usecases/verifyAdminCode.js
+++ src/usecases/verifyAdminCode.js
@@ -1,19 +1,39 @@
-const verifyAdminCode = ({ getDb }) => async (code) => {
+import bcrypt from "bcryptjs";
+
+const verifyAdminCode = ({ getDb }) => async (code, password) => {
   const db = await getDb();
+
+  if (!password) {
+    return {
+      validAdminCode: false,
+      error: "password is not defined",
+    };
+  }
 
   try {
     const dbResponse = await db.any(
-      `SELECT id FROM admins WHERE code = $1 LIMIT 1`,
+      `SELECT id, password FROM admins WHERE code = $1 LIMIT 1`,
       [code]
     );
 
     if (dbResponse.length > 0) {
+      const [admin] = dbResponse;
+
+      if (!bcrypt.compareSync(password, admin.password))
+        return {
+          validAdminCode: false,
+          error: "Incorrect trust admin code or password",
+        };
+
       return {
         validAdminCode: true,
         error: null,
       };
     } else {
-      return { validAdminCode: false, error: null };
+      return {
+        validAdminCode: false,
+        error: null,
+      };
     }
   } catch (error) {
     console.log(error);
