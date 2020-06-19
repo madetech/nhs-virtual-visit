bb14b5273f170c2130e47b9772e98575e7e7e1bc
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-06-19 09:53:26 +0100

    Update trust factory to use spread syntax

--- src/testUtils/factories.js
+++ src/testUtils/factories.js
@@ -1,14 +1,11 @@
 import AppContainer from "../containers/AppContainer";
 const container = AppContainer.getInstance();
 
-export const setupTrust = async ({
-  name = null,
-  adminCode = null,
-  password = null,
-} = {}) => {
+export const setupTrust = async (args = {}) => {
   return await container.getCreateTrust()({
-    name: name || "Test Trust",
-    adminCode: adminCode || "TESTCODE",
-    password: password || "TESTPASSWORD",
+    name: "Test Trust",
+    adminCode: "TESTCODE",
+    password: "TESTPASSWORD",
+    ...args,
   });
 };
