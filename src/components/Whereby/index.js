e408bd9660f07ae7e8b83486524e4fa2218acb67
Wen Ting Wang <chubberlisk@outlook.com>
2020-06-19 10:37:07 +0100

    Update Whereby component to contain the end call button
    Co-authored-by: Stu Mackellar <mackella@users.noreply.github.com>

--- src/components/Whereby/index.js
+++ src/components/Whereby/index.js
@@ -1,16 +1,30 @@
 import React from "react";
+import Router from "next/router";
 
-const Whereby = ({ callId, displayName }) => (
-  <iframe
-    data-testid="whereby"
-    style={{
-      width: "100%",
-      height: "calc(100vh - 195px)",
-      border: 0,
-    }}
-    src={`https://${process.env.WHEREBY_SUBDOMAIN}.whereby.com/${callId}?embed&iframeSource=${process.env.WHEREBY_SUBDOMAIN}&background=off&displayName=${displayName}&screenshare=off&chat=off`}
-    allow="camera; microphone; fullscreen; speaker"
-  ></iframe>
+const Whereby = ({ callId, displayName, onEnd }) => (
+  <div>
+    <iframe
+      data-testid="whereby"
+      style={{
+        width: "100%",
+        height: "calc(100vh - 195px)",
+        border: 0,
+      }}
+      src={`https://${process.env.WHEREBY_SUBDOMAIN}.whereby.com/${callId}?embed&iframeSource=${process.env.WHEREBY_SUBDOMAIN}&background=off&displayName=${displayName}&screenshare=off&chat=off`}
+      allow="camera; microphone; fullscreen; speaker"
+    ></iframe>
+    <button
+      className="nhsuk-button"
+      style={{ margin: "0 30%", width: "40%" }}
+      type="submit"
+      onClick={() => {
+        onEnd();
+        Router.push(`/visits/end?callId=${callId}`);
+      }}
+    >
+      End call
+    </button>
+  </div>
 );
 
 export default Whereby;
