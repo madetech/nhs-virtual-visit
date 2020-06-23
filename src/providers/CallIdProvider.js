43dbed02b5d822fd0c509501f710790ea92fb0b5
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-06-23 13:03:36 +0100

    Add a CallIdProvider to return the correct callId based on the video provider

---
+++ src/providers/CallIdProvider.js
@@ -0,0 +1,31 @@
+import JitsiCallIdProvider from "./call_id_providers/JitsiCallIdProvider";
+import WherebyCallIdProvider from "./call_id_providers/WherebyCallIdProvider";
+
+const WHEREBY = "whereby";
+const JITSI = "jitsi";
+
+class CallIdProvider {
+  constructor(provider, callTime = null) {
+    this.provider = provider;
+    this.callTime = callTime;
+  }
+
+  async generate() {
+    let callIdProvider = null;
+
+    switch (this.provider) {
+      case WHEREBY:
+        callIdProvider = new WherebyCallIdProvider(this.callTime);
+        break;
+      case JITSI:
+        callIdProvider = new JitsiCallIdProvider();
+        break;
+      default:
+        throw `Provider ${this.provider} not supported`;
+    }
+
+    return await callIdProvider.generate();
+  }
+}
+
+export default CallIdProvider;
