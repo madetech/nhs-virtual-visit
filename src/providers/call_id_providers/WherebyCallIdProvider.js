e53fbcedb9c9623da3a8a565e8d128d95a070f51
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-06-23 12:45:57 +0100

    Add whereby CallId provider
    Co-authored-by: Wen Ting Wang <chubberlisk@outlook.com>

---
+++ src/providers/call_id_providers/WherebyCallIdProvider.js
@@ -0,0 +1,31 @@
+import moment from "moment";
+import fetch from "node-fetch";
+
+class WherebyCallIdProvider {
+  constructor(callTime) {
+    this.callTime = callTime;
+  }
+
+  async generate() {
+    let startTime = moment(this.callTime).format();
+    let endTime = moment(this.callTime).add(1, "years").format();
+
+    const response = await fetch("https://api.whereby.dev/v1/meetings", {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${process.env.WHEREBY_API_KEY}`,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        startDate: startTime,
+        endDate: endTime,
+      }),
+    });
+
+    let jsonResponse = await response.json();
+    let roomUrl = new URL(jsonResponse.roomUrl);
+    return roomUrl.pathname.slice(1);
+  }
+}
+
+export default WherebyCallIdProvider;
