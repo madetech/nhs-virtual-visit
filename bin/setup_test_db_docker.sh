ee653c82d26ea789fa82e0a9ab8684a39f3829f2
Robert Marshall <robert.marshall@madetech.com>
2020-06-24 11:32:54 +0100

    DB setup scripts now run sudo only if required

--- bin/setup_test_db_docker.sh
+++ bin/setup_test_db_docker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-sudo docker-compose exec postgres dropdb nhs-virtual-visit-test -U postgres
-sudo docker-compose exec postgres createdb nhs-virtual-visit-test -U postgres
+if [[ "$OSTYPE" == "linux-gnu"* && $EUID != 0 ]]
+then
+	commandPrefix="sudo"
+else
+	commandPrefix=
+fi
+
+$commandPrefix docker-compose exec postgres dropdb nhs-virtual-visit-test -U postgres
+$commandPrefix docker-compose exec postgres createdb nhs-virtual-visit-test -U postgres
 npm run dbmigratetest up
