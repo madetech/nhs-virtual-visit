60d6789985573b97cfaad47d9c113471849080b5
Wen Ting Wang <chubberlisk@outlook.com>
2020-06-24 11:47:28 +0100

    Add eventActions helper to hold our magic strings for events

---
+++ src/helpers/eventActions.test.js
@@ -0,0 +1,11 @@
+import { JOIN_VISIT, LEAVE_VISIT } from "./eventActions";
+
+describe("eventActions", () => {
+  it("returns the wardStaff user type", () => {
+    expect(JOIN_VISIT).toEqual("join-visit");
+  });
+
+  it("returns the trustAdmin user type", () => {
+    expect(LEAVE_VISIT).toEqual("leave-visit");
+  });
+});
