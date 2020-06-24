9ff3c358fba263b4aac692b1ff485f8fbb365baf
Robert Marshall <robert.marshall@madetech.com>
2020-06-24 11:32:54 +0100

    DB setup scripts now run sudo only if required

--- bin/setup_dev_db_docker.sh
+++ bin/setup_dev_db_docker.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-sudo docker-compose exec postgres dropdb nhs-virtual-visit-dev -U postgres
-sudo docker-compose exec postgres createdb nhs-virtual-visit-dev -U postgres
+if [[ "$OSTYPE" == "linux-gnu"* && $EUID != 0 ]]
+then
+	commandPrefix="sudo"
+else
+	commandPrefix=
+fi
+
+$commandPrefix docker-compose exec postgres dropdb nhs-virtual-visit-dev -U postgres
+$commandPrefix docker-compose exec postgres createdb nhs-virtual-visit-dev -U postgres
 npm run dbmigrate up
-sudo docker-compose exec postgres psql nhs-virtual-visit-dev -U postgres -f /db/seeds.sql
+$commandPrefix docker-compose exec postgres psql nhs-virtual-visit-dev -U postgres -f /db/seeds.sql
