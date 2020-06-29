da44e13beeff58e4fdd662cd09618520f5383b6a
Wen Ting Wang <chubberlisk@outlook.com>
2020-06-29 12:51:57 +0100

    Fix login page redirect after logging out as an admin
    Previously, an admin would be redirected to the ward staff's login page
    instead of the admin one.

--- src/components/AdminsNavigationBar/index.js
+++ src/components/AdminsNavigationBar/index.js
@@ -7,9 +7,20 @@ const AdminsNavigationBar = () => {
       text: "Home",
       href: "/",
     },
+    {
+      text: "Log out",
+      href: "#",
+      onClick: logout,
+    },
   ];
 
   return <NavigationBar links={links} testId="admins-navbar" />;
+};
+
+const logout = async () => {
+  await fetch("/api/session", { method: "DELETE" });
+
+  window.location.href = "/admin/login";
 };
 
 export default AdminsNavigationBar;
