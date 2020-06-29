a03d8006b704b126ae653a6f3f7efa81bd8630d1
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-06-29 15:25:19 +0100

    Fix case on autoComplete option

--- pages/trust-admin/login.js
+++ pages/trust-admin/login.js
@@ -92,7 +92,7 @@ const Login = () => {
                 className="nhsuk-input--width-10"
                 onChange={(event) => setPassword(event.target.value)}
                 name="password"
-                autocomplete="off"
+                autoComplete="off"
               />
               <br />
             </FormGroup>
