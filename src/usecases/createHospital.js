197786db37afe2d395d1c79c0205bae8b73fe991
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-06-30 15:23:54 +0100

    Update createHospital usecase with optional supportUrl param

--- src/usecases/createHospital.js
+++ src/usecases/createHospital.js
@@ -1,15 +1,19 @@
-const createHospital = ({ getDb }) => async ({ name, trustId }) => {
+const createHospital = ({ getDb }) => async ({
+  name,
+  trustId,
+  supportUrl = null,
+}) => {
   const db = await getDb();
 
   try {
     console.log("Creating hospital for", name);
     const createdHospital = await db.one(
       `INSERT INTO hospitals
-          (id, name, trust_id)
-          VALUES (default, $1, $2)
+          (id, name, trust_id, support_url)
+          VALUES (default, $1, $2, $3)
           RETURNING id
         `,
-      [name, trustId]
+      [name, trustId, supportUrl]
     );
 
     return {
