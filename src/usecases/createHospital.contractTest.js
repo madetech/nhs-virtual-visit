a20943cfd2097fa6feba518a87969da3ec27921e
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-06-30 15:34:26 +0100

    Add a createHospital contract test

---
+++ src/usecases/createHospital.contractTest.js
@@ -0,0 +1,28 @@
+import AppContainer from "../containers/AppContainer";
+import { setupTrust } from "../testUtils/factories";
+
+describe("createHospital contract tests", () => {
+  const container = AppContainer.getInstance();
+
+  it("creates a valid hospital", async () => {
+    const { trustId } = await setupTrust();
+
+    const { hospitalId } = await container.getCreateHospital()({
+      trustId,
+      name: "Test Hospital",
+      supportUrl: "https://www.support.example.com",
+    });
+
+    const { hospital, error } = await container.getRetrieveHospitalById()(
+      hospitalId,
+      trustId
+    );
+
+    expect(error).toBeNull();
+    expect(hospital).toEqual({
+      id: hospitalId,
+      name: "Test Hospital",
+      supportUrl: "https://www.support.example.com",
+    });
+  });
+});
