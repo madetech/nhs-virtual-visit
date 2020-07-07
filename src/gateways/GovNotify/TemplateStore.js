3aed5a2375c62920c8c78759b1015b3e9e9f9efb
Wen Ting Wang <wenting@madetech.com>
2020-07-07 11:58:48 +0100

    Add updated visit text and email to TemplateStore

--- src/gateways/GovNotify/TemplateStore.js
+++ src/gateways/GovNotify/TemplateStore.js
@@ -8,12 +8,30 @@ export default {
       "visit_time",
     ],
   },
+  updatedVisitText: {
+    templateId: process.env.SMS_UPDATED_VISIT_TEMPLATE_ID,
+    personalisationKeys: [
+      "hospital_name",
+      "ward_name",
+      "visit_date",
+      "visit_time",
+    ],
+  },
   secondText: {
     templateId: process.env.SMS_JOIN_TEMPLATE_ID,
     personalisationKeys: ["call_url", "ward_name", "hospital_name"],
   },
   firstEmail: {
     templateId: process.env.EMAIL_INITIAL_TEMPLATE_ID,
+    personalisationKeys: [
+      "hospital_name",
+      "ward_name",
+      "visit_date",
+      "visit_time",
+    ],
+  },
+  updatedVisitEmail: {
+    templateId: process.env.EMAIL_UPDATED_VISIT_TEMPLATE_ID,
     personalisationKeys: [
       "hospital_name",
       "ward_name",
