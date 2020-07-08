23a56c5e14a36bacfde03490b3bc8ca275063354
Wen Ting Wang <wenting@madetech.com>
2020-07-08 12:14:10 +0100

    Update validateMobileNumber to reject random string
    Previously it would throw an exception which isn't caught.

--- src/helpers/validateMobileNumber.js
+++ src/helpers/validateMobileNumber.js
@@ -2,10 +2,15 @@ import { PhoneNumberUtil, PhoneNumberType } from "google-libphonenumber";
 
 export default (input) => {
   const validator = PhoneNumberUtil.getInstance();
-  const parsed = validator.parseAndKeepRawInput(input, "GB");
 
-  return (
-    validator.isValidNumber(parsed) &&
-    validator.getNumberType(parsed) === PhoneNumberType.MOBILE
-  );
+  try {
+    const parsed = validator.parseAndKeepRawInput(input, "GB");
+
+    return (
+      validator.isValidNumber(parsed) &&
+      validator.getNumberType(parsed) === PhoneNumberType.MOBILE
+    );
+  } catch (error) {
+    return false;
+  }
 };
--- src/helpers/validateMobileNumber.test.js
+++ src/helpers/validateMobileNumber.test.js
@@ -5,16 +5,24 @@ describe("validateMobileNumber", () => {
     const actual = validateMobileNumber("07123456789");
     expect(actual).toEqual(true);
   });
+
   it("accepts an Italian mobile number (+)", () => {
     const actual = validateMobileNumber("+393123456789");
     expect(actual).toEqual(true);
   });
+
   it("accepts an Italian mobile number (00)", () => {
     const actual = validateMobileNumber("00393123456789");
     expect(actual).toEqual(true);
   });
+
   it("rejects a UK landline number", () => {
     const actual = validateMobileNumber("02045678901");
+    expect(actual).toEqual(false);
+  });
+
+  it("rejects a random string", () => {
+    const actual = validateMobileNumber("invalidMobileNumber");
     expect(actual).toEqual(false);
   });
 });
