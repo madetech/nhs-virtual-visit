2ace80cb99afe169b7843134221f23b1950e2725
Wen Ting Wang <wenting@madetech.com>
2020-07-20 12:40:19 +0100

    Add very basic edit visit success page

---
+++ pages/wards/visits/[id]/edit-success.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { GridRow, GridColumn } from "../../../../src/components/Grid";
+import ActionLink from "../../../../src/components/ActionLink";
+import AnchorLink from "../../../../src/components/AnchorLink";
+import Layout from "../../../../src/components/Layout";
+import verifyToken from "../../../../src/usecases/verifyToken";
+import propsWithContainer from "../../../../src/middleware/propsWithContainer";
+import { WARD_STAFF } from "../../../../src/helpers/userTypes";
+
+const EditVisitSuccess = () => (
+  <Layout
+    title="Virtual visit has been updated"
+    showNavigationBar={true}
+    showNavigationBarForType={WARD_STAFF}
+  >
+    <GridRow>
+      <GridColumn width="two-thirds">
+        <div
+          className="nhsuk-panel nhsuk-panel--confirmation nhsuk-u-margin-top-0 nhsuk-u-margin-bottom-4"
+          style={{ textAlign: "center" }}
+        >
+          <h1 className="nhsuk-panel__title">Virtual visit has been updated</h1>
+        </div>
+
+        <h2>What happens next</h2>
+
+        <ActionLink href={`/wards/book-a-visit`}>
+          Book a virtual visit
+        </ActionLink>
+        <p>
+          <AnchorLink href="/wards/visits">Return to virtual visits</AnchorLink>
+        </p>
+      </GridColumn>
+    </GridRow>
+  </Layout>
+);
+
+export const getServerSideProps = propsWithContainer(
+  verifyToken(() => {
+    return {
+      props: {},
+    };
+  })
+);
+
+export default EditVisitSuccess;
