b6c285b3e638a83d05e11580b4a6a4ac8cf052de
Wen Ting Wang <wenting@madetech.com>
2020-08-10 12:18:47 +0100

    Update TrustsTable to display video provider
    Co-authored-by: Shaun Wild <shaun.wild@madetech.com>

--- src/components/TrustsTable/index.js
+++ src/components/TrustsTable/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AnchorLink from "../AnchorLink";
+import { VIDEO_PROVIDER_OPTIONS } from "../../providers/CallIdProvider";
 
 const TrustsTable = ({ trusts }) => (
   <div className="nhsuk-table-responsive">
@@ -10,6 +11,9 @@ const TrustsTable = ({ trusts }) => (
           <th className="nhsuk-table__header" scope="col">
             Trust name
           </th>
+          <th className="nhsuk-table__header" scope="col">
+            Video provider
+          </th>
           <th className="nhsuk-table__header" scope="col" colSpan="2">
             <span className="nhsuk-u-visually-hidden">Actions</span>
           </th>
@@ -17,8 +21,19 @@ const TrustsTable = ({ trusts }) => (
       </thead>
       <tbody className="nhsuk-table__body">
         {trusts.map((trust) => (
-          <tr key={trust.callId} className="nhsuk-table__row">
+          <tr
+            key={trust.callId}
+            className="nhsuk-table__row"
+            data-testid={trust.name.toLowerCase().replace(/\W+/g, "-")}
+          >
             <td className="nhsuk-table__cell">{trust.name}</td>
+            <td className="nhsuk-table__cell">
+              {
+                VIDEO_PROVIDER_OPTIONS.find(
+                  ({ id }) => id === trust.videoProvider
+                ).name
+              }
+            </td>
             <td className="nhsuk-table__cell">
               <AnchorLink href={`/admin/trusts/${trust.id}/edit`}>
                 Edit
