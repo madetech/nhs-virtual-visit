1a87c17767f7f0f980d7de641ca758e7bb6ffd73
Wen Ting Wang <wenting@madetech.com>
2020-08-10 11:40:38 +0100

    Add edit a trust form to change video provider
    Co-authored-by: Shaun Wild <shaun.wild@madetech.com>

---
+++ pages/admin/trusts/[id]/edit-success.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Error from "next/error";
+import Layout from "../../../../src/components/Layout";
+import AnchorLink from "../../../../src/components/AnchorLink";
+import propsWithContainer from "../../../../src/middleware/propsWithContainer";
+import verifyAdminToken from "../../../../src/usecases/verifyAdminToken";
+import { ADMIN } from "../../../../src/helpers/userTypes";
+
+const EditATrustSuccess = ({ error, trust }) => {
+  if (error) {
+    return <Error />;
+  }
+
+  return (
+    <Layout
+      title={`${trust.name} has been updated`}
+      showNavigationBarForType={ADMIN}
+      showNavigationBar={true}
+    >
+      <div className="nhsuk-grid-row">
+        <div className="nhsuk-grid-column-two-thirds">
+          <div
+            className="nhsuk-panel nhsuk-panel--confirmation nhsuk-u-margin-top-0 nhsuk-u-margin-bottom-4"
+            style={{ textAlign: "center" }}
+          >
+            <h1 className="nhsuk-panel__title">
+              {trust.name} has been updated
+            </h1>
+          </div>
+          <h2>What happens next</h2>
+
+          <p>
+            <AnchorLink href="/admin">Return to site administration</AnchorLink>
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export const getServerSideProps = propsWithContainer(
+  verifyAdminToken(async ({ container, query }) => {
+    const getRetrieveTrustById = container.getRetrieveTrustById();
+    const { trust, error } = await getRetrieveTrustById(query.id);
+
+    if (error) {
+      return { props: { error: error } };
+    } else {
+      return {
+        props: {
+          trust,
+          error,
+        },
+      };
+    }
+  })
+);
+
+export default EditATrustSuccess;
--- src/components/TrustsTable/index.js
+++ src/components/TrustsTable/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AnchorLink from "../AnchorLink";
 
 const TrustsTable = ({ trusts }) => (
   <div className="nhsuk-table-responsive">
@@ -9,12 +10,21 @@ const TrustsTable = ({ trusts }) => (
           <th className="nhsuk-table__header" scope="col">
             Trust name
           </th>
+          <th className="nhsuk-table__header" scope="col" colSpan="2">
+            <span className="nhsuk-u-visually-hidden">Actions</span>
+          </th>
         </tr>
       </thead>
       <tbody className="nhsuk-table__body">
         {trusts.map((trust) => (
           <tr key={trust.callId} className="nhsuk-table__row">
             <td className="nhsuk-table__cell">{trust.name}</td>
+            <td className="nhsuk-table__cell">
+              <AnchorLink href={`/admin/trusts/${trust.id}/edit`}>
+                Edit
+                <span className="nhsuk-u-visually-hidden"> {trust.name}</span>
+              </AnchorLink>
+            </td>
           </tr>
         ))}
       </tbody>
