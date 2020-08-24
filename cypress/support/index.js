6ce1d0ea7a85823f517920d70776f79d21b4e9b7
John Nicholas <john.nicholas@madetech.com>
2020-08-24 12:36:10 +0100

    cypress lifghthouse runs with no performance

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,6 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import "./commands";
 import "./audit";
-
+// import "./databaseSewtup";
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
