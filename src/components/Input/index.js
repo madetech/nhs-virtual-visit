34ab657cc33026506279cda13b0214252f754348
shaunwild97 <shaun.wild@madetech.com>
2020-09-10 11:06:03 +0100

    Fixed autofill bug

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,23 +2,28 @@ import React from "react";
 import classnames from "classnames";
 import ErrorMessage from "../ErrorMessage";
 
-const Input = ({ id, className, hasError, errorMessage, ...props }) => (
-  <>
-    {hasError && errorMessage ? (
-      <ErrorMessage id={`${id}-error`}>{errorMessage}</ErrorMessage>
-    ) : null}
-    <input
-      id={id}
-      className={classnames(
-        {
-          "nhsuk-input--error": hasError,
-        },
-        "nhsuk-input",
-        className
-      )}
-      {...props}
-    />
-  </>
+const Input = React.forwardRef(
+  ({ id, className, hasError, errorMessage, ...props }, ref) => (
+    <>
+      {hasError && errorMessage ? (
+        <ErrorMessage id={`${id}-error`}>{errorMessage}</ErrorMessage>
+      ) : null}
+      <input
+        id={id}
+        className={classnames(
+          {
+            "nhsuk-input--error": hasError,
+          },
+          "nhsuk-input",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    </>
+  )
 );
+
+Input.displayName = "Input";
 
 export default Input;
