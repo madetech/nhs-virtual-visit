f2db64dbc040bc6aae7868a6e3343601d68f96dd
Matthew Leak <matthew.leak@madetech.com>
2020-10-08 11:38:12 +0100

    Removed Heroku SSL redirect from server.js and removed package entry from yml

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 const next = require("next");
 const express = require("express");
-const sslRedirect = require("heroku-ssl-redirect");
+// const sslRedirect = require("heroku-ssl-redirect");
 const log = require("./logger");
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-  // redirect to SSL
-  server.use(sslRedirect());
+    // redirect to SSL
+    // server.use(sslRedirect());
 
-  server.all("*", (req, res) => {
-    return handle(req, res);
+    server.all("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(port, (err) => {
+      if (err) throw err;
+      log.info(`Running as ${process.env.NODE_ENV}`);
+      log.info(`Ready on http://localhost:${port}`);
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
   });
-
-  server.listen(port, (err) => {
-    if (err) throw err;
-    log.info(`Running as ${process.env.NODE_ENV}`);
-    log.info(`Ready on http://localhost:${port}`);
-  });
-});
