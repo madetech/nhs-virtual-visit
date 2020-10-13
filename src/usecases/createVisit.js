9e180ae5a47ed36de86856be64c94d5064d02e38
rebecca fitzsimmons <rebecca@madetech.com>
2020-10-13 13:51:40 +0100

    Extract insertVisit db interaction of createVisit to gateway

--- src/usecases/createVisit.js
+++ src/usecases/createVisit.js
@@ -1,4 +1,3 @@
-import { SCHEDULED } from "../../src/helpers/visitStatus";
 import validateVisit from "../../src/helpers/validateVisit";
 import { updateWardVisitTotalsSql } from "./updateWardVisitTotals";
 import logger from "../../logger";
@@ -79,28 +78,4 @@ const createVisit = (
   }
 };
 
-const insertVisit = async (db, visit, wardId) => {
-  const { id, call_id } = await db.one(
-    `INSERT INTO scheduled_calls_table
-      (id, patient_name, recipient_email, recipient_number, recipient_name, call_time, call_id, provider, ward_id, call_password, status)
-      VALUES (default, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
-      RETURNING id, call_id
-    `,
-    [
-      visit.patientName,
-      visit.contactEmail || "",
-      visit.contactNumber || "",
-      visit.contactName || "",
-      visit.callTime,
-      visit.callId,
-      visit.provider,
-      wardId,
-      visit.callPassword,
-      SCHEDULED,
-    ]
-  );
-
-  return { id, callId: call_id };
-};
-
-export { insertVisit, createVisit };
+export default createVisit;
