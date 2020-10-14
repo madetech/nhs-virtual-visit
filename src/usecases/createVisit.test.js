c55e3bf0fb7e575a11be718552e3d6c13e8801c2
rebecca fitzsimmons <rebecca@madetech.com>
2020-10-14 15:18:58 +0100

    Test createVisit in createVisit unit test rather than insert visit

--- src/usecases/createVisit.test.js
+++ src/usecases/createVisit.test.js
@@ -1,41 +1,148 @@
-import { insertVisit } from "./createVisit";
-import { SCHEDULED } from "../../src/helpers/visitStatus";
+import createVisit from "./createVisit";
 
-describe("insertVisit sql test", () => {
-  it("creates a visit in the db when valid", async () => {
-    const oneSpy = jest.fn().mockResolvedValue({ id: 10, call_id: "12345" });
-    const db = {
-      one: oneSpy,
-    };
+describe("createVisit", () => {
+  let randomIdProviderStub = jest.fn();
+  let callIdProviderStub = jest.fn();
+  let retrieveTrustByIdStub = jest.fn();
+  let retrieveWardByIdStub = jest.fn();
+  let createVisitUnitOfWorkSpy = jest.fn();
 
-    const request = {
-      patientName: "Bob Smith",
-      contactEmail: "bob.smith@madetech.com",
-      contactName: "John Smith",
-      contactNumber: "07123456789",
-      callTime: new Date(),
-      callId: "12345",
-      provider: "jitsi",
-      wardId: 1,
-      callPassword: "securePassword",
-    };
+  let date = new Date();
+  date.setDate(date.getDate() + 1);
+
+  const setupTestDoubles = (
+    callPassword,
+    callId,
+    trust,
+    ward,
+    bookingNotificationSuccess,
+    bookingNotificationErrors
+  ) => {
+    randomIdProviderStub.mockReturnValue({ generate: () => callPassword });
+    callIdProviderStub.mockReturnValue(callId);
+    retrieveTrustByIdStub.mockReturnValue({ trust: trust, error: "" });
+    retrieveWardByIdStub.mockReturnValue({ ward });
+    createVisitUnitOfWorkSpy.mockReturnValue({
+      bookingNotificationSuccess,
+      bookingNotificationErrors,
+    });
+  };
+
+  it("calls createVisitUnitOfWork with the visit and ward", async () => {
     const wardId = "wardId";
-    const { id, callId } = await insertVisit(db, request, wardId);
+    const trustId = "trustId";
+    const trust = { id: trustId, videoProvider: "testVideoProvider" };
+    const ward = { id: wardId, name: "wardName", hospitalName: "hospitalName" };
 
-    expect(id).toEqual(10);
-    expect(callId).toEqual("12345");
+    setupTestDoubles("123abc", "456cde", trust, ward, true, "");
 
-    expect(oneSpy).toHaveBeenCalledWith(expect.anything(), [
-      request.patientName,
-      request.contactEmail,
-      request.contactNumber,
-      request.contactName,
-      request.callTime,
-      request.callId,
-      request.provider,
-      wardId,
-      request.callPassword,
-      SCHEDULED,
-    ]);
+    const visit = {
+      patientName: "Patient Name",
+      contactEmail: "contact@example.com",
+      contactName: "Contact Name",
+      callTime: date,
+    };
+
+    const response = await createVisit(
+      randomIdProviderStub,
+      callIdProviderStub,
+      retrieveTrustByIdStub,
+      retrieveWardByIdStub,
+      createVisitUnitOfWorkSpy
+    )(visit, wardId, trustId);
+
+    const expectedVisit = {
+      patientName: "Patient Name",
+      contactEmail: "contact@example.com",
+      contactName: "Contact Name",
+      callTime: date,
+      callId: "456cde",
+      callPassword: "123abc",
+      provider: trust.videoProvider,
+    };
+
+    expect(createVisitUnitOfWorkSpy).toHaveBeenCalledWith(expectedVisit, ward);
+    expect(callIdProviderStub).toHaveBeenCalledWith(trust, visit.callTime);
+    expect(retrieveTrustByIdStub).toHaveBeenCalledWith(trustId);
+    expect(retrieveWardByIdStub).toHaveBeenCalledWith(wardId, trustId);
+    expect(response).toEqual({ success: true, err: undefined });
+  });
+
+  it("throws error if booking notification error", async () => {
+    const wardId = "wardId2";
+    const trustId = "trustId2";
+    const trust = { videoProvider: "testVideoProvider" };
+    const ward = {
+      id: wardId,
+      name: "wardName2",
+      hospitalName: "hospitalName2",
+    };
+
+    setupTestDoubles("12ab", "45cd", trust, ward, false, "emailError");
+
+    const visit = {
+      patientName: "Another Patient Name",
+      contactEmail: "anothercontact@example.com",
+      contactName: "Another Contact Name",
+      callTime: date,
+    };
+
+    const response = await createVisit(
+      randomIdProviderStub,
+      callIdProviderStub,
+      retrieveTrustByIdStub,
+      retrieveWardByIdStub,
+      createVisitUnitOfWorkSpy
+    )(visit, wardId, trustId);
+
+    const expectedVisit = {
+      patientName: "Another Patient Name",
+      contactEmail: "anothercontact@example.com",
+      contactName: "Another Contact Name",
+      callTime: date,
+      callId: "45cd",
+      callPassword: "12ab",
+      provider: trust.videoProvider,
+    };
+
+    expect(createVisitUnitOfWorkSpy).toHaveBeenCalledWith(expectedVisit, ward);
+    expect(response).toEqual({
+      success: false,
+      err: "Failed to send notification",
+    });
+  });
+
+  it("throws error if invalid visit", async () => {
+    const wardId = "wardId3";
+    const trustId = "trustId3";
+    const trust = { videoProvider: "testVideoProvider" };
+    const ward = {
+      id: wardId,
+      name: "wardName3",
+      hospitalName: "hospitalName3",
+    };
+
+    setupTestDoubles("12ab", "45cd", trust, ward, true, "");
+
+    const visit = {
+      patientName: "",
+      contactEmail: "contact@example.com",
+      contactName: "Contact Name",
+      callTime: date,
+    };
+
+    const response = await createVisit(
+      randomIdProviderStub,
+      callIdProviderStub,
+      retrieveTrustByIdStub,
+      retrieveWardByIdStub,
+      createVisitUnitOfWorkSpy
+    )(visit, wardId, trustId);
+
+    expect(response).toEqual({
+      success: false,
+      err: { patientName: "patientName must be present" },
+    });
+    expect(createVisitUnitOfWorkSpy).toHaveBeenCalledTimes(0);
   });
 });
