34ca200d54daa67a2c3b9d88432f1a8a526c3c38
Faissal Bensefia <faissal@madetech.com>
2020-10-21 14:38:18 +0100

    Apparently you don't need to set this reserved = true

--- userEvents/az/main.tf
+++ userEvents/az/main.tf
@@ -50,7 +50,6 @@ resource "azurerm_app_service_plan" "event_logger_service_plan" {
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind = "FunctionApp"
-  reserved = true
 
   sku {
     tier = "Standard"
