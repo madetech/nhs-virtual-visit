329362f69aab5410863cdefae57dbf9617b6e86d
Faissal Bensefia <faissal@madetech.com>
2020-10-21 12:18:14 +0100

    Add Azure function and storage account to Azure Terraform

--- userEvents/az/main.tf
+++ userEvents/az/main.tf
@@ -12,14 +12,15 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_cosmosdb_account" "visit_events_db" {
-  name                = "visit-events"
-  location            = azurerm_resource_group.rg.location
+  name = "visit-events"
+  location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  offer_type          = "Standard"
-  kind                = "GlobalDocumentDB"
+  offer_type = "Standard"
+  enable_free_tier = true
+  kind = "GlobalDocumentDB"
 
-  enable_automatic_failover = true
-
+  enable_automatic_failover = false
+  
   capabilities {
     name = "EnableAggregationPipeline"
   }
@@ -39,7 +40,37 @@ resource "azurerm_cosmosdb_account" "visit_events_db" {
   }
   
   geo_location {
-    location          = azurerm_resource_group.rg.location
+    location = azurerm_resource_group.rg.location
     failover_priority = 0
   }
+}
+
+resource "azurerm_app_service_plan" "event_logger_service_plan" {
+  name = "azure-functions-test-service-plan"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind = "FunctionApp"
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_storage_account" "event_logger_storage" {
+  name                     = "eventloggerstorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_function_app" "log_events" {
+  name = "log-events"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  
+  app_service_plan_id        = azurerm_app_service_plan.event_logger_service_plan.id
+  storage_account_name       = azurerm_storage_account.event_logger_storage.name
+  storage_account_access_key = azurerm_storage_account.event_logger_storage.primary_access_key
 }
