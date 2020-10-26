a4001aa0a721fb8a07ca9f102bc1328873d6253b
rebecca fitzsimmons <rebecca@madetech.com>
2020-10-26 13:57:32 +0000

    Remove unnecessary ternary and use true/false rather than yes/no in feature flag provider

--- src/helpers/featureFlag.test.js
+++ src/helpers/featureFlag.test.js
@@ -2,12 +2,12 @@ import featureIsEnabled from "./featureFlag";
 
 describe("featureFlag", () => {
   it("returns true if env var for feature is set to yes", () => {
-    process.env.FEATURE_FLAG_COOL_FEATURE = "yes";
+    process.env.FEATURE_FLAG_COOL_FEATURE = "true";
     expect(featureIsEnabled("FEATURE_FLAG_COOL_FEATURE")).toBeTruthy();
   });
 
   it("returns false if env var for feature is set to no", () => {
-    process.env.FEATURE_FLAG_COOL_FEATURE = "no";
+    process.env.FEATURE_FLAG_COOL_FEATURE = "false";
     expect(featureIsEnabled("FEATURE_FLAG_COOL_FEATURE")).toBeFalsy();
   });
 
