875350b0ba56dd5b902bc5c257a45c5fa819bc15
rebecca fitzsimmons <rebecca@madetech.com>
2020-11-04 16:16:12 +0000

    Remove console log from migration file

--- db/migrations/20201030102755-move-patient-details-to-new-table.js
+++ db/migrations/20201030102755-move-patient-details-to-new-table.js
@@ -45,8 +45,6 @@ exports.down = function (db) {
   return new Promise(function (resolve, reject) {
     fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
       if (err) return reject(err);
-      console.log("received data: " + data);
-
       resolve(data);
     });
   }).then(function (data) {
