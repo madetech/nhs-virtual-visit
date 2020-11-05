0d9738e31f552302753b8597743429dafa370f9b
rebecca fitzsimmons <rebecca@madetech.com>
2020-11-05 13:42:29 +0000

    Update updateVisit usecase to set visitor details on new table

--- src/usecases/updateVisitById.js
+++ src/usecases/updateVisitById.js
@@ -10,14 +10,11 @@ const updateVisitById = ({ getDb }) => async ({
   try {
     const updatedVisit = await db.one(
       `UPDATE scheduled_calls_table
-      SET recipient_name = $1,
-          recipient_email = $2,
-          recipient_number = $3,
-          call_time = $4
+      SET call_time = $1
       WHERE
-          id = $5
+          id = $2
       RETURNING *`,
-      [recipientName, recipientEmail, recipientNumber, callTime, id]
+      [callTime, id]
     );
 
     const updatedPatientDetails = await db.one(
@@ -30,13 +27,30 @@ const updateVisitById = ({ getDb }) => async ({
       [patientName, updatedVisit.patient_details_id]
     );
 
+    const updatedVisitorDetails = await db.one(
+      `
+        UPDATE visitor_details
+        SET recipient_name = $1,
+            recipient_email = $2,
+            recipient_number = $3
+        WHERE id = $4
+        RETURNING *
+      `,
+      [
+        recipientName,
+        recipientEmail,
+        recipientNumber,
+        updatedVisit.visitor_details_id,
+      ]
+    );
+
     return {
       visit: {
         id: updatedVisit.id,
         patientName: updatedPatientDetails.patient_name,
-        recipientName: updatedVisit.recipient_name,
-        recipientNumber: updatedVisit.recipient_number,
-        recipientEmail: updatedVisit.recipient_email,
+        recipientName: updatedVisitorDetails.recipient_name,
+        recipientNumber: updatedVisitorDetails.recipient_number,
+        recipientEmail: updatedVisitorDetails.recipient_email,
         callTime: updatedVisit.call_time,
         callId: updatedVisit.call_id,
       },
