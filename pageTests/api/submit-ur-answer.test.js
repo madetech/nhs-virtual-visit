06d9123f3d4f84e5a1412609099f3b1c2fa33e65
Faissal Bensefia <faissal@madetech.com>
2020-11-20 11:59:48 +0000

    chore: add test for calling the event logger when submitting ur answer

--- pageTests/api/submit-ur-answer.test.js
+++ pageTests/api/submit-ur-answer.test.js
@@ -1,11 +1,27 @@
 import submitUrAnswer from "../../pages/api/submit-ur-answer";
 
 describe("api/submit-ur-answer", () => {
-  it("submitting an answer triggers a request to the azure function endpoint", () => {
+  it("submitting an answer triggers a request to the azure function endpoint", async () => {
+    let logEventStub = jest.fn(async (/*event*/) => {});
     let container = {
-      getLogEventGateway: jest.fn(),
+      getLogEventGateway: jest.fn(() => logEventStub),
     };
-    submitUrAnswer({}, {}, { container });
+    let request = {
+      headers: {},
+      body: {},
+    };
+    await submitUrAnswer(request, {}, { container });
     expect(container.getLogEventGateway).toHaveBeenCalled();
+    expect(logEventStub).toHaveBeenCalledWith({
+      sessionId: expect.anything(),
+      correlationId: undefined,
+      createdOn: expect.anything(),
+      event: {
+        answer: undefined,
+      },
+      eventType: "ur-question-answered",
+      streamName: "ward-undefined",
+      trustId: undefined,
+    });
   });
 });
--- pages/api/submit-ur-answer.js
+++ pages/api/submit-ur-answer.js
@@ -1,5 +1,21 @@
 import withContainer from "../../src/middleware/withContainer";
+import { v4 as uuidv4 } from "uuid";
 
 export default withContainer(async (req, res, { container }) => {
-  /*const logEvent =*/ container.getLogEventGateway();
+  const logEvent = container.getLogEventGateway();
+  const sessionId = uuidv4();
+
+  const event = {
+    sessionId: sessionId,
+    correlationId: req.headers["x-correlation-id"],
+    createdOn: Date.now(),
+    streamName: `ward-${undefined}`,
+    trustId: undefined /*ward.trustId*/,
+    eventType: "ur-question-answered",
+    event: {
+      answer: req.body["would miss nhs vv"],
+    },
+  };
+
+  await logEvent(event);
 });
