56f68bef1b94563e3e5a763983ce41935365742e
nax2uk <azlinayeo@gmail.com>
2020-12-21 15:37:55 +0000

    chore: undo latest changes

--- test/testUtils/factories.js
+++ test/testUtils/factories.js
@@ -24,7 +24,6 @@ export const setupWard = async (args = {}) => {
   return await container.getCreateWard()({
     name: "Test Ward",
     code: "wardCode",
-    pin: "1233",
     ...args,
   });
 };
