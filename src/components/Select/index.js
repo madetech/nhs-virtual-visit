52a15174eed12c7b157173907e68723bcc16935f
nax2uk <azlinayeo@gmail.com>
2020-12-21 18:33:30 +0000

    chore: changed option value to equals value instead of id

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -35,7 +35,7 @@ const Select = ({
           {prompt}
         </option>
         {options.map((option) => (
-          <option key={option.id} value={option.id}>
+          <option key={option.id} value={option.value}>
             {option.name}
           </option>
         ))}
