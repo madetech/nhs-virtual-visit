09674526d36aab78bb382588db5525590bd1633c
paulolancao <paulo.lancao@madetech.com>
2020-12-22 22:04:27 +0000

    test: subject: integration with mssql

---
+++ db/migrations/mssql/sqls/20201218000001-create-user-table.sql
@@ -0,0 +1,16 @@
+USE nhs_virtual_visit_dev
+
+CREATE TABLE dbo.[user] (
+  [id] int IDENTITY(1, 1) NOT NULL,
+  [email] nvarchar(255) NOT NULL,
+  [password] nvarchar(255) NOT NULL,
+  [created_at] datetime NOT NULL DEFAULT GETDATE(),
+  [updated_at] datetime NOT NULL DEFAULT GETDATE(),
+  [type] nvarchar(255) NOT NULL,
+  [organisation_id] int,
+  [uuid] uniqueidentifier NOT NULL DEFAULT newid(),
+  [change_password] bit DEFAULT 0,
+  [status] tinyint DEFAULT 0,
+  CONSTRAINT PK_user PRIMARY KEY (id),
+  CONSTRAINT UN_email UNIQUE ([email]) 
+)
