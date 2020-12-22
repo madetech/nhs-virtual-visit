bf1c87b5c9e17ac6c34d600de6399344b5dbd8d6
paulolancao <paulo.lancao@madetech.com>
2020-12-22 22:04:27 +0000

    test: subject: integration with mssql

---
+++ db/migrations/mssql/sqls/20201218000008-insert-user-table.sql
@@ -0,0 +1,4 @@
+USE nhs_virtual_visit_dev
+
+INSERT INTO dbo.[user] ([email], [password], [type], [status]) 
+	VALUES('nhs-admin@nhs.co.uk', '$2b$10$Kwzuu07E.lx6ezpK58RYGuBZtrd9ULd5PsCPJXbe3BlN7Sax.rrmS', 'admin', 1);
