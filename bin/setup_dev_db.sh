4b6eb2f5e52e79178027b1bbd64449d3344cb978
paulolancao <paulo.lancao@madetech.com>
2020-12-22 18:48:15 +0000

    test: subject: updating env variables and fix docker local scripts

--- bin/setup_dev_db.sh
+++ bin/setup_dev_db.sh
@@ -4,4 +4,6 @@
 dropdb -U "postgres" -h "localhost" -p "5432"  nhs-virtual-visit-dev
 createdb -U "postgres" -h "localhost" -p "5432"  nhs-virtual-visit-dev
 npm run dbmigrate up
-cat db/seeds.sql | psql -U "postgres" -h "localhost" -p "5432" -d "nhs-virtual-visit-dev"
+# Remove after testing PL
+#cat db/seeds.sql | psql -U "postgres" -h "localhost" -p "5432" -d "nhs-virtual-visit-dev"
+npm run dbmigrate up:seed
