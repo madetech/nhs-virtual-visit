bdc5eb27ecf6bcd588e2063628b9849e3e98b2a7
nax2uk <azlinayeo@gmail.com>
2020-12-22 20:33:38 +0000

    chore: added edit pages

---
+++ pages/trust-admin/trust-managers/[id]/edit-success.js
@@ -0,0 +1,78 @@
+import React from "react";
+import Error from "next/error";
+import Layout from "../../../../src/components/Layout";
+import ActionLink from "../../../../src/components/ActionLink";
+import TrustAdminHeading from "../../../../src/components/TrustAdminHeading";
+import PanelUpdateSuccess from "../../../../src/components/PanelUpdateSuccess";
+import propsWithContainer from "../../../../src/middleware/propsWithContainer";
+import verifyTrustAdminToken from "../../../../src/usecases/verifyTrustAdminToken";
+import { GridRow, GridColumn } from "../../../../src/components/Grid";
+import { TRUST_ADMIN } from "../../../../src/helpers/userTypes";
+
+const EditATrustManagerSuccess = ({ error, trustManager, trust }) => {
+  if (error) {
+    return <Error />;
+  }
+
+  return (
+    <Layout
+      title={`${trustManager.email} has been updated`}
+      showNavigationBar={true}
+      showNavigationBarForType={TRUST_ADMIN}
+    >
+      <TrustAdminHeading
+        trustName={`${trust.name}`}
+        subHeading="Trust Managers"
+      />
+      <GridRow>
+        <GridColumn width="two-thirds">
+          <PanelUpdateSuccess name={`${trustManager.email}`} />
+          <h2>What happens next</h2>
+          <p>
+            <ActionLink href="/trust-admin/trust-managers">
+              Return to Trust Managers
+            </ActionLink>
+          </p>
+        </GridColumn>
+      </GridRow>
+    </Layout>
+  );
+};
+
+export const getServerSideProps = propsWithContainer(
+  verifyTrustAdminToken(async ({ container, query, authenticationToken }) => {
+    const trustResponse = await container.getRetrieveTrustById()(
+      authenticationToken.trustId
+    );
+    const trustManagerId = query.id;
+    /*** Trust Manager Array needs to swapped out with info from db once available *****/
+    const trustManagers = [
+      {
+        id: "1",
+        email: "abc@nhs.co.uk",
+        status: "active",
+      },
+      {
+        id: "2",
+        email: "def@nhs.co.uk",
+        status: "active",
+      },
+      {
+        id: "3",
+        email: "ghi@nhs.co.uk",
+        status: "active",
+      },
+    ];
+    const trustManager = trustManagers?.find(
+      (manager) => manager.id === trustManagerId
+    );
+    return {
+      props: {
+        trust: { name: trustResponse.trust?.name },
+        trustManager,
+      },
+    };
+  })
+);
+
+export default EditATrustManagerSuccess;
