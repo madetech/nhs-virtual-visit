587ac14a02a926cc2a7d22bf35dc1d81c4010001
paulolancao <paulo.lancao@madetech.com>
2020-12-22 23:53:43 +0000

    test: subject: include package json scripts

---
+++ src/gateways/MsSQL/index.js
@@ -0,0 +1,54 @@
+const sql = require("mssql");
+
+function setPoolConfigPerEnvironment(config) {
+  if (
+    process.env.NODE_ENV === "development-mssql" ||
+    process.env.APP_ENV === "development-mssql"
+  ) {
+    config.user = process.env.MSQL_DEV_DB_USER;
+    config.password = process.env.MSQL_DEV_DB_PASSWORD;
+    config.server = process.env.MSQL_DEV_DB_SERVER;
+    config.database = process.env.MSQL_DEV_DB_DATABASE;
+  } else if (
+    process.env.NODE_ENV === "e2e-mssql" ||
+    process.env.APP_ENV === "e2e-mssql"
+  ) {
+    config.user = process.env.MSQL_E2E_DB_USER;
+    config.password = process.env.MSQL_E2E_DB_PASSWORD;
+    config.server = process.env.MSQL_E2E_DB_SERVER;
+    config.database = process.env.MSQL_E2E_DB_DATABASE;
+  }
+}
+
+async function initPool() {
+  let config = {
+    driver: process.env.MSQL_DB_USER,
+    user: process.env.MSQL_DB_USER,
+    password: process.env.MSQL_DB_PASSWORD,
+    server: process.env.MSQL_DB_SERVER,
+    database: process.env.MSQL_DB_DATABASE,
+    options: {
+      encrypt: true,
+      validateBulkLoadParameters: false,
+      truestedConnection: true,
+      enableArithAbort: true,
+      integratedSecurity: true,
+      trustServerCertificate: true,
+      rowCollectionOnDone: true,
+    },
+  };
+
+  setPoolConfigPerEnvironment(config);
+
+  const DB_KEY = Symbol.for("MyApp.db");
+  const globalSymbols = Object.getOwnPropertySymbols(global);
+  const hasDb = globalSymbols.indexOf(DB_KEY) > -1;
+
+  if (!hasDb) {
+    return (global[DB_KEY] = await sql.connect(config));
+  } else {
+    return global[DB_KEY];
+  }
+}
+
+export default { getConnectionPool: () => initPool() };
