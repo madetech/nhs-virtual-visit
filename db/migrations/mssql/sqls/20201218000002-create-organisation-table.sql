1e2717394916d9292c8ba57eee4f4c62f9a89b09
paulolancao <paulo.lancao@madetech.com>
2020-12-22 22:04:27 +0000

    test: subject: integration with mssql

---
+++ db/migrations/mssql/sqls/20201218000002-create-organisation-table.sql
@@ -0,0 +1,12 @@
+USE nhs_virtual_visit_dev
+
+CREATE TABLE dbo.[organisation] (
+  [id] int IDENTITY(1, 1) NOT NULL,
+  [name] nvarchar(255),
+  [created_at] datetime DEFAULT GETDATE(),
+  [created_by] int,
+  [type] nvarchar(255) NOT NULL,
+  [uuid] uniqueidentifier NOT NULL DEFAULT newid(),
+  [status] tinyint DEFAULT 0,
+  CONSTRAINT PK_organisation PRIMARY KEY (id)
+)
