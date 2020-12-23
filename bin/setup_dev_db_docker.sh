367c8667c621ba490e4d8c75199b754e359f91a0
paulolancao <paulo.lancao@madetech.com>
2020-12-23 12:10:08 +0000

    fix: subject: rebase master and fix errors

--- bin/setup_dev_db_docker.sh
+++ bin/setup_dev_db_docker.sh
@@ -12,7 +12,11 @@ docker-compose -f docker/pg/docker-compose.yml up -d postgres
 
 wait_time=5s
 
+<<<<<<< HEAD
 # Wait for PostgreSQL Server to come up.
+=======
+# Wait for PostgreSQL Server to come up
+>>>>>>> test: subject: updating env variables and fix docker local scripts
 echo waiting database to start in $wait_time...
 sleep $wait_time
 echo database started...
@@ -20,7 +24,12 @@ echo database started...
 # Drop if exists and re-create database.
 $commandPrefix docker exec postgres dropdb nhs-virtual-visit-dev -U postgres --if-exists
 $commandPrefix docker exec postgres createdb nhs-virtual-visit-dev -U postgres
+<<<<<<< HEAD
 
 # Run create tables and seed scripts.
 npm run dbmigrate up
 npm run dbmigrate up:seed
+=======
+npm run dbmigrate up
+npm run dbmigrate up:seed
+>>>>>>> test: subject: updating env variables and fix docker local scripts
