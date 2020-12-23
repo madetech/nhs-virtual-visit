0fb1ef573ef8dc8a20ebc1abb3d77ecce072e2e9
paulolancao <paulo.lancao@madetech.com>
2020-12-23 16:33:22 +0000

    fix: subject: fix merge from master

--- bin/setup_dev_db.sh
+++ bin/setup_dev_db.sh
@@ -3,7 +3,6 @@
 #password is 'postgres'
 dropdb -U "postgres" -h "localhost" -p "5432"  nhs-virtual-visit-dev
 createdb -U "postgres" -h "localhost" -p "5432"  nhs-virtual-visit-dev
+
 npm run dbmigrate up
-# Remove after testing PL
-#cat db/seeds.sql | psql -U "postgres" -h "localhost" -p "5432" -d "nhs-virtual-visit-dev"
 npm run dbmigrate up:seed
