d5088d458c2db4c50d115c9b574495ceb22b6d0a
Richard Pentecost <richard.pentecost@madetech.com>
2020-12-23 09:58:46 +0000

    fix: updating cypress tests

--- cypress/integration/admin/addingATrust.js
+++ cypress/integration/admin/addingATrust.js
@@ -17,10 +17,7 @@ describe("As an admin, I want to add a trust so that a trust can use the virtual
   it("allows an admin to add a trust", () => {
     GivenIAmLoggedInAsAnAdmin();
     WhenIClickOnViewAllTrusts();
-<<<<<<< HEAD
     ThenISeeAListOfTrusts();
-=======
->>>>>>> chore: create retrieveOrganization tests
     WhenIClickOnAddATrust();
     ThenISeeTheAddATrustForm();
 
@@ -43,10 +40,7 @@ describe("As an admin, I want to add a trust so that a trust can use the virtual
   it("displays errors when fields have been left blank", () => {
     GivenIAmLoggedInAsAnAdmin();
     WhenIClickOnViewAllTrusts();
-<<<<<<< HEAD
     ThenISeeAListOfTrusts();
-=======
->>>>>>> chore: create retrieveOrganization tests
     WhenIClickOnAddATrust();
     ThenISeeTheAddATrustForm();
 
