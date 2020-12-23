14f446516b43e0eaa0d8ec84b3da6d2c95f92b48
nax2uk <azlinayeo@gmail.com>
2020-12-23 11:24:14 +0000

    chore: added FormHeading

--- src/components/FormHeading/index.js
+++ src/components/FormHeading/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-const FormHeading = ({ children }) => <h2>{children}</h2>;
+const FormHeading = ({ children }) => (
+  <h2 className="nhsuk-heading-l" id="tm-form-heading">
+    {children}
+  </h2>
+);
 
 export default FormHeading;
