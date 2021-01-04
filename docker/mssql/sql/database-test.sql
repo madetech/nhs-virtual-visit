9d194949d9277a2c1151b9f8ce6736a75ff35032
paulolancao <paulo.lancao@madetech.com>
2021-01-04 19:08:11 +0000

    fix: subject: fix test github workflow

--- docker/mssql/sql/database-test.sql
+++ docker/mssql/sql/database-test.sql
@@ -1,5 +1,3 @@
-ALTER AUTHORIZATION ON DATABASE::nhs_virtual_visit_test TO sa;
-
 IF EXISTS (SELECT * FROM sys.databases WHERE name = 'nhs_virtual_visit_test')
 BEGIN
     DROP DATABASE nhs_virtual_visit_test;  
