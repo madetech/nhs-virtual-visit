11a7ea7cf52d77d8dd9268806a27106808162817
paulolancao <paulo.lancao@madetech.com>
2021-01-04 20:46:15 +0000

    fix: subject: fix test github workflow

--- docker/mssql/sql/entrypoint-test.sh
+++ docker/mssql/sql/entrypoint-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # run the database script to create the DB
-/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P P@55w0rd -i ./database-test.sql
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P abcdef -i ./database-test.sql
