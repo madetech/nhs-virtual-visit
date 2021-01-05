a9b45e3afb10eccc61f98c0b881c280a8b69800a
paulolancao <paulo.lancao@madetech.com>
2021-01-05 14:26:04 +0000

    fix: subject: update mssql test server script

--- bin/setup_test_mssql_docker.sh
+++ bin/setup_test_mssql_docker.sh
@@ -13,6 +13,7 @@ docker build -t mssql-test:2017-GA-ubuntu docker/mssql/test
 # Run container scripts
 docker run --name=nhs-virtual-visit-test -p 1433:1433 -d mssql-test:2017-GA-ubuntu
 
+# Microsoft docs advise 45s
 wait_time=15s
 
 # Wait for PostgreSQL Server to come up.
