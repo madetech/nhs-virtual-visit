51f064b584d147487263f39c2e7e249db306cf70
Richard Pentecost <richard.pentecost@madetech.com>
2021-01-05 13:44:50 +0000

    chore: insert manager user into user table

--- db/migrations/mssql/sqls/20210105000010-insert-manager-to-user-table.sql
+++ db/migrations/mssql/sqls/20210105000010-insert-manager-to-user-table.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 INSERT INTO dbo.[user] ([email], [password], [type], [organisation_id], [status]) 
 	VALUES('nhs-manager11@nhs.co.uk', '$2b$10$Kwzuu07E.lx6ezpK58RYGuBZtrd9ULd5PsCPJXbe3BlN7Sax.rrmS', 'manager', 1, 1);
 INSERT INTO dbo.[user] ([email], [password], [type], [organisation_id], [status]) 
@@ -6,3 +7,9 @@ INSERT INTO dbo.[user] ([email], [password], [type], [organisation_id], [status]
 	VALUES('nhs-manager13@nhs.co.uk', '$2b$10$Kwzuu07E.lx6ezpK58RYGuBZtrd9ULd5PsCPJXbe3BlN7Sax.rrmS', 'manager', 1, 1);
 INSERT INTO dbo.[user] ([email], [password], [type], [organisation_id], [status]) 
 	VALUES('nhs-manager14@nhs.co.uk', '$2b$10$Kwzuu07E.lx6ezpK58RYGuBZtrd9ULd5PsCPJXbe3BlN7Sax.rrmS', 'manager', 1, 1);	
+=======
+USE nhs_virtual_visit_dev
+
+INSERT INTO dbo.[user] ([email], [password], [type], [organisation_id], [status]) 
+	VALUES('nhs-manager@nhs.co.uk', '$2b$10$Kwzuu07E.lx6ezpK58RYGuBZtrd9ULd5PsCPJXbe3BlN7Sax.rrmS', 'manager', 1, 1);
+>>>>>>> chore: insert manager user into user table
