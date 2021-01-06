3a02afbf4ad6aa2c1371c1964c26308ae8d1935b
Richard Pentecost <richard.pentecost@madetech.com>
2021-01-06 17:09:49 +0000

    chore: reset password form working erroring when email address is not valid

---
+++ pages/reset-password/index.js
@@ -0,0 +1,98 @@
+import React, { useState, useRef } from "react";
+import Layout from "../../src/components/Layout";
+import Heading from "../../src/components/Heading";
+import ErrorSummary from "../../src/components/ErrorSummary";
+import Form from "../../src/components/Form";
+import FormGroup from "../../src/components/FormGroup";
+import Label from "../../src/components/Label";
+import Input from "../../src/components/Input";
+import Button from "../../src/components/Button";
+import Text from "../../src/components/Text";
+import { GridRow, GridColumn } from "../../src/components/Grid";
+// import fetchEndpointWithCorrelationId from '../../src/helpers/fetchEndpointWithCorrelationId';
+
+const ResetPassword = () => {
+  const [errors, setErrors] = useState([]);
+
+  const emailRef = useRef();
+
+  const hasError = (field) =>
+    errors.find((error) => error.id === `${field}-error`);
+
+  const errorMessage = (field) => {
+    const error = errors.filter((err) => err.id === `${field}-error`);
+    return error.length === 1 ? error[0].message : "";
+  };
+
+  const onSubmit = async () => {
+    const onSubmitErrors = [];
+
+    const email = emailRef.current.value;
+
+    if (!email) {
+      onSubmitErrors.push({
+        id: "email-error",
+        message: "Enter an email",
+      });
+    }
+
+    if (onSubmitErrors.length === 0) {
+      const body = JSON.stringify({ email });
+      const response = await fetch("/api/verify-email", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body,
+      });
+
+      if (response.status === 201) {
+        // Send email
+        console.log("An email has been sent to reset your password");
+      } else {
+        onSubmitErrors.push({
+          id: "email-invalid-error",
+          message: "The email you entered was not recognised",
+        });
+      }
+    }
+
+    setErrors(onSubmitErrors);
+    return false;
+  };
+
+  return (
+    <Layout title="Reset Password" hasErrors={errors.length > 0}>
+      <GridRow>
+        <GridColumn>
+          <ErrorSummary errors={errors} />
+          <Heading>Reset Password</Heading>
+          <Text>
+            To reset password, type in your email and a link will be sent to
+            your email address
+          </Text>
+          <Form onSubmit={onSubmit}>
+            <FormGroup>
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                ref={emailRef}
+                hasError={hasError("email")}
+                errorMessage={errorMessage("email")}
+                className="nhsuk-input--width-20"
+                name="email"
+              />
+            </FormGroup>
+            <Button className="nhsuk-u-margin-top-5" type="submit">
+              Reset Password
+            </Button>
+          </Form>
+        </GridColumn>
+        <span style={{ clear: "both", display: "block" }}></span>
+      </GridRow>
+    </Layout>
+  );
+};
+
+export default ResetPassword;
