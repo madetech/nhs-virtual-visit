eb913083b1f9322e956e5c1b1aa59b256cf031e7
Richard Pentecost <richard.pentecost@madetech.com>
2021-01-11 15:34:19 +0000

    chore: success pages for sending email and resetting password done

--- pages/reset-password/send-email-success.js
+++ pages/reset-password/send-email-success.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { GridRow, GridColumn } from "../../src/components/Grid";
+import PanelSuccess from "../../src/components/PanelSuccess";
+import Layout from "../../src/components/Layout";
 
-const SendEmailSuccess = () => {
-  return <div>Email has been sent for reset password</div>;
-};
+const SendEmailSuccess = ({ email }) => (
+  <Layout title="Reset Password Link email has been sent">
+    <GridRow>
+      <GridColumn>
+        <PanelSuccess
+          name={`Email`}
+          action={`sent to ${email}`}
+          subAction="Check your email for the link to reset your password"
+        />
+      </GridColumn>
+    </GridRow>
+  </Layout>
+);
 
 export default SendEmailSuccess;
+
+export const getServerSideProps = ({ query }) => {
+  const email = query.email;
+  return {
+    props: {
+      email,
+    },
+  };
+};
