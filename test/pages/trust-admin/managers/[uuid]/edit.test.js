831f73aca720da86244e41883082be966adef5a6
nax2uk <azlinayeo@gmail.com>
2021-01-11 20:44:29 +0000

    chore: added test for trust-admin/managers/[uuid]/edit.test

--- test/pages/trust-admin/managers/[uuid]/edit.test.js
+++ test/pages/trust-admin/managers/[uuid]/edit.test.js
@@ -1,9 +1,7 @@
-import { getServerSideProps } from "pages/trust-admin/managers/index";
+import { getServerSideProps } from "pages/trust-admin/managers/[uuid]/edit";
 import { TRUST_ADMIN } from "src/helpers/userTypes";
 
-describe("trust-admin/managers", () => {
-  const trustId = 1;
-
+describe("trust-admin/managers/[uuid]/edit", () => {
   const anonymousReq = {
     headers: {
       cookie: "",
@@ -16,8 +14,9 @@ describe("trust-admin/managers", () => {
     },
   };
 
+  const trustId = 1;
   const tokenProvider = {
-    validate: jest.fn(() => ({ type: TRUST_ADMIN, trustId: trustId })),
+    validate: jest.fn(() => ({ type: TRUST_ADMIN, trustId })),
   };
 
   const retrieveTrustByIdSuccessStub = jest.fn(async () => ({
@@ -25,20 +24,13 @@ describe("trust-admin/managers", () => {
     error: null,
   }));
 
-  const managersArray = [
-    {
-      uuid: "1BBE43B3-4B2E-443E-8399-8299F22AB139",
-      email: "nhs-manager1@nhs.co.uk",
-      status: "active",
-    },
-    {
-      uuid: "F8F800FE-7A7E-4419-BA6F-7EFDD7871331",
-      email: "nhs-manager2@nhs.co.uk",
-      status: "disabled",
-    },
-  ];
-  const retrieveManagersByOrgIdSuccessSpy = jest.fn(async () => ({
-    managers: managersArray,
+  const expectedManager = {
+    uuid: "1BBE43B3-4B2E-443E-8399-8299F22AB139",
+    email: "nhs-manager1@nhs.co.uk",
+    status: "active",
+  };
+  const retrieveManagerByUuidSuccessSpy = jest.fn(async () => ({
+    manager: expectedManager,
     error: null,
   }));
 
@@ -50,7 +42,7 @@ describe("trust-admin/managers", () => {
     };
     container = {
       getRetrieveTrustById: () => retrieveTrustByIdSuccessStub,
-      getRetrieveManagersByOrgId: () => retrieveManagersByOrgIdSuccessSpy,
+      getRetrieveManagerByUuid: () => retrieveManagerByUuidSuccessSpy,
       getTokenProvider: () => tokenProvider,
       getRegenerateToken: () => jest.fn().mockReturnValue({}),
     };
@@ -65,20 +57,19 @@ describe("trust-admin/managers", () => {
       });
     });
 
-    it("retrieves managers", async () => {
+    it("retrieves manager by uuid", async () => {
+      const uuid = "1BBE43B3-4B2E-443E-8399-8299F22AB139";
       const { props } = await getServerSideProps({
         req: authenticatedReq,
         res,
+        query: { uuid },
         container,
       });
-
-      expect(retrieveManagersByOrgIdSuccessSpy).toHaveBeenCalledWith(trustId);
-      expect(props.managers.length).toEqual(2);
-      props.managers.forEach((manager, idx) => {
-        expect(manager.uuid).toEqual(managersArray[idx].uuid);
-        expect(manager.email).toEqual(managersArray[idx].email);
-        expect(manager.status).toEqual(managersArray[idx].status);
-      });
+      const actualManager = props.manager;
+      expect(retrieveManagerByUuidSuccessSpy).toHaveBeenCalledWith(uuid);
+      expect(actualManager.uuid).toEqual(expectedManager.uuid);
+      expect(actualManager.email).toEqual(expectedManager.email);
+      expect(actualManager.status).toEqual(expectedManager.status);
     });
   });
 });
