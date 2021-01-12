787ec7ea5c368f122ed881036c18747635ae25b8
nax2uk <azlinayeo@gmail.com>
2021-01-12 23:10:21 +0000

    chore: uninstall babel plugin and made relevant changes

--- test/pages/trust-admin/managers/[uuid]/archive-confirmation.test.js
+++ test/pages/trust-admin/managers/[uuid]/archive-confirmation.test.js
@@ -1,5 +1,5 @@
-import { getServerSideProps } from "pages/trust-admin/managers/[uuid]/archive-confirmation";
-import { TRUST_ADMIN } from "src/helpers/userTypes";
+import { getServerSideProps } from "../../../../../pages/trust-admin/managers/[uuid]/archive-confirmation";
+import { TRUST_ADMIN } from "../../../../../src/helpers/userTypes";
 
 describe("trust-admin/managers/[uuid]/archive-confirmation", () => {
   let res;
