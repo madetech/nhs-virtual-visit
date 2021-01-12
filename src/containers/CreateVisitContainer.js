7864d8c39df2864e78fda5a628a1fe40ce0aa764
Miranda Hawkes <17408278+miranda-hawkes@users.noreply.github.com>
2021-01-12 18:30:16 +0000

    Fix compilation

--- src/containers/CreateVisitContainer.js
+++ src/containers/CreateVisitContainer.js
@@ -1,6 +1,6 @@
 import userIsAuthenticated from "../usecases/userIsAuthenticated";
 import retrieveTrustById from "../gateways/retrieveTrustById";
-import retrieveWardById from "../gateways/retrieveWardById";
+import retrieveWardById from "../gateways/PostgreSQL/retrieveWardById";
 import createVisit from "../usecases/createVisit";
 import CallIdProvider from "../providers/CallIdProvider";
 import RandomIdProvider from "../providers/RandomIdProvider";
