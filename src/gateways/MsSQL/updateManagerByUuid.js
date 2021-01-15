fe034c742c29f2bd38d4ee6aceca79eab32c818c
nax2uk <azlinayeo@gmail.com>
2021-01-15 20:46:20 +0000

    chore: added unit test for updateManagerByUuid and changed name to updateManagerStatusByUUid

--- src/gateways/MsSQL/updateManagerByUuid.js
+++
@@ -1,9 +0,0 @@
-const updateManagerByUuidGateway = async (db, uuid, status) => {
-  await db
-    .request()
-    .input("uuid", uuid)
-    .input("status", status)
-    .query("UPDATE dbo.[user] SET status = @status WHERE uuid = @uuid");
-};
-
-export default updateManagerByUuidGateway;
