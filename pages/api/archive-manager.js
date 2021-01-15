17623613bf44d268ad19fbd8c2643b3ace160c3b
nax2uk <azlinayeo@gmail.com>
2021-01-15 15:52:18 +0000

    chore: removed console.logs

--- pages/api/archive-manager.js
+++ pages/api/archive-manager.js
@@ -24,8 +24,6 @@ export default withContainer(
 
     res.setHeader("Content-Type", "application/json");
 
-    console.log("*********BODY.UUID********");
-    console.log(body.uuid);
     const archiveManagerByUuid = container.getArchiveManagerByUuid();
 
     const { error } = await archiveManagerByUuid(body.uuid);
