b5c85facb09bbdf4e7b2fcf0be801732067eb70a
nax2uk <azlinayeo@gmail.com>
2021-01-16 15:47:27 +0000

    chore: added unit test for archiveManagerByUuid gateway

---
+++ test/gateways/MsSQL/archiveManagerByUuid.test.js
@@ -0,0 +1,55 @@
+import archiveManagerByUuidGateway from "../../../src/gateways/MsSQL/archiveManagerByUuid";
+
+// !!!!!! this is a mocked database !!!!!
+describe("archiveManagerByUuidGateway", () => {
+  const expectedUuid = "abc";
+  const inputSpy = jest.fn().mockReturnThis();
+  const request = jest.fn().mockReturnThis();
+  const querySpy = jest.fn().mockReturnValue({
+    recordset: [{ uuid: expectedUuid }],
+  });
+  let dbSpy;
+  beforeEach(() => {
+    dbSpy = {
+      request,
+      input: inputSpy,
+      query: querySpy,
+    };
+  });
+  it("deletes a managers status in the db when valid", async () => {
+    // Act
+    const actualUuid = await archiveManagerByUuidGateway(dbSpy, expectedUuid);
+    // Assert
+    expect(actualUuid).toEqual(expectedUuid);
+    expect(inputSpy).toHaveBeenCalledWith("uuid", expectedUuid);
+    expect(querySpy).toHaveBeenCalledWith(
+      "DELETE FROM dbo.[user] OUTPUT deleted.uuid WHERE uuid = @uuid"
+    );
+  });
+  it("throws an error if db is undefined", async () => {
+    // Arrange
+    dbSpy = undefined;
+
+    // Act && Assert
+    expect(
+      async () => await archiveManagerByUuidGateway(dbSpy, expectedUuid)
+    ).rejects.toThrow();
+  });
+
+  it("throws an error if uuid is undefined", async () => {
+    // Arrange
+    const undefinedUuid = undefined;
+    const queryUndefinedSpy = jest
+      .fn()
+      .mockReturnValue({ recordset: undefined });
+    dbSpy = {
+      ...dbSpy,
+      query: queryUndefinedSpy,
+    };
+
+    // Act && Assert
+    expect(
+      async () => await archiveManagerByUuidGateway(dbSpy, undefinedUuid)
+    ).rejects.toThrowError(TypeError);
+  });
+});
