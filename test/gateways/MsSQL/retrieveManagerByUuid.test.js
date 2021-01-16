d61a6a567e7b56f79993a92b0724dc1760542345
nax2uk <azlinayeo@gmail.com>
2021-01-16 15:37:00 +0000

    chore: addded unit test for retrieveManagerByUuid gateway

---
+++ test/gateways/MsSQL/retrieveManagerByUuid.test.js
@@ -0,0 +1,61 @@
+import retrieveManagerByUuidGateway from "../../../src/gateways/MsSQL/retrieveManagerByUuid";
+
+// !!!!!! this is a mocked database !!!!!
+describe("retrieveManagerByUuidGateway", () => {
+  const expectedUuid = "12SD";
+  const expectedManager = {
+    uuid: expectedUuid,
+    status: 1,
+  };
+  const inputSpy = jest.fn().mockReturnThis();
+  const request = jest.fn().mockReturnThis();
+  const querySpy = jest.fn().mockReturnValue({
+    recordset: [expectedManager],
+  });
+  let dbSpy;
+  beforeEach(() => {
+    dbSpy = {
+      request,
+      input: inputSpy,
+      query: querySpy,
+    };
+  });
+  it("retrieve manager in the db when valid", async () => {
+    // Act
+    const actualManager = await retrieveManagerByUuidGateway(
+      dbSpy,
+      expectedUuid
+    );
+    // Assert
+    expect(actualManager).toEqual(expectedManager);
+    expect(inputSpy).toHaveBeenCalledWith("uuid", expectedUuid);
+    expect(querySpy).toHaveBeenCalledWith(
+      "SELECT email, organisation_id, uuid, status FROM dbo.[user] WHERE uuid = @uuid"
+    );
+  });
+  it("throws an error if db is undefined", async () => {
+    // Arrange
+    dbSpy = undefined;
+
+    // Act && Assert
+    expect(
+      async () => await retrieveManagerByUuidGateway(dbSpy, expectedUuid)
+    ).rejects.toThrow();
+  });
+
+  it("throws an error if uuid is undefined", async () => {
+    // Arrange
+    const queryUndefinedSpy = jest
+      .fn()
+      .mockReturnValue({ recordset: undefined });
+    dbSpy = {
+      ...dbSpy,
+      query: queryUndefinedSpy,
+    };
+    const undefinedUuid = undefined;
+    // Act && Assert
+    expect(
+      async () => await retrieveManagerByUuidGateway(dbSpy, undefinedUuid)
+    ).rejects.toThrowError(TypeError);
+  });
+});
