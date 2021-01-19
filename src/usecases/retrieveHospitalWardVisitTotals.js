0b0e042eb8d6b4b113628c2c9ec516be31d6c8fb
Joe Roberts <jroberts@jroberts.me.uk>
2021-01-19 11:56:47 +0000

    Actual retrieveWardVisitTotals

--- src/usecases/retrieveHospitalWardVisitTotals.js
+++ src/usecases/retrieveHospitalWardVisitTotals.js
@@ -1,5 +1,27 @@
-export default ({ getRetrieveWardVisitTotalsGateway }) => async (hospitalId) => {
-  const { wards, sortedByTotalVisitsDescending } = await getRetrieveWardVisitTotalsGateway()(hospitalId);
+export default ({ getDb }) => async (hospitalId) => {
+  const db = await getDb();
+
+  const queryResult = await db.any(
+    `SELECT
+      wards.id as ward_id,
+      wards.name as ward_name,
+      COALESCE(SUM(totals.total),0) AS total_visits
+    FROM wards
+    LEFT JOIN ward_visit_totals AS totals ON wards.id = totals.ward_id
+    WHERE wards.hospital_id = $1 AND wards.archived_at IS NULL
+    GROUP BY wards.id`,
+    [hospitalId]
+  );
+
+  let wards = {};
+
+  queryResult.map(({ ward_id, total_visits }) => {
+    wards[ward_id] = parseInt(total_visits);
+  });
+
+  const sortedByTotalVisitsDescending = queryResult.sort(
+    (a, b) => b.total_visits - a.total_visits
+  );
 
   const mostVisitedWard = sortedByTotalVisitsDescending[0];
   const leastVisitedWard =
