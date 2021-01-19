cfc05683433f553904daccf78f74f8e0ef67c11a
nax2uk <azlinayeo@gmail.com>
2021-01-19 23:54:59 +0000

    chore: refactored unit test for hospital add-success page

--- test/pages/trust-admin/hospitals/[id]/add-success.test.js
+++ test/pages/trust-admin/hospitals/[id]/add-success.test.js
@@ -1,61 +1,58 @@
 import { getServerSideProps } from "../../../../../pages/trust-admin/hospitals/[id]/add-success";
 
-const authenticatedReq = {
-  headers: {
-    cookie: "token=123",
-  },
-};
-
-const tokenProvider = {
-  validate: jest.fn(() => ({ type: "trustAdmin", trustId: 1 })),
-};
-
 describe("/trust-admin/hospitals/[id]/add-success", () => {
-  const anonymousReq = {
+  // Arrange
+  const authenticatedReq = {
     headers: {
-      cookie: "",
+      cookie: "token=123",
     },
   };
-
-  let res;
-
+  const tokenProvider = {
+    validate: jest.fn(() => ({ type: "trustAdmin", trustId: 1 })),
+  };
+  const retrieveOrganisationByIdSpy = jest.fn(async () => ({
+    organisation: { name: "Doggo Trust" },
+    error: null,
+  }));
+  const retrieveHospitalByIdSpy = jest.fn().mockReturnValue({
+    hospital: {
+      id: 1,
+      name: "Northwick Park Hospital",
+    },
+    error: null,
+  });
+  let res, container;
   beforeEach(() => {
     res = {
       writeHead: jest.fn().mockReturnValue({ end: () => {} }),
+    };
+    container = {
+      getRetrieveOrganisationById: () => retrieveOrganisationByIdSpy,
+      getRetrieveHospitalById: () => retrieveHospitalByIdSpy,
+      getTokenProvider: () => tokenProvider,
+      getRegenerateToken: () => jest.fn().mockReturnValue({}),
     };
   });
 
   describe("getServerSideProps", () => {
     it("redirects to login page if not authenticated", async () => {
+      // Arrange
+      const anonymousReq = {
+        headers: {
+          cookie: "",
+        },
+      };
+      // Act
       await getServerSideProps({ req: anonymousReq, res });
 
       expect(res.writeHead).toHaveBeenCalledWith(302, {
         Location: "/trust-admin/login",
       });
     });
-
+    // Assert
     describe("with hospitalId parameter", () => {
       it("retrieves a hospital by the hospitalId parameter", async () => {
-        const retrieveTrustByIdSpy = jest.fn(async () => ({
-          trust: { name: "Doggo Trust" },
-          error: null,
-        }));
-
-        const retrieveHospitalByIdSpy = jest.fn().mockReturnValue({
-          hospital: {
-            id: 1,
-            name: "Northwick Park Hospital",
-          },
-          error: null,
-        });
-
-        const container = {
-          getRetrieveTrustById: () => retrieveTrustByIdSpy,
-          getRetrieveHospitalById: () => retrieveHospitalByIdSpy,
-          getTokenProvider: () => tokenProvider,
-          getRegenerateToken: () => jest.fn().mockReturnValue({}),
-        };
-
+        // Act
         await getServerSideProps({
           req: authenticatedReq,
           res,
@@ -64,31 +61,12 @@ describe("/trust-admin/hospitals/[id]/add-success", () => {
           },
           container,
         });
-
+        // Assert
         expect(retrieveHospitalByIdSpy).toHaveBeenCalledWith("hospital ID", 1);
       });
 
       it("set a hospital prop based on the retrieved hospital", async () => {
-        const retrieveTrustByIdSpy = jest.fn(async () => ({
-          trust: { name: "Doggo Trust" },
-          error: null,
-        }));
-
-        const retrieveHospitalByIdSpy = jest.fn().mockReturnValue({
-          hospital: {
-            id: 1,
-            name: "Northwick Park Hospital",
-          },
-          error: null,
-        });
-
-        const container = {
-          getRetrieveTrustById: () => retrieveTrustByIdSpy,
-          getRetrieveHospitalById: () => retrieveHospitalByIdSpy,
-          getTokenProvider: () => tokenProvider,
-          getRegenerateToken: () => jest.fn().mockReturnValue({}),
-        };
-
+        // Act
         const { props } = await getServerSideProps({
           req: authenticatedReq,
           res,
@@ -97,7 +75,7 @@ describe("/trust-admin/hospitals/[id]/add-success", () => {
           },
           container,
         });
-
+        // Assert
         expect(props.name).toEqual("Northwick Park Hospital");
       });
     });
