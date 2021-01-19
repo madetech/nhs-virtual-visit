1423d640d66bfb2d94c44a987e35c45535ca81e9
nax2uk <azlinayeo@gmail.com>
2021-01-19 12:38:46 +0000

    chore: added try catch block to catch errors from async calls

--- src/usecases/retrieveHospitalVisitTotals.js
+++ src/usecases/retrieveHospitalVisitTotals.js
@@ -1,13 +1,22 @@
 const retrieveHospitalVisitTotals = ({
   getRetrieveHospitalVisitTotalsGateway,
 }) => async (trustId) => {
-  var hospitals = await getRetrieveHospitalVisitTotalsGateway()(trustId);
+  try {
+    var hospitals = await getRetrieveHospitalVisitTotalsGateway()(trustId);
 
-  const usageListitemCount = 3;
-  const mostVisited = hospitals.slice(0, usageListitemCount);
-  const leastVisited = hospitals.slice(-usageListitemCount).reverse();
+    const usageListitemCount = 3;
+    const mostVisited = hospitals.slice(0, usageListitemCount);
+    const leastVisited = hospitals.slice(-usageListitemCount).reverse();
 
-  return { hospitals, mostVisited, leastVisited };
+    return { hospitals, mostVisited, leastVisited, error: null };
+  } catch (error) {
+    return {
+      hospitals: null,
+      mostVisited: null,
+      leastVisited: null,
+      error: "There has been error retrieving hospital visit totals",
+    };
+  }
 };
 
 export default retrieveHospitalVisitTotals;
