57a8783be83fc30d31bceeb892e2a35763274a6b
nax2uk <azlinayeo@gmail.com>
2021-01-20 23:16:04 +0000

    chore: added missing code for unit test for wards add page

--- test/pages/trust-admin/wards/add.test.js
+++ test/pages/trust-admin/wards/add.test.js
@@ -1,49 +1,76 @@
-import React from "react";
 import { getServerSideProps } from "../../../../pages/trust-admin/wards/add";
-import AddAWard from "../../../../pages/trust-admin/wards/add";
-import { render, screen, fireEvent } from "@testing-library/react";
-
+import { TRUST_ADMIN } from "../../../../src/helpers/userTypes";
 describe("/trust-admin/wards/add", () => {
-  const anonymousReq = {
-    headers: {
-      cookie: "",
-    },
-  };
-
   let res;
-
   beforeEach(() => {
     res = {
       writeHead: jest.fn().mockReturnValue({ end: () => {} }),
     };
   });
-
   describe("getServerSideProps", () => {
     it("redirects to login page if not authenticated", async () => {
+      // Arrange
+      const anonymousReq = {
+        headers: {
+          cookie: "",
+        },
+      };
+      // Act
       await getServerSideProps({ req: anonymousReq, res });
-
+      // Assert
       expect(res.writeHead).toHaveBeenCalledWith(302, {
         Location: "/trust-admin/login",
       });
     });
-  });
-
-  describe("AddAWard", () => {
-    it("does not throw a hospital error when default select hospital option", () => {
-      render(
-        <AddAWard
-          hospitals={[
-            { id: "1", name: "Adora Hospital" },
-            { id: "2", name: "Catra Hospital" },
-          ]}
-          hospitalId={1}
-          trust={{ name: "Trust Test" }}
-        />
-      );
-
-      fireEvent.click(screen.getByText("Add ward"));
-
-      expect(screen.queryAllByText(/Select a hospital/)).toEqual([]);
+    it("returns hospitals, hospitalId, organisation and error through props", async () => {
+      // Arrange
+      const orgId = 10;
+      const expectedHospitalId = 1;
+      const authenticatedReq = {
+        headers: {
+          cookie: "token=123",
+        },
+      };
+      const expectedOrganisationName = "Doggo Trust";
+      const expectedHospitals = [
+        { id: 1, name: "Hospital1", wards: [{ id: 1, name: "Ward 1" }] },
+        { id: 2, name: "Hospital2", wards: [{ id: 2, name: "Ward 2" }] },
+      ];
+      const tokenProvider = {
+        validate: jest.fn(() => ({ type: TRUST_ADMIN, trustId: orgId })),
+      };
+      const retrieveOrganisationByIdSpy = jest.fn(async () => ({
+        organisation: { name: expectedOrganisationName },
+        error: null,
+      }));
+      const retrieveHospitalsByTrustIdSpy = jest.fn().mockReturnValue({
+        hospitals: expectedHospitals,
+        error: null,
+      });
+      const container = {
+        getRetrieveOrganisationById: () => retrieveOrganisationByIdSpy,
+        getRetrieveHospitalsByTrustId: () => retrieveHospitalsByTrustIdSpy,
+        getTokenProvider: () => tokenProvider,
+        getRegenerateToken: () => jest.fn().mockReturnValue({}),
+      };
+      // Act
+      const {
+        props: { hospitals, hospitalId, organisation, error },
+      } = await getServerSideProps({
+        req: authenticatedReq,
+        res,
+        query: {
+          hospitalId: expectedHospitalId,
+        },
+        container,
+      });
+      // Assert
+      expect(retrieveOrganisationByIdSpy).toBeCalledWith(orgId);
+      expect(retrieveHospitalsByTrustIdSpy).toBeCalledWith(orgId);
+      expect(hospitals).toEqual(expectedHospitals);
+      expect(organisation.name).toEqual(expectedOrganisationName);
+      expect(hospitalId).toEqual(expectedHospitalId);
+      expect(error).toBeNull();
     });
   });
 });
