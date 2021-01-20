fc904668a555c900647150143bd4c39de7a74d9c
nax2uk <azlinayeo@gmail.com>
2021-01-20 00:13:42 +0000

    chore: refactored unit test for hospital id page

--- test/pages/trust-admin/hospitals/[id].test.js
+++ test/pages/trust-admin/hospitals/[id].test.js
@@ -1,17 +1,6 @@
 import { getServerSideProps } from "../../../../pages/trust-admin/hospitals/[id]";
 
 describe("trust-admin/hospitals/[id]", () => {
-  const anonymousReq = {
-    headers: {
-      cookie: "",
-    },
-  };
-
-  const authenticatedReq = {
-    headers: {
-      cookie: "token=123",
-    },
-  };
   let res;
 
   const trustId = 1;
@@ -28,30 +17,40 @@ describe("trust-admin/hospitals/[id]", () => {
 
   describe("getServerSideProps", () => {
     it("redirects to login page if not authenticated", async () => {
+      // Arrange
+      const anonymousReq = {
+        headers: {
+          cookie: "",
+        },
+      };
+      // Act
       await getServerSideProps({ req: anonymousReq, res });
-
+      // Assert
       expect(res.writeHead).toHaveBeenCalledWith(302, {
         Location: "/trust-admin/login",
       });
     });
 
     it("provides the trust, hospital and wards as props", async () => {
+      // Arrange
+      const authenticatedReq = {
+        headers: {
+          cookie: "token=123",
+        },
+      };
       const hospitalId = 1;
-
-      const TrustSpy = jest.fn(async () => ({
-        trust: { name: "Doggo Trust" },
+      const retrieveOrganisationByIdSpy = jest.fn(async () => ({
+        organisation: { name: "Doggo Trust" },
         error: null,
       }));
       const wardsSpy = jest.fn(async () => ({
         wards: [{ id: 1 }, { id: 2 }],
         error: null,
       }));
-
       const hospitalSpy = jest.fn(async () => ({
         hospital: { id: hospitalId, name: "Test Hospital" },
         error: null,
       }));
-
       const visitTotalsSpy = jest.fn().mockReturnValue({
         hospitals: [
           { id: 1, name: "Test Hospital", totalVisits: 10 },
@@ -60,15 +59,13 @@ describe("trust-admin/hospitals/[id]", () => {
         leastVisited: [{ id: 2, name: "Test Hospital", totalVisits: 3 }],
         mostVisited: [{ id: 1, name: "Test Hospital", totalVisits: 10 }],
       });
-
       const hospitalWardTotalsSpy = jest.fn().mockReturnValue({
         wards: { 1: 10, 2: 5 },
         mostVisited: { wardName: "Most Visited", total_visits: 10 },
         leastVisited: { wardName: "Least Visited", total_visits: 5 },
       });
-
       const container = {
-        getRetrieveTrustById: () => TrustSpy,
+        getRetrieveOrganisationById: () => retrieveOrganisationByIdSpy,
         getRetrieveWardsByHospitalId: () => wardsSpy,
         getRetrieveHospitalById: () => hospitalSpy,
         getRetrieveHospitalVisitTotals: () => visitTotalsSpy,
@@ -76,18 +73,17 @@ describe("trust-admin/hospitals/[id]", () => {
         getTokenProvider: () => tokenProvider,
         getRegenerateToken: () => jest.fn().mockReturnValue({}),
       };
-
+      // Act
       const { props } = await getServerSideProps({
         req: authenticatedReq,
         res,
         query: { id: hospitalId },
         container,
       });
-
+      // Assert
       expect(hospitalSpy).toHaveBeenCalledWith(hospitalId, trustId);
       expect(visitTotalsSpy).toHaveBeenCalledWith(trustId);
       expect(hospitalWardTotalsSpy).toHaveBeenCalledWith(hospitalId);
-
       expect(props.wards).toEqual([{ id: 1 }, { id: 2 }]);
       expect(props.hospital).toEqual({ id: hospitalId, name: "Test Hospital" });
       expect(props.totalBookedVisits).toEqual(10);
@@ -100,6 +96,7 @@ describe("trust-admin/hospitals/[id]", () => {
         total_visits: 5,
       });
       expect(props.wardVisitTotals).toEqual({ 1: 10, 2: 5 });
+      expect(props.error).toBeNull();
     });
   });
 });
