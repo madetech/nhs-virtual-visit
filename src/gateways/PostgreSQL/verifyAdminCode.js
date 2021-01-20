39204eb8bc4c199f54d8754dab6139e6c40a82d0
Joe Roberts <jroberts@jroberts.me.uk>
2021-01-20 18:07:33 +0000

    Gateway for verifyAdminCode

---
+++ src/gateways/PostgreSQL/verifyAdminCode.js
@@ -0,0 +1,47 @@
+import bcrypt from "bcryptjs";
+import logger from "../../../logger";
+
+export default ({ getDb }) => async (email, password) => {
+  const db = await getDb();
+
+  if (!password) {
+    return {
+      validAdminCode: false,
+      error: "password is not defined",
+    };
+  }
+
+  try {
+    const dbResponse = await db.any(
+      `SELECT id, password FROM admins WHERE email = $1 LIMIT 1`,
+      [email]
+    );
+
+    if (dbResponse.length > 0) {
+      const [admin] = dbResponse;
+
+      if (!bcrypt.compareSync(password, admin.password))
+        return {
+          validAdminCode: false,
+          error: "Incorrect email or password",
+        };
+
+      return {
+        validAdminCode: true,
+        error: null,
+      };
+    } else {
+      return {
+        validAdminCode: false,
+        error: null,
+      };
+    }
+  } catch (error) {
+    logger.error(error);
+
+    return {
+      validAdminCode: false,
+      error: error.toString(),
+    };
+  }
+};
