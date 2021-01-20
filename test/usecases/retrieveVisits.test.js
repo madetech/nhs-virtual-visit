129d2bb5177c65c26db365043ffd21ce09bebeac
Joe Roberts <jroberts@jroberts.me.uk>
2021-01-20 14:30:56 +0000

    Gateway for retrieveVisits

--- test/usecases/retrieveVisits.test.js
+++ test/usecases/retrieveVisits.test.js
@@ -3,37 +3,34 @@ import { SCHEDULED, COMPLETE } from "../../src/helpers/visitStatus";
 
 describe("retrieveVisits", () => {
   it("returns a json object containing the calls", async () => {
-    const anySpy = jest.fn(() => [
-      {
-        id: 1,
-        patient_name: "Bob",
-        call_time: new Date("2020-04-15T23:00:00.000Z"),
-        recipient_number: "07700900900",
-        recipient_email: "billy@bob.com",
-        recipient_name: "Billy",
-        call_id: "cb238rfv23cuv3",
-        provider: "whereby",
-        status: SCHEDULED,
-      },
-      {
-        id: 2,
-        patient_name: "Harry",
-        call_time: new Date("2020-04-15T23:00:00.000Z"),
-        recipient_number: "07700900900",
-        recipient_email: "bob@bob.com",
-        recipient_name: "Bob",
-        call_id: "cb238rfv23cuv3",
-        provider: "whereby",
-        status: COMPLETE,
-      },
-    ]);
-
     const container = {
-      async getDb() {
-        return {
-          any: anySpy,
-        };
-      },
+      getRetrieveVisitsGateway: () => async () => ({
+        scheduledCalls: [
+          {
+            id: 1,
+            patientName: "Bob",
+            callTime: "2020-04-15T23:00:00.000Z",
+            recipientNumber: "07700900900",
+            recipientEmail: "billy@bob.com",
+            recipientName: "Billy",
+            callId: "cb238rfv23cuv3",
+            provider: "whereby",
+            status: SCHEDULED,
+          },
+          {
+            id: 2,
+            patientName: "Harry",
+            callTime: "2020-04-15T23:00:00.000Z",
+            recipientNumber: "07700900900",
+            recipientEmail: "bob@bob.com",
+            recipientName: "Bob",
+            callId: "cb238rfv23cuv3",
+            provider: "whereby",
+            status: COMPLETE,
+          }
+        ],
+        error: null
+      })
     };
 
     const { scheduledCalls, error } = await retrieveVisits(container)({
@@ -41,11 +38,6 @@ describe("retrieveVisits", () => {
     });
 
     expect(error).toBeNull();
-    expect(anySpy).toHaveBeenCalledWith(expect.anything(), [
-      1,
-      "scheduled",
-      "complete",
-    ]);
     expect(scheduledCalls).toHaveLength(2);
     expect(scheduledCalls[0]).toEqual({
       id: 1,
@@ -72,36 +64,22 @@ describe("retrieveVisits", () => {
   });
 
   it("returns a query with a 12 hour interval", async () => {
-    const anySpy = jest.fn(() => []);
-
     const container = {
-      async getDb() {
-        return {
-          any: anySpy,
-        };
-      },
+      getRetrieveVisitsGateway: () => async () => ({
+        scheduledCalls: []
+      })
     };
 
     await retrieveVisits(container)({
       wardId: 1,
     });
-
-    expect(anySpy).toHaveBeenCalledWith(expect.anything(), [
-      1,
-      "scheduled",
-      "complete",
-    ]);
   });
 
   it("returns an error object on db exception", async () => {
     const container = {
-      async getDb() {
-        return {
-          any: jest.fn(() => {
-            throw new Error("DB Error!");
-          }),
-        };
-      },
+      getRetrieveVisitsGateway: () => async () => ({
+        error: "foo"
+      })
     };
 
     const { error } = await retrieveVisits(container)({ wardId: 1 });
