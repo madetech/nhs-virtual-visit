d4b015961597c9292342da548a734cef160f48ec
nax2uk <azlinayeo@gmail.com>
2021-01-21 20:35:33 +0000

    added createFacility gateway and usecases and updated relevant pages

--- src/gateways/MsSQL/createFacility.js
+++ src/gateways/MsSQL/createFacility.js
@@ -1,36 +1,14 @@
-import logger from "../../../logger";
-
-export default ({ getMsSqlConnPool }) => async ({
-  name,
-  organisationId,
-  createdBy,
-  code,
-  status,
-}) => {
-  logger.info(`Creating facility ${name}`);
-
-  try {
-    const db = await getMsSqlConnPool();
-    const response = await db
-      .request()
-      .input("name", name)
-      .input("organisationId", organisationId)
-      .input("createdBy", createdBy)
-      .input("code", code)
-      .input("status", status)
-      .query(
-        "INSERT INTO dbo.[facility] ([name], [organisation_id], [created_by], [code], [status] ) OUTPUT inserted.* VALUES (@name, @organisationId, @createdBy, @code, @status)"
-      );
-    logger.info(response.recordset[0]);
-    return {
-      facility: response.recordset[0],
-      error: null,
-    };
-  } catch (error) {
-    logger.error(`Error creating organisation ${name}, ${error}`);
-    return {
-      facility: null,
-      error: error.toString(),
-    };
-  }
+import mssql from "mssql";
+export default ({ getMsSqlConnPool }) => async (name, orgId, code, userId) => {
+  const db = await getMsSqlConnPool();
+  const res = await db
+    .request()
+    .input("name", mssql.NVarChar(255), name)
+    .input("orgId", mssql.Int, orgId)
+    .input("code", mssql.NVarChar(255), code)
+    .input("userId", mssql.Int, userId)
+    .query(
+      "INSERT INTO dbo.[facility] ([name], [organisation_id], [code], [created_by]) OUTPUT inserted.id VALUES (@name, @orgId, @code, @userId)"
+    );
+  return res.recordset[0].id;
 };
