0d46a4db382bdfcefd2793c976505d09d480a778
nax2uk <azlinayeo@gmail.com>
2021-01-21 20:34:55 +0000

    chore: added userId to token

--- src/gateways/MsSQL/verifyUserLogin.js
+++ src/gateways/MsSQL/verifyUserLogin.js
@@ -10,12 +10,14 @@ const verifyUserLogin = async (email, password) => {
       validUser: false,
       trust_id: null,
       type: null,
+      user_id: null,
       error: "email is not defined",
     };
   }
   if (!password) {
     return {
       validUser: false,
+      user_id: null,
       trust_id: null,
       type: null,
       error: "password is not defined",
@@ -27,21 +29,24 @@ const verifyUserLogin = async (email, password) => {
       .request()
       .input("email", email)
       .query(
-        `SELECT organisation_id, password, type FROM dbo.[user] WHERE email = @email`
+        `SELECT organisation_id, password, type, id FROM dbo.[user] WHERE email = @email`
       );
 
     if (dbResponse.recordset.length > 0) {
       const user = dbResponse.recordset[0];
+      console.log(user);
       if (!bcrypt.compareSync(password, user.password))
         return {
           validUser: false,
           trust_id: null,
           type: null,
+          user_id: null,
           error: "Incorrect email or password",
         };
 
       return {
         validUser: true,
+        user_id: user.id,
         trust_id: user.organisation_id,
         type: user.type,
         error: null,
@@ -50,6 +55,7 @@ const verifyUserLogin = async (email, password) => {
       return {
         validUser: false,
         trust_id: null,
+        user_id: null,
         type: null,
         error: null,
       };
@@ -59,6 +65,7 @@ const verifyUserLogin = async (email, password) => {
 
     return {
       validUser: false,
+      user_id: null,
       trust_id: null,
       type: null,
       error: error.toString(),
