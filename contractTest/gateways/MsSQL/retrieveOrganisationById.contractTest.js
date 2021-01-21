70ca94c676bb48bcb4b9a804325766665a2d31d5
nax2uk <azlinayeo@gmail.com>
2021-01-21 13:12:58 +0000

    chore: refactored unit test because retrieveOrganisationById gateway code changed

--- contractTest/gateways/MsSQL/retrieveOrganisationById.contractTest.js
+++ contractTest/gateways/MsSQL/retrieveOrganisationById.contractTest.js
@@ -3,16 +3,18 @@ import { setupOrganization } from "../../../test/testUtils/factories";
 import AppContainer from "../../../src/containers/AppContainer";
 
 describe("retrieveOrganisationById", () => {
+  // Arrange
   const container = AppContainer.getInstance();
   it("returns an object containing the trust", async () => {
+    // Arrange
     const {
       organisation: { id },
     } = await setupOrganization();
-
+    // Act
     const { organisation, error } = await retrieveOrganisationById(container)(
       id
     );
-
+    // Assert
     expect(error).toBeNull();
     expect(organisation).toEqual({
       id: id,
@@ -24,15 +26,18 @@ describe("retrieveOrganisationById", () => {
       uuid: organisation.uuid,
     });
   });
-
   it("returns an error when organisation not found for id", async () => {
+    // Act
     const { error } = await retrieveOrganisationById(container)(2000);
+    // Assert
     expect(error).toBeDefined();
     expect(error).toEqual("Organisation not found for id");
   });
 
   it("returns an error when organisation id not set", async () => {
+    // Act
     const { error } = await retrieveOrganisationById(container)();
+    // Assert
     expect(error).toBeDefined();
     expect(error).toEqual("Organisation not found for id");
   });
