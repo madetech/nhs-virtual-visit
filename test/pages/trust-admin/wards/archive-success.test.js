3b649df11c2e2a96eb609f39c25dde2702005490
nax2uk <azlinayeo@gmail.com>
2021-01-21 00:00:17 +0000

    chore: added missing code for unit test wards archive success

--- test/pages/trust-admin/wards/archive-success.test.js
+++ test/pages/trust-admin/wards/archive-success.test.js
@@ -1,27 +1,71 @@
 import { getServerSideProps } from "../../../../pages/trust-admin/wards/archive-success";
-
+import { TRUST_ADMIN } from "../../../../src/helpers/userTypes";
 describe("/trust-admin/wards/archive-success", () => {
-  const anonymousReq = {
-    headers: {
-      cookie: "",
-    },
-  };
-
   let res;
-
   beforeEach(() => {
     res = {
       writeHead: jest.fn().mockReturnValue({ end: () => {} }),
     };
   });
-
   describe("getServerSideProps", () => {
     it("redirects to login page if not authenticated", async () => {
+      // Arrange
+      const anonymousReq = {
+        headers: {
+          cookie: "",
+        },
+      };
+      // Act
       await getServerSideProps({ req: anonymousReq, res });
-
+      // Assert
       expect(res.writeHead).toHaveBeenCalledWith(302, {
         Location: "/trust-admin/login",
       });
+    });
+    it("returns hospitals, hospitalId, organisation and error through props", async () => {
+      // Arrange
+      const orgId = 10;
+      const expectedHospitalId = 1;
+      const expectedHospitalName = "Hospital1";
+      const expectedWardName = "Ward1";
+      const authenticatedReq = {
+        headers: {
+          cookie: "token=123",
+        },
+      };
+      const expectedOrganisationName = "Doggo Trust";
+      const tokenProvider = {
+        validate: jest.fn(() => ({ type: TRUST_ADMIN, trustId: orgId })),
+      };
+      const retrieveOrganisationByIdSpy = jest.fn(async () => ({
+        organisation: { name: expectedOrganisationName },
+        error: null,
+      }));
+      const container = {
+        getRetrieveOrganisationById: () => retrieveOrganisationByIdSpy,
+        getTokenProvider: () => tokenProvider,
+        getRegenerateToken: () => jest.fn().mockReturnValue({}),
+      };
+      // Act
+      const {
+        props: { name, hospitalName, hospitalId, organisation, error },
+      } = await getServerSideProps({
+        req: authenticatedReq,
+        res,
+        query: {
+          hospitalId: expectedHospitalId,
+          hospitalName: expectedHospitalName,
+          name: expectedWardName,
+        },
+        container,
+      });
+      // Assert
+      expect(retrieveOrganisationByIdSpy).toBeCalledWith(orgId);
+      expect(organisation.name).toEqual(expectedOrganisationName);
+      expect(hospitalId).toEqual(expectedHospitalId);
+      expect(hospitalName).toEqual(expectedHospitalName);
+      expect(name).toEqual(expectedWardName);
+      expect(error).toBeNull();
     });
   });
 });
