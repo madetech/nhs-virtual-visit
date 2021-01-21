7e254649db7bf1066fdf91046595f45769bb86e2
paulolancao <paulo.lancao@madetech.com>
2021-01-21 14:59:53 +0000

    Add unit tests for logger.js

--- test/pages/login.test.js
+++ test/pages/login.test.js
@@ -13,7 +13,7 @@ describe("login", () => {
   let res;
   beforeEach(() => {
     res = {
-      writeHead: jest.fn().mockReturnValue({ end: () => {} }),
+      writeHead: jest.fn().mockReturnValue({ end: () => { } }),
     };
   });
 
