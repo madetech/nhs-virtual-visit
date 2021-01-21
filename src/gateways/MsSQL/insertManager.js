adecbc0a9853e4a83f070fd97d1877db53ee18bf
Richard Pentecost <richard.pentecost@madetech.com>
2021-01-21 14:40:58 +0000

    create unit tests for create and insert manager usecase and gateway

--- src/gateways/MsSQL/insertManager.js
+++ src/gateways/MsSQL/insertManager.js
@@ -1,20 +1,36 @@
-const insertManagerGateway = ({ getMsSqlConnPool }) => async (
+import logger from "../../../logger";
+
+const insertManagerGateway = ({ getMsSqlConnPool }) => async ({
   email,
   password,
   organisationId,
-  type
-) => {
-  const db = await getMsSqlConnPool();
-  const res = await db
-    .request()
-    .input("email", email)
-    .input("password", password)
-    .input("type", type)
-    .input("organisationId", organisationId)
-    .query(
-      "INSERT INTO dbo.[user] ([email], [password], [type], [organisation_id]) OUTPUT inserted.* VALUES (@email, @password, @type, @organisationId)"
-    );
-  return res.recordset[0];
+  type,
+}) => {
+  logger.info(`Creating manager ${email}`);
+
+  try {
+    const db = await getMsSqlConnPool();
+    const res = await db
+      .request()
+      .input("email", email)
+      .input("password", password)
+      .input("type", type)
+      .input("organisationId", organisationId)
+      .query(
+        "INSERT INTO dbo.[user] ([email], [password], [type], [organisation_id]) OUTPUT inserted.* VALUES (@email, @password, @type, @organisationId)"
+      );
+
+    return {
+      user: res.recordset[0],
+      error: null,
+    };
+  } catch (error) {
+    logger.error(`Error creating manager ${email}, ${error}`);
+    return {
+      user: null,
+      error: error.toString(),
+    };
+  }
 };
 
 export default insertManagerGateway;
