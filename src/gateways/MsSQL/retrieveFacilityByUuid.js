d75bac208931abff95a41938fb81373f04cb9eab
nax2uk <azlinayeo@gmail.com>
2021-01-22 17:19:46 +0000

    chore: added UC and GW for retrieveFacilityByUuid

---
+++ src/gateways/MsSQL/retrieveFacilityByUuid.js
@@ -0,0 +1,14 @@
+import mssql from "mssql";
+
+export default ({ getMsSqlConnPool }) => async (uuid) => {
+  const db = await getMsSqlConnPool();
+  console.log("****ingateway***");
+  console.log(uuid);
+  const res = await db
+    .request()
+    .input("uuid", mssql.UniqueIdentifier, uuid)
+    .query(
+      "SELECT id, name, code, status, uuid FROM dbo.[facility] WHERE uuid=@uuid"
+    );
+  return res.recordset[0];
+};
