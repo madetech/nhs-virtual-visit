b21dad33e5aa45da6558884ebac2f29de4eb05df
nax2uk <azlinayeo@gmail.com>
2021-01-23 21:21:05 +0000

    chore: moved wards into hospitals folder

---
+++ pages/trust-admin/hospitals/[uuid]/wards/[id]/edit-success.js
@@ -0,0 +1,84 @@
+import React from "react";
+import Error from "next/error";
+import Layout from "../../../../../../src/components/Layout";
+import AnchorLink from "../../../../../../src/components/AnchorLink";
+import propsWithContainer from "../../../../../../src/middleware/propsWithContainer";
+import verifyTrustAdminToken from "../../../../../../src/usecases/verifyTrustAdminToken";
+import ActionLink from "../../../../../../src/components/ActionLink";
+import { TRUST_ADMIN } from "../../../../../../src/helpers/userTypes";
+import TrustAdminHeading from "../../../../../../src/components/TrustAdminHeading";
+import { GridRow, GridColumn } from "../../../../../../src/components/Grid";
+import PanelSuccess from "../../../../../../src/components/PanelSuccess";
+
+const EditAWardSuccess = ({
+  error,
+  name,
+  hospitalName,
+  hospitalId,
+  organisation,
+}) => {
+  if (error) {
+    return <Error />;
+  }
+
+  return (
+    <Layout
+      title={`${name} has been updated`}
+      showNavigationBar={true}
+      showNavigationBarForType={TRUST_ADMIN}
+    >
+      <TrustAdminHeading
+        trustName={organisation.name}
+        subHeading={hospitalName}
+      />
+      <GridRow>
+        <GridColumn width="two-thirds">
+          <PanelSuccess
+            name={`${name}`}
+            action={`updated`}
+            subAction={`for ${hospitalName}`}
+          />
+          <h2>What happens next</h2>
+          <ActionLink href={`/trust-admin/wards/add?hospitalId=${hospitalId}`}>
+            Add a ward for {hospitalName}
+          </ActionLink>
+          <p>
+            <AnchorLink
+              href="/trust-admin/hospitals/[id]"
+              as={`/trust-admin/hospitals/${hospitalId}`}
+            >
+              {`Return to ${hospitalName}`}
+            </AnchorLink>
+          </p>
+        </GridColumn>
+      </GridRow>
+    </Layout>
+  );
+};
+
+export const getServerSideProps = propsWithContainer(
+  verifyTrustAdminToken(async ({ container, query, authenticationToken }) => {
+    const orgId = authenticationToken.trustId;
+    const {
+      organisation,
+      error: organisationError,
+    } = await container.getRetrieveOrganisationById()(orgId);
+    const getRetrieveWardById = container.getRetrieveWardById();
+    const { ward, error: wardError } = await getRetrieveWardById(
+      query.id,
+      orgId
+    );
+
+    return {
+      props: {
+        error: organisationError || wardError,
+        name: ward.name,
+        hospitalName: ward.hospitalName,
+        hospitalId: ward.hospitalId,
+        organisation,
+      },
+    };
+  })
+);
+
+export default EditAWardSuccess;
---
+++ pages/trust-admin/hospitals/[uuid]/wards/[id]/edit.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import Error from "next/error";
+import { GridRow, GridColumn } from "../../../../src/components/Grid";
+import Layout from "../../../../src/components/Layout";
+import verifyTrustAdminToken from "../../../../src/usecases/verifyTrustAdminToken";
+import propsWithContainer from "../../../../src/middleware/propsWithContainer";
+import EditWardForm from "../../../../src/components/EditWardForm";
+import { TRUST_ADMIN } from "../../../../src/helpers/userTypes";
+import TrustAdminHeading from "../../../../src/components/TrustAdminHeading";
+
+const EditAWard = ({ organisation, error, ward, hospitals }) => {
+  if (error) {
+    return <Error />;
+  }
+
+  const [errors, setErrors] = useState([]);
+  const hospital = hospitals.find((hosp) => hosp.id == ward.hospitalId);
+
+  return (
+    <Layout
+      title="Edit a ward"
+      hasErrors={errors.length != 0}
+      showNavigationBar={true}
+      showNavigationBarForType={TRUST_ADMIN}
+    >
+      <TrustAdminHeading
+        trustName={organisation.name}
+        subHeading={hospital.name}
+      />
+      <GridRow>
+        <GridColumn width="two-thirds">
+          <EditWardForm
+            errors={errors}
+            setErrors={setErrors}
+            id={ward.id}
+            initialName={ward.name}
+            hospitalId={ward.hospitalId}
+            status={ward.status}
+          />
+        </GridColumn>
+      </GridRow>
+    </Layout>
+  );
+};
+
+export const getServerSideProps = propsWithContainer(
+  verifyTrustAdminToken(async ({ container, query, authenticationToken }) => {
+    const orgId = authenticationToken.trustId;
+    const getRetrieveWardById = container.getRetrieveWardById();
+    const {
+      organisation,
+      error: organisationError,
+    } = await container.getRetrieveOrganisationById()(orgId);
+    const { ward, error: wardError } = await getRetrieveWardById(
+      query.id,
+      orgId
+    );
+
+    const {
+      facilities,
+      error: facilitiesError,
+    } = await container.getRetrieveFacilitiesByOrgId()(orgId);
+
+    return {
+      props: {
+        error: organisationError || wardError || facilitiesError,
+        ward,
+        hospitals: facilities,
+        organisation,
+      },
+    };
+  })
+);
+
+export default EditAWard;
