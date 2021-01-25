ff286d59c09857139e7c4929f606a8f2dabfb7f1
nax2uk <azlinayeo@gmail.com>
2021-01-25 16:57:37 +0000

    chore: added unit tests for getRetrieveDepartmentsByFacilityId

--- __mocks__/src/containers/AppContainer.js
+++ __mocks__/src/containers/AppContainer.js
@@ -6,6 +6,9 @@ export default {
   getRetrieveFacilityByUuidGateway: jest.fn(() => () => Promise.resolve({})),
   getRetrieveDepartmentByUuidGateway: jest.fn(() => () => Promise.resolve({})),
   getRetrieveDepartmentByUuid: jest.fn(() => () => Promise.resolve({})),
+  getRetrieveDepartmentsByFacilityIdGateway: jest.fn(() => () =>
+    Promise.resolve({})
+  ),
   getUpdateDepartmentById: jest.fn(() => () => Promise.resolve({})),
   getUpdateDepartmentByIdGateway: jest.fn(() => () => Promise.resolve({})),
   getUpdateFacilityByIdGateway: jest.fn(() => () => Promise.resolve({})),
