6948c313608a49ae9df48e6f58c66c5e0365e828
Faissal Bensefia <faissal@madetech.com>
2021-01-26 14:40:07 +0000

    Add CaptureEvents gateway for MSSQL

---
+++ src/gateways/MsSQL/captureEvent.js
@@ -0,0 +1,39 @@
+import moment from "moment";
+import logger from "../../../logger";
+
+const captureEvent = ({ getMsSqlConnPool }) => async ({
+  action,
+  visitId,
+  callSessionId,
+}) => {
+  try {
+    const db = await getMsSqlConnPool();
+    const timestamp = moment().utc().toISOString();
+    const event = await db
+      .request()
+      .input("created_at", timestamp)
+      .input("action", action)
+      .input("scheduled_call_id", visitId)
+      .input("session_id", callSessionId)
+      .query(
+        "INSERT INTO dbo.[events] ([created_at], [action], [scheduled_call_id], [session_id]) OUTPUT inserted.id VALUES (@created_at, @action, @scheduled_call_id, @session_id)"
+      );
+
+    return {
+      event: {
+        id: event.recordset[0].id,
+        time: timestamp,
+        action,
+        visitId,
+        callSessionId,
+      },
+      error: null,
+    };
+  } catch (err) {
+    logger.error(err);
+    const message = `Failed to add ${action} event for visit ${visitId}`;
+    return { event: null, error: message };
+  }
+};
+
+export default captureEvent;
