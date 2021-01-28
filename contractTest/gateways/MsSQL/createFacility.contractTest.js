ae3ff1e0667c3cdba12ec549f0bb31a9454b5b25
nax2uk <azlinayeo@gmail.com>
2021-01-28 10:02:20 +0000

    chore: added more test cases for createFacility

--- contractTest/gateways/MsSQL/createFacility.contractTest.js
+++ contractTest/gateways/MsSQL/createFacility.contractTest.js
@@ -7,6 +7,10 @@ import AppContainer from "../../../src/containers/AppContainer";
 
 describe("createFacility", () => {
   const container = AppContainer.getInstance();
+  const facilityArgs = {
+    name: "Test Facility One",
+    code: "TF1",
+  };
   it("returns an object containing the facility", async () => {
     // Arrange
     const {
@@ -16,11 +20,6 @@ describe("createFacility", () => {
     const {
       user: { id: userId },
     } = await setUpManager({ organisationId: orgId, email });
-
-    const facilityArgs = {
-      name: "Test Facility One",
-      code: "TF1",
-    };
     const uuid = await createFacility(container)({
       ...facilityArgs,
       orgId,
@@ -30,7 +29,6 @@ describe("createFacility", () => {
     const { facility, error } = await container.getRetrieveFacilityByUuid(
       container
     )(uuid);
-    console.log(facility);
     // Assert
     expect(error).toBeNull();
     expect(facility).toEqual({
@@ -38,6 +36,82 @@ describe("createFacility", () => {
       id: facility.id,
       uuid,
       status: "active",
+    });
+  });
+  describe("throws an error", () => {
+    it("name is undefined", async () => {
+      // Arrange
+      const {
+        organisation: { id: orgId },
+      } = await setupOrganization();
+      const email = `${Math.random()}@nhs.co.uk`;
+      const {
+        user: { id: userId },
+      } = await setUpManager({ organisationId: orgId, email });
+      // Act && Assert
+      expect(
+        async () =>
+          await createFacility(container)({
+            ...facilityArgs,
+            name: undefined,
+            orgId,
+            createdBy: userId,
+          })
+      ).rejects.toThrow();
+    });
+    it("orgId is undefined", async () => {
+      // Arrange
+      const {
+        organisation: { id: orgId },
+      } = await setupOrganization();
+      const email = `${Math.random()}@nhs.co.uk`;
+      const {
+        user: { id: userId },
+      } = await setUpManager({ organisationId: orgId, email });
+      // Act && Assert
+      expect(
+        async () =>
+          await createFacility(container)({
+            ...facilityArgs,
+            orgId: undefined,
+            createdBy: userId,
+          })
+      ).rejects.toThrow();
+    });
+    it("code is undefined", async () => {
+      // Arrange
+      const {
+        organisation: { id: orgId },
+      } = await setupOrganization();
+      const email = `${Math.random()}@nhs.co.uk`;
+      const {
+        user: { id: userId },
+      } = await setUpManager({ organisationId: orgId, email });
+      // Act && Assert
+      expect(
+        async () =>
+          await createFacility(container)({
+            ...facilityArgs,
+            code: undefined,
+            orgId,
+            createdBy: userId,
+          })
+      ).rejects.toThrow();
+    });
+    it("createdBy is undefined", async () => {
+      // Arrange
+      const {
+        organisation: { id: orgId },
+      } = await setupOrganization();
+      // Act && Assert
+      expect(
+        async () =>
+          await createFacility(container)({
+            ...facilityArgs,
+            createdBy: undefined,
+            orgId,
+          })
+      ).rejects.toThrow();
     });
   });
 });
