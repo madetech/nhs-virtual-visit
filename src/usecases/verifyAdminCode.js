383f10ac819d123cfdeb5e2400c2fa6e463b0a09
Richard Pentecost <richard.pentecost@madetech.com>
2021-02-01 11:47:15 +0000

    verifyAdminCode usecase and unit test updated

--- src/usecases/verifyAdminCode.js
+++ src/usecases/verifyAdminCode.js
@@ -1,3 +1,27 @@
-export default ({ getVerifyAdminCodeGateway }) => async (email, password) => {
-  return await getVerifyAdminCodeGateway()(email, password);
+const verifyAdminCode = ({ getVerifyAdminCodeGateway }) => async (
+  email,
+  password
+) => {
+  if (!email) {
+    return {
+      validAdminCode: false,
+      error: "email is not defined",
+    };
+  }
+
+  if (!password) {
+    return {
+      validAdminCode: false,
+      error: "password is not defined",
+    };
+  }
+
+  const { validAdminCode, error } = await getVerifyAdminCodeGateway()(
+    email,
+    password
+  );
+
+  return { validAdminCode, error };
 };
+
+export default verifyAdminCode;
