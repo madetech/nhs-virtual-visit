f435a9d975ccadb0d548cb88dfecb859f7928d91
nax2uk <azlinayeo@gmail.com>
2021-02-03 12:22:35 +0000

    chore: added contract test

--- contractTest/gateways/MsSQL/updateVisitById.contractTest.js
+++ contractTest/gateways/MsSQL/updateVisitById.contractTest.js
@@ -13,9 +13,10 @@ describe("updateVisitByIdGateway", () => {
       departmentId,
     } = await setupOrganisationFacilityDepartmentAndManager();
     const { id: callId } = await setUpScheduledCall({ departmentId });
+    console.log(callId);
     const callTime = new Date(2021, 0, 27, 13, 37, 0, 0);
     const updatedVisit = {
-      id: callId,
+      callId,
       patientName: "Patient Updated",
       recipientEmail: "testupdated@testemail.com",
       recipientName: "Recipient Name Updated",
@@ -23,9 +24,10 @@ describe("updateVisitByIdGateway", () => {
       callTime,
     };
     // Act
-    const { error, visit } = await updateVisitByIdGateway(container)({
-      visit: updatedVisit,
-    });
+    const { error, visit } = await updateVisitByIdGateway(container)(
+      updatedVisit
+    );
+    console.log(visit);
     // Assert
     expect(visit.patientName).toEqual(updatedVisit.patientName);
     expect(visit.recipientEmail).toEqual(updatedVisit.recipientEmail);
@@ -33,5 +35,29 @@ describe("updateVisitByIdGateway", () => {
     expect(visit.recipientNumber).toEqual(updatedVisit.recipientNumber);
     expect(visit.callTime).toEqual(updatedVisit.callTime);
     expect(error).toBeNull();
+  });
+  it("returns an error if callId is undefined", async () => {
+    // Arrange
+    const {
+      departmentId,
+    } = await setupOrganisationFacilityDepartmentAndManager();
+    const { id: callId } = await setUpScheduledCall({ departmentId });
+    console.log(callId);
+    const callTime = new Date(2021, 0, 27, 13, 37, 0, 0);
+    const updatedVisit = {
+      patientName: "Patient Updated",
+      recipientEmail: "testupdated@testemail.com",
+      recipientName: "Recipient Name Updated",
+      recipientNumber: "07123456567",
+      callTime,
+    };
+    // Act
+    const { error, visit } = await updateVisitByIdGateway(container)(
+      updatedVisit
+    );
+
+    // Assert
+    expect(error).toBeDefined();
+    expect(visit).toBeNull();
   });
 });
