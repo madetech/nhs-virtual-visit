5a4dffda71fd52a48ba7bb787f9144ff1ce76298
nax2uk <azlinayeo@gmail.com>
2021-02-04 15:26:24 +0000

    removed Edit functionality from Managers Table

--- src/components/ManagersTable/index.js
+++ src/components/ManagersTable/index.js
@@ -10,9 +10,6 @@ const ManagersTable = ({ managers }) => (
           <th className="nhsuk-table__header" scope="col">
             Email
           </th>
-          <th className="nhsuk-table__header" scope="col">
-            Status
-          </th>
           <th className="nhsuk-table__header" scope="col" colSpan="2">
             <span className="nhsuk-u-visually-hidden">Actions</span>
           </th>
@@ -22,21 +19,6 @@ const ManagersTable = ({ managers }) => (
         {managers?.map((info) => (
           <tr key={info.uuid} className="nhsuk-table__row">
             <td className="nhsuk-table__cell">{info.email}</td>
-            <td className="nhsuk-table__cell">{info.status}</td>
-
-            <td className="nhsuk-table__cell">
-              <AnchorLink
-                className="nhsuk-link"
-                href={{
-                  pathname: `/trust-admin/managers/[uuid]/edit`,
-                  query: { uuid: info.uuid },
-                }}
-                as={`/trust-admin/managers/${info.uuid}/edit`}
-                data-cy="edit-tm-link"
-              >
-                Edit
-              </AnchorLink>
-            </td>
             <td className="nhsuk-table__cell">
               <AnchorLink
                 className="nhsuk-link"
