f1a215913dfd2c5b7cffb6f3bcdb4d5b06091349
Richard Pentecost <richard.pentecost@madetech.com>
2021-02-04 12:32:44 +0000

    retrieveScheduledCallById mssql gateway and contract tests

---
+++ src/gateways/MsSQL/retrieveScheduledCallById.js
@@ -0,0 +1,64 @@
+import logger from "../../../logger";
+import {
+  statusToId,
+  SCHEDULED,
+  COMPLETE,
+} from "../../../src/helpers/visitStatus";
+
+const retrieveScheduledCallByIdGateway = ({ getMsSqlConnPool }) => async ({
+  callId,
+  departmentId,
+}) => {
+  logger.info(`Retrieving scheduled call with id ${callId}`);
+
+  try {
+    const db = await getMsSqlConnPool();
+    const response = await db
+      .request()
+      .input("id", callId)
+      .input("departmentId", departmentId)
+      .input("scheduled", statusToId(SCHEDULED))
+      .input("complete", statusToId(COMPLETE))
+      .query(
+        `SELECT * from dbo.[scheduled_call] 
+          WHERE id = @id AND department_id = @departmentId
+          AND pii_cleared_out IS NULL
+          AND status = @scheduled
+          OR status = @complete`
+      );
+
+    const {
+      patient_name,
+      recipient_email,
+      recipient_name,
+      recipient_number,
+      call_time,
+      uuid,
+      id,
+      department_id,
+      status,
+    } = response.recordset[0];
+    return {
+      visit: {
+        id,
+        uuid,
+        patientName: patient_name,
+        recipientName: recipient_name,
+        recipientNumber: recipient_number,
+        recipientEmail: recipient_email,
+        callTime: call_time,
+        departmentId: department_id,
+        status,
+      },
+      error: null,
+    };
+  } catch (error) {
+    logger.error(`Error retrieving scheduled call with id ${callId}: ${error}`);
+    return {
+      visit: null,
+      error: error.toString(),
+    };
+  }
+};
+
+export default retrieveScheduledCallByIdGateway;
