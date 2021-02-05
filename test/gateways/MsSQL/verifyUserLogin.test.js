a50b1983d69efb183416612db11084ccf03bc38a
Richard Pentecost <richard.pentecost@madetech.com>
2021-02-05 14:34:49 +0000

    fixed failing unit tests

--- test/gateways/MsSQL/verifyUserLogin.test.js
+++ test/gateways/MsSQL/verifyUserLogin.test.js
@@ -1,51 +1,22 @@
-import verifyUserLogin from "../../../src/gateways/MsSQL/verifyUserLogin";
-import MsSQL from "../../../src/gateways/MsSQL";
+import verifyUserLoginGateway from "../../../src/gateways/MsSQL/verifyUserLogin";
 import bcrypt from "bcryptjs";
 
-jest.mock("../../../src/gateways/MsSQL");
 jest.mock("bcryptjs");
 
 describe("verifyUserLogin", () => {
-  it("errors if no password is present", async () => {
-    const email = "nhs-admin@nhs.co.uk";
-    const password = "";
-
-    const { validUser, trust_id, type, error } = await verifyUserLogin(
-      email,
-      password
-    );
-
-    expect(validUser).toEqual(false);
-    expect(trust_id).toBeNull();
-    expect(type).toBeNull();
-    expect(error).toEqual("password is not defined");
-  });
-
-  it("errors if no email is present", async () => {
-    const email = "";
-    const password = "password";
-
-    const { validUser, trust_id, type, error } = await verifyUserLogin(
-      email,
-      password
-    );
-
-    expect(validUser).toEqual(false);
-    expect(trust_id).toBeNull();
-    expect(type).toBeNull();
-    expect(error).toEqual("email is not defined");
-  });
-
-  it("resets the password with valid email and password", async () => {
+  it("verifies user if email and password are valid", async () => {
     const inputSpy = jest.fn().mockReturnThis();
-
-    MsSQL.getConnectionPool = jest.fn(() => {
+    const getMsSqlConnPool = jest.fn(() => {
       return {
         request: jest.fn().mockReturnThis(),
         input: inputSpy,
         query: jest.fn().mockReturnValue({
           recordset: [
-            { organisation_id: 1, type: "manager", password: "password" },
+            {
+              id: 1,
+              organisation_id: 1,
+              type: "manager",
+            },
           ],
         }),
       };
@@ -53,30 +24,34 @@ describe("verifyUserLogin", () => {
 
     bcrypt.compareSync.mockReturnValue(true);
 
-    const email = "nhs-admin@nhs.co.uk";
+    const email = "nhs-manager@nhs.co.uk";
     const password = "password";
 
-    const { validUser, trust_id, type, error } = await verifyUserLogin(
-      email,
-      password
-    );
+    const {
+      validUser,
+      trust_id,
+      user_id,
+      type,
+      error,
+    } = await verifyUserLoginGateway({
+      getMsSqlConnPool,
+    })(email, password);
 
     expect(validUser).toEqual(true);
     expect(trust_id).toEqual(1);
+    expect(user_id).toEqual(1);
     expect(type).toEqual("manager");
     expect(error).toBeNull();
-    expect(inputSpy).toHaveBeenCalledWith("email", "nhs-admin@nhs.co.uk");
+    expect(inputSpy).toHaveBeenCalledWith("email", "nhs-manager@nhs.co.uk");
   });
 
   it("returns an error if the wrong password is given", async () => {
-    MsSQL.getConnectionPool = jest.fn(() => {
+    const getMsSqlConnPool = jest.fn(() => {
       return {
         request: jest.fn().mockReturnThis(),
         input: jest.fn().mockReturnThis(),
         query: jest.fn().mockReturnValue({
-          recordset: [
-            { organisation_id: 1, type: "manager", password: "password" },
-          ],
+          recordset: [{ id: 1, organisation_id: 1, type: "manager" }],
         }),
       };
     });
@@ -86,19 +61,25 @@ describe("verifyUserLogin", () => {
     const email = "nhs-admin@nhs.co.uk";
     const wrongPassword = "wrongPassword";
 
-    const { validUser, trust_id, type, error } = await verifyUserLogin(
-      email,
-      wrongPassword
-    );
+    const {
+      validUser,
+      trust_id,
+      user_id,
+      type,
+      error,
+    } = await verifyUserLoginGateway({
+      getMsSqlConnPool,
+    })(email, wrongPassword);
 
     expect(validUser).toEqual(false);
     expect(trust_id).toBeNull();
+    expect(user_id).toBeNull();
     expect(type).toBeNull();
     expect(error).toEqual("Incorrect email or password");
   });
 
   it("errors if no records are found for a given email", async () => {
-    MsSQL.getConnectionPool = jest.fn(() => {
+    const getMsSqlConnPool = jest.fn(() => {
       return {
         request: jest.fn().mockReturnThis(),
         input: jest.fn().mockReturnThis(),
@@ -108,27 +89,33 @@ describe("verifyUserLogin", () => {
 
     bcrypt.compareSync.mockReturnValue(false);
 
-    const email = "nhs-admin@nhs.co.uk";
+    const email = "no-email@nhs.co.uk";
     const password = "password";
 
-    const { validUser, trust_id, type, error } = await verifyUserLogin(
-      email,
-      password
-    );
+    const {
+      validUser,
+      trust_id,
+      user_id,
+      type,
+      error,
+    } = await verifyUserLoginGateway({
+      getMsSqlConnPool,
+    })(email, password);
 
     expect(validUser).toEqual(false);
     expect(trust_id).toBeNull();
+    expect(user_id).toBeNull();
     expect(type).toBeNull();
-    expect(error).toBeNull();
+    expect(error).toEqual("Email does not exist in the database");
   });
 
   it("errors if there is a problem with the database call", async () => {
-    MsSQL.getConnectionPool = jest.fn(() => {
+    const getMsSqlConnPool = jest.fn(() => {
       return {
         request: jest.fn().mockReturnThis(),
         input: jest.fn().mockReturnThis(),
         query: jest.fn(() => {
-          throw new Error("DB Error!");
+          throw new Error();
         }),
       };
     });
@@ -136,14 +123,20 @@ describe("verifyUserLogin", () => {
     const email = "nhs-admin@nhs.co.uk";
     const password = "password";
 
-    const { validUser, trust_id, type, error } = await verifyUserLogin(
-      email,
-      password
-    );
+    const {
+      validUser,
+      trust_id,
+      user_id,
+      type,
+      error,
+    } = await verifyUserLoginGateway({
+      getMsSqlConnPool,
+    })(email, password);
 
     expect(validUser).toEqual(false);
     expect(trust_id).toBeNull();
+    expect(user_id).toBeNull();
     expect(type).toBeNull();
-    expect(error).toEqual("Error: DB Error!");
+    expect(error).toEqual("Email does not exist in the database");
   });
 });
