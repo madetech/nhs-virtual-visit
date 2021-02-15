26d6c39ace2618c2ccfd35d094ce07e950d4b56c
Joe Roberts <jroberts@jroberts.me.uk>
2021-02-15 13:05:21 +0000

    Basic terraform cloud setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,10 +5,20 @@ terraform {
       version = "2.47.0"
     }
   }
+
+  backend "remote" {
+    organization = "nhs-virtual-visit"
+
+    workspaces {
+      prefix = "nhs-virtual-visit-"
+    }
+  }
 }
 
 provider "azurerm" {
   features {}
+
+  subscription_id = "9d247a76-6114-4d06-8bd2-2be4bd6a0cdc"
 }
 
 resource "azurerm_resource_group" "rg" {
