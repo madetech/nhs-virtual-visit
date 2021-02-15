3f9d165410e0652fcffaf1f47a34873aa440cc87
Joe Roberts <jroberts@jroberts.me.uk>
2021-02-15 12:28:06 +0000

    Resource group

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,17 @@
-output "test" {
-  value = "Hello world"
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.47.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.location
+  name = "nhs-virtual-visit-${var.environment}"
 }
