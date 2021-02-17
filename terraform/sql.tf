31c6d6c7a853a25158875c46bb83ebf651112bf8
Joe Roberts <jroberts@jroberts.me.uk>
2021-02-17 09:28:31 +0000

    terraform fmt

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -1,9 +1,9 @@
 resource "azurerm_sql_server" "sql" {
-  name = "nhs-virtual-visits-${var.environment}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  version = "12.0"
-  administrator_login = var.azure_sql_username
+  name                         = "nhs-virtual-visits-${var.environment}"
+  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  version                      = "12.0"
+  administrator_login          = var.azure_sql_username
   administrator_login_password = var.azure_sql_password
 }
 
@@ -12,5 +12,5 @@ resource "azurerm_sql_database" "sql" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   server_name         = azurerm_sql_server.sql.name
-  edition = "Basic"
+  edition             = "Basic"
 }
