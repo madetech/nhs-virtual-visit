53f95d59fe301d7c0d9d3db3f5406907e810d213
nax2uk <azlinayeo@gmail.com>
2021-02-18 12:25:40 +0000

    WIP

---
+++ src/helpers/validateSignUpEmailAddress.js
@@ -0,0 +1,17 @@
+const ALPHANUMERIC =
+  "a-zA-Z0-9àèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ" + // Number and letters including accented characters
+  "\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\uFF00-\uFFEF\u4E00-\u9FAF\u2605-\u2606\u2190-\u2195\u203B"; // Chinese/Japanese/Korean characters
+
+const validateSignUpEmailAddress = (email) => {
+    const emailDomain = process.env.NODE_ENV === "production" ? process.env.SIGN_UP_EMAIL_DOMAIN_PROD: process.env.SIGN_UP_EMAIL_DOMAIN_DEV
+    const EMAIL_VALIDATION_STRING =
+    `^[${ALPHANUMERIC}.!#$%&'*+/=?^_\`{|}~-]` + 
+    `+@${emailDomain.toUpperCase()}`
+
+    const EMAIL_VALIDATION_REGEX = new RegExp(EMAIL_VALIDATION_STRING);
+    console.log(process.env.SIGN_UP_EMAIL_DOMAIN_PROD);
+    console.log(emailDomain);
+    return EMAIL_VALIDATION_REGEX.test(email.toUpperCase());
+};
+
+export default validateSignUpEmailAddress;
