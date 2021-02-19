74186b08a9f064bc8762c0a9e5e120a013af35ea
Joe Roberts <jroberts@jroberts.me.uk>
2021-02-19 14:29:10 +0000

    fix tests

--- src/helpers/validateSignUpEmailAddress.js
+++ src/helpers/validateSignUpEmailAddress.js
@@ -1,10 +1,13 @@
 const REGEX = /^(?:(?:[^<>()[\]\\.,;:\s@"]+(?:\.[^<>()[\]\\.,;:\s@"]+)*)|(?:".+"))@(?:[A-Za-z0-9-]+\.)*(\w+\.\w+)$/;
 
-export default (email) => {
-    if(process.env.NEXT_PUBLIC_SIGN_UP_EMAIL_DOMAIN === undefined)
-        return true;
-    const emailDomains = process.env.NEXT_PUBLIC_SIGN_UP_EMAIL_DOMAIN.split(",");
+export default (email, emailDomains) => {
+    if(emailDomains === undefined) {
+        if (process.env.NEXT_PUBLIC_SIGN_UP_EMAIL_DOMAIN === undefined)
+            return true;
+        emailDomains = process.env.NEXT_PUBLIC_SIGN_UP_EMAIL_DOMAIN.split(",");
+    }
     const matches = email.match(REGEX)
+    console.dir(matches)
     if(!matches)
         return false
     return emailDomains.includes(matches[1])
--- test/helpers/validateSignUpEmailAddress.test.js
+++ test/helpers/validateSignUpEmailAddress.test.js
@@ -1,5 +1,4 @@
 import validateSignUpEmailAddress from "../../src/helpers/validateSignUpEmailAddress";
-import { invalidEmailAddresses } from "./validateEmailAddress.test";
 
 const validFrontEmail = [
     "firstname.lastname@",
@@ -12,6 +11,39 @@ const validFrontEmail = [
     "bam_bam_özgur@",
     "test@trust."
 ]
+
+const invalidEmailAddresses = [
+  "email@[123.123.123.123]",
+  "plainaddress",
+  "@no-local-part.com",
+  "Outlook Contact <outlook-contact@domain.com>",
+  "no-at.domain.com",
+  "no-tld@domain",
+  ";beginning-semicolon@domain.co.uk",
+  "middle-semicolon@domain.co;uk",
+  "trailing-semicolon@domain.com;",
+  '"email+leading-quotes@domain.com',
+  'email+middle"-quotes@domain.com',
+  '"quoted-local-part"@domain.com',
+  '"quoted@domain.com"',
+  "lots-of-dots@domain..gov..uk",
+  "multiple@domains@domain.com",
+  "spaces in local@domain.com",
+  "spaces-in-domain@dom ain.com",
+  "underscores-in-domain@dom_ain.com",
+  "pipe-in-domain@example.com|gov.uk",
+  "comma,in-local@gov.uk",
+  "comma-in-domain@domain,gov.uk",
+  "pound-sign-in-local£@domain.com",
+  "local-with-’-apostrophe@domain.com",
+  "local-with-”-quotes@domain.com",
+  "domain-starts-with-a-dot@.domain.com",
+  "brackets(in)local@domain.com",
+  "emaill@.trust.nhs.uk",
+  "test@test.org",
+  "test@test.nhs.com",
+  "example@example.com",
+];
 
 const createValidEmailArray = (emailDomainString, validFrontEmail) =>{
     const emailDomainArray = emailDomainString ? emailDomainString.split(',') : []
