e9faec19c7625831a2c26df50566c35badb95a91
nax2uk <azlinayeo@gmail.com>
2021-02-19 12:36:03 +0000

    chore: refactored according to review comments.

--- src/helpers/validateSignUpEmailAddress.js
+++ src/helpers/validateSignUpEmailAddress.js
@@ -3,9 +3,9 @@ const EMAIL_DOMAIN_REGEX_STRING = emailDomain.replace(',','|').toUpperCase();
 const EMAIL_VALIDATION_STRING =
 `(?:[a-z0-9!#$%&'*+/=?^_\`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")` + 
 `+@([A-Za-z0-9\-]\.)*${EMAIL_DOMAIN_REGEX_STRING}`
-const EMAIL_VALIDATION_REGEX = new RegExp(EMAIL_VALIDATION_STRING,'i');
+const EMAIL_VALIDATION_REGEX = new RegExp(EMAIL_VALIDATION_STRING);
 
 const validateSignUpEmailAddress = (email) => {
-    return EMAIL_VALIDATION_REGEX.test(email);
+    return EMAIL_VALIDATION_REGEX.test(email.toUpperCase());
 };
 export default validateSignUpEmailAddress;
