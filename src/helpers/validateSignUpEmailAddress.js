f2f5985009b0cf0f8d75d2661efd3bd8b4438581
nax2uk <azlinayeo@gmail.com>
2021-02-19 11:07:30 +0000

    chore: refactored according to review comments.

--- src/helpers/validateSignUpEmailAddress.js
+++ src/helpers/validateSignUpEmailAddress.js
@@ -1,4 +1,4 @@
-const emailDomain = process.env.NEXT_PUBLIC_SIGN_UP_EMAIL_DOMAIN
+const emailDomain = process.env.NEXT_PUBLIC_SIGN_UP_EMAIL_DOMAIN || ".+";
 const EMAIL_DOMAIN_REGEX_STRING = emailDomain.replace(',','|').toUpperCase();
 const EMAIL_VALIDATION_STRING =
 `(?:[a-z0-9!#$%&'*+/=?^_\`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")` + 
