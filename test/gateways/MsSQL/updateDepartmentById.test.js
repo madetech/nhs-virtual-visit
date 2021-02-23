60838727cafcaaaaafd2fb3eea23ef05716e27d9
nax2uk <azlinayeo@gmail.com>
2021-02-23 11:36:21 +0000

    fix: failing unit tests

--- test/gateways/MsSQL/updateDepartmentById.test.js
+++ test/gateways/MsSQL/updateDepartmentById.test.js
@@ -1,11 +1,15 @@
 import updateDepartmentByIdGateway from "../../../src/gateways/MsSQL/updateDepartmentById";
 import mockAppContainer from "src/containers/AppContainer";
-
+import bcrypt from "bcryptjs";
+jest.mock("bcryptjs");
 describe("updateDepartmentByIdGateway", () => {
+  bcrypt.genSalt = jest.fn();
+  bcrypt.hashSync.mockReturnValue("hashedPin");
   const expectedDepartmentUuid = "uuid";
   const departmentObj = {
     id: 1,
     name: "wardNameOne",
+    pin: "4321"
   };
   it("updates department in the db when valid", async () => {
     // Arrange
@@ -26,16 +30,23 @@ describe("updateDepartmentByIdGateway", () => {
       expect.anything(),
       departmentObj.id
     );
-    expect(mockAppContainer.getMsSqlConnPool().input).toHaveBeenCalledTimes(2);
+    expect(mockAppContainer.getMsSqlConnPool().input).toHaveBeenCalledTimes(3);
     expect(mockAppContainer.getMsSqlConnPool().input).toHaveBeenNthCalledWith(
       2,
       "name",
       expect.anything(),
       departmentObj.name
     );
+    expect(mockAppContainer.getMsSqlConnPool().input).toHaveBeenNthCalledWith(
+      3,
+      "pin",
+      expect.anything(),
+      "hashedPin"
+    );
+
 
     expect(mockAppContainer.getMsSqlConnPool().query).toHaveBeenCalledWith(
-      "UPDATE dbo.[department] SET name = @name OUTPUT inserted.uuid WHERE id = @id"
+      "UPDATE dbo.[department] SET name = @name, pin = @pin OUTPUT inserted.uuid WHERE id = @id"
     );
   });
   it("throws an error if db is undefined", async () => {
