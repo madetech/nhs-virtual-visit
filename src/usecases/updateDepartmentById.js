2770539bbcc1c1c623c781d235f32223476115f0
nax2uk <azlinayeo@gmail.com>
2021-02-23 16:53:27 +0000

    chore: refactored edit ward pin functionality

--- src/usecases/updateDepartmentById.js
+++ src/usecases/updateDepartmentById.js
@@ -7,14 +7,13 @@ export default ({ getUpdateDepartmentByIdGateway }) => async ({ id, name, pin })
   if (name === undefined) {
     return { uuid: null, error: "department name must be provided." };
   }
-
+  let args = {  id, name }
+  if (pin) {
+    args = { ...args, pin }
+  }
   try {
     logger.info(`Updating department  ${name} with ${id}`);
-    const returnedUuid = await getUpdateDepartmentByIdGateway()({
-      id,
-      name,
-      pin
-    });
+    const returnedUuid = await getUpdateDepartmentByIdGateway()(args);
     return { uuid: returnedUuid, error: null };
   } catch (error) {
     return { uuid: null, error: "There was an error updating the department." };
