dc8d5744a821275407d761837a7e2bada1947aeb
nax2uk <azlinayeo@gmail.com>
2021-02-24 12:46:46 +0000

    added minimal Landing Page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,31 @@
 import React from "react";
+import Button from "../src/components/Button";
+import { GridRow, GridColumn } from "../src/components/Grid";
+import Heading from "../src/components/Heading";
+import Layout from "../src/components/Layout";
+import AnchorLink from "../src/components/AnchorLink";
+import Router from "next/router";
 
-function LoginRedirect() {
-  return <div></div>;
-}
+const LandingPage = () => {
+  const goToSignUpPage = () =>{
+    Router.push("/sign-up");
+  }
+  return (
+    <Layout title="NHS Virtual Visit">
+      <GridRow>
+        <GridColumn width="two-thirds">
+          <Heading>NHS Virtual Visit</Heading>
+          <p>Use this service to:</p>
+          <ul>
+            <li><AnchorLink href="/login"> Manage Your Trust</AnchorLink></li>
+            <li><AnchorLink href="/wards/login">Book a Virtual Visit</AnchorLink></li>
+          </ul>
+          <p>Registering takes around 5 minutes.</p>
+          <Button onClick={goToSignUpPage}>Start now</Button>
+        </GridColumn>
+      </GridRow>
+    </Layout>
+  );
+};
 
-export function getServerSideProps({ res }) {
-  res.writeHead(301, {
-    Location: "/wards/login",
-  });
-  res.end();
-
-  return { props: {} };
-}
-
-export default LoginRedirect;
+export default LandingPage;
