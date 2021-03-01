a1dd271bbc7da3d75f0fb06c86a578754800cbb7
Joe Roberts <jroberts@jroberts.me.uk>
2021-03-01 11:12:23 +0000

    Debug email sign up envvar

--- src/helpers/validateSignUpEmailAddress.js
+++ src/helpers/validateSignUpEmailAddress.js
@@ -1,6 +1,7 @@
 const REGEX = /^(?:(?:[^<>()[\]\\.,;:\s@"]+(?:\.[^<>()[\]\\.,;:\s@"]+)*)|(?:".+"))@(?:[A-Za-z0-9-]+\.)*(\w+\.\w+)$/;
 
 const validateSignUpEmailAddress = (email, emailDomains) => {
+    console.log(JSON.stringify(process.env));
     if(emailDomains === undefined) {
         if (process.env.NEXT_PUBLIC_SIGN_UP_EMAIL_DOMAIN === undefined)
             return true;
