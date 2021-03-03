63a6d2f9c477ee152a43abd7231054cd5758692b
Joe Roberts <jroberts@jroberts.me.uk>
2021-03-03 11:14:10 +0000

    Fix issue with cypress not being a dev dependency

--- pages/api/__coverage__.js
+++ pages/api/__coverage__.js
@@ -1,1 +1,6 @@
-module.exports = require('@cypress/code-coverage/middleware/nextjs')
+module.exports = (req, res) => {
+  // only GET is supported
+  res.status(200).json({
+    coverage: global.__coverage__ || null
+  })
+}
