44459e1a518e71e366e5cbdf5c7ca0af8b63e7cd
nax2uk <azlinayeo@gmail.com>
2021-03-04 13:10:31 +0000

    chore: moved updateDepartmentStatusById gateway to test/testUtils

--- test/testUtils/disableDepartment.js
+++ test/testUtils/disableDepartment.js
@@ -1,11 +1,12 @@
 import mssql from "mssql";
+import { DISABLED, statusToId } from "../../src/helpers/statusTypes";
 
-export default ({ getMsSqlConnPool }) => async ({ id, status }) => {
+export default ({ getMsSqlConnPool }) => async ({ id }) => {
   const db = await getMsSqlConnPool();
   const res = await db
     .request()
     .input("id", mssql.Int, id)
-    .input("status", mssql.TinyInt, status)
+    .input("status", mssql.TinyInt, statusToId(DISABLED))
     .query(
       "UPDATE dbo.[department] SET status = @status OUTPUT inserted.uuid WHERE id = @id"
     );
