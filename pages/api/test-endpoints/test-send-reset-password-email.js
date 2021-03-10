67fc641baf40f8cbd0c6cc24f1f3574c82a072f6
Richard Pentecost <richard.pentecost@madetech.com>
2021-03-10 10:41:00 +0000

    cadded console.logs in test endpoints

--- pages/api/test-endpoints/test-send-reset-password-email.js
+++ pages/api/test-endpoints/test-send-reset-password-email.js
@@ -4,6 +4,8 @@ import { validateHttpMethod } from "../../../src/helpers/apiErrorHandler";
 
 export default withContainer(
   async ({ headers, method }, res, { container }) => {
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.APP_ENV);
     if (process.env.NODE_ENV !== "e2e") {
       res.status(403);
       res.send(JSON.stringify({ error: "Can't access this endpoint from a production environment" }));
