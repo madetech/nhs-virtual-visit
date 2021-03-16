eac9a6239c637dcbfecd7592f11c2cd7f080a46a
nax2uk <azlinayeo@gmail.com>
2021-03-16 12:15:17 +0000

    chore: added back-up retentions

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -7,10 +7,20 @@ resource "azurerm_sql_server" "sql" {
   administrator_login_password = var.azure_sql_password
 }
 
-resource "azurerm_sql_database" "sql" {
-  name                = "virtualvisits"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name         = azurerm_sql_server.sql.name
-  edition             = "Basic"
+resource "azurerm_mssql_database" "sql" {
+  name      = "virtualvisits"
+  server_id = azurerm_sql_server.sql.id
+  sku_name  = var.azure_sql_sku_name
+  max_size_gb = var.azure_sql_max_size_gb
+
+  long_term_retention_policy {
+    weekly_retention  = var.azure_sql_weekly_retention
+    monthly_retention = var.azure_sql_monthly_retention
+    yearly_retention  = var.azure_sql_yearly_retention
+    week_of_year      = var.azure_sql_week_of_year
+  }
+
+  short_term_retention_policy {
+    retention_days = var.azure_sql_retention_days
+  }
 }
