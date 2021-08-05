95feb01363b776e0024e823aacf57affd46ca020
Micheal Wilkinson <micheal.wilkinson@madetech.com>
2021-08-05 14:58:14 +0100

    Updated database setup script to handle db creation

--- bin/setup_local_mssql_docker.sh
+++ bin/setup_local_mssql_docker.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+function createDB() {
+  databaseName="${1}"
+  username="${2}"
+  password="${3}"
+  docker exec -it nhs-virtual-visit /opt/mssql-tools/bin/sqlcmd -S localhost -U ${username} -P ${password} -Q "IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = '${databaseName}')
+    BEGIN
+      CREATE DATABASE ${databaseName}
+  END"
+}
 
 if [[ "$OSTYPE" == "linux-gnu"* && $EUID != 0 ]]
 then
@@ -8,7 +18,7 @@ else
 fi
 
 # Start docker yml from path and detached.
-docker-compose -f docker/mssql/local/docker-compose.yml up -d
+$commandPrefix docker-compose -f docker/mssql/local/docker-compose.yml up -d
 
 wait_time=15s
 
@@ -16,6 +26,14 @@ wait_time=15s
 echo waiting database to start in $wait_time...
 sleep $wait_time
 echo database started...
+
+# Create Databases
+if [ -f .env ]; then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+createDB ${MSQL_DB_NAME} ${MSQL_DB_USER} ${MSQL_DB_PASSWORD}
+createDB ${MSQL_TEST_DB_NAME} ${MSQL_TEST_DB_USER} ${MSQL_TEST_DB_PASSWORD}
 
 # Run create tables scripts.
 npm run dbmigratedev up:mssql
