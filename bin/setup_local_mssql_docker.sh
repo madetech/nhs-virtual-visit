a985c3e11980a90d8001be35eddcbe45a92e7401
MichealWilkinson-MT <88385899+MichealWilkinson-MT@users.noreply.github.com>
2021-08-09 10:30:32 +0100

    Npm audit (#6)
    * Run NPM audit against node 14
    
    * Updated database setup script to handle db creation
    
    * Updated node version for tests
    
    * Create docker test db
    
    * Make script executable
    
    * Correct version list
    
    Co-authored-by: Micheal Wilkinson <michealwilkinson@Micheals-MBP.hrafn.xyz>

--- bin/setup_local_mssql_docker.sh
+++ bin/setup_local_mssql_docker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [[ "$OSTYPE" == "linux-gnu"* && $EUID != 0 ]]
 then
@@ -7,8 +7,10 @@ else
 	commandPrefix=
 fi
 
+containerName="nhs-virtual-visit"
+
 # Start docker yml from path and detached.
-docker-compose -f docker/mssql/local/docker-compose.yml up -d
+$commandPrefix docker-compose -f docker/mssql/local/docker-compose.yml up -d
 
 wait_time=15s
 
@@ -16,6 +18,14 @@ wait_time=15s
 echo waiting database to start in $wait_time...
 sleep $wait_time
 echo database started...
+
+# Create Databases
+if [ -f .env ]; then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+bin/createDatabaseInDocker.sh ${containerName} ${MSQL_DB_NAME} ${MSQL_DB_USER} ${MSQL_DB_PASSWORD}
+bin/createDatabaseInDocker.sh ${containerName} ${MSQL_TEST_DB_NAME} ${MSQL_TEST_DB_USER} ${MSQL_TEST_DB_PASSWORD}
 
 # Run create tables scripts.
 npm run dbmigratedev up:mssql
