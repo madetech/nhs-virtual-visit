4df200e47110185b2c4ced8e7d071b3b87f414ac
MichealWilkinson-MT <88385899+MichealWilkinson-MT@users.noreply.github.com>
2021-08-11 15:58:29 +0100

    Update createDatabaseInDocker.sh
    We don't have an interactive terminal in github actions

--- bin/createDatabaseInDocker.sh
+++ bin/createDatabaseInDocker.sh
@@ -3,7 +3,7 @@ containerName="${1}"
 databaseName="${2}"
 username="${3}"
 password="${4}"
-docker exec -it ${containerName} /opt/mssql-tools/bin/sqlcmd -S localhost -U ${username} -P ${password} -Q "IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = '${databaseName}')
+docker exec ${containerName} /opt/mssql-tools/bin/sqlcmd -S localhost -U ${username} -P ${password} -Q "IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = '${databaseName}')
   BEGIN
     CREATE DATABASE ${databaseName}
 END"
