93189a9b5e0365bd07a05547a7b29cd80b929e58
MichealWilkinson-MT <88385899+MichealWilkinson-MT@users.noreply.github.com>
2021-08-11 16:26:24 +0100

    Update setup_test_mssql_docker.sh

--- bin/setup_test_mssql_docker.sh
+++ bin/setup_test_mssql_docker.sh
@@ -26,7 +26,7 @@ sleep $wait_time
 echo database started...
 
 # Create Databases
-bin/createDatabaseInDocker.sh ${containerName} ${MSQL_TEST_DB_NAME} ${testDBUsername} ${testDBPassword}
+bin/createDatabaseInDocker.sh ${containerName} $MSQL_TEST_DB_NAME ${testDBUsername} ${testDBPassword}
 
 # Run create tables scripts.
 npm run dbmigratetest up:mssql
